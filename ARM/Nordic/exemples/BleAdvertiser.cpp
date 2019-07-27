b751f8822654cd1c914155527a927d483caefba1
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2019-07-27 11:26:25 -0400

    nRF51 initial commit

---
+++ ARM/Nordic/exemples/BleAdvertiser.cpp
@@ -0,0 +1,134 @@
+/**-------------------------------------------------------------------------
+@example	BleAdvertiser.cpp
+
+@brief	BLE non-connectable, advertising incremental counter. Counter increments every second.
+
+@author	Hoang Nguyen Hoan
+@date	Dec. 19, 2017
+
+@license
+
+Copyright (c) 2017, I-SYST inc., all rights reserved
+
+Permission to use, copy, modify, and distribute this software for any purpose
+with or without fee is hereby granted, provided that the above copyright
+notice and this permission notice appear in all copies, and none of the
+names : I-SYST or its contributors may be used to endorse or
+promote products derived from this software without specific prior written
+permission.
+
+For info or contributing contact : hnhoan at i-syst dot com
+
+THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+----------------------------------------------------------------------------*/
+#include <string.h>
+
+#include "app_util.h"
+
+#include "istddef.h"
+#include "ble_app.h"
+#include "iopinctrl.h"
+
+#define DEVICE_NAME                     "Advertiser"
+
+#define APP_ADV_INTERVAL                MSEC_TO_UNITS(500, UNIT_0_625_MS)
+#define APP_ADV_TIMEOUT_IN_SECONDS      MSEC_TO_UNITS(1000, UNIT_10_MS)
+
+uint32_t g_AdvCnt = 0;
+
+const BLEAPP_CFG s_BleAppCfg = {
+	{ // Clock config nrf_clock_lf_cfg_t
+#ifdef IMM_NRF51822
+		NRF_CLOCK_LF_SRC_RC,	// Source RC
+		1, 1, 0
+#else
+		NRF_CLOCK_LF_SRC_XTAL,	// Source 32KHz XTAL
+#ifdef NRF51
+		0, 0, NRF_CLOCK_LF_XTAL_ACCURACY_20_PPM
+#else
+		0, 0, NRF_CLOCK_LF_ACCURACY_20_PPM
+#endif
+#endif
+	},
+	0,						// Number of central link
+	1,						// Number of peripheral link
+	BLEAPP_MODE_NOCONNECT,	// Connectionless beacon type
+	DEVICE_NAME,			// Device name
+	ISYST_BLUETOOTH_ID,		// PnP Bluetooth/USB vendor id
+	1,                      // PnP Product ID
+	0,						// Pnp prod version
+	false,					// Enable device information service (DIS)
+	NULL,					// Pointer device info descriptor
+	(uint8_t*)&g_AdvCnt,   	// Manufacture specific data to advertise
+	sizeof(g_AdvCnt),      	// Length of manufacture specific data
+	NULL,
+	0,
+	BLEAPP_SECTYPE_NONE,    // Secure connection type
+	BLEAPP_SECEXCHG_NONE,   // Security key exchange
+	NULL,      				// Service uuids to advertise
+	0, 						// Total number of uuids
+	APP_ADV_INTERVAL,       	// Advertising interval in msec
+	APP_ADV_TIMEOUT_IN_SECONDS,	// Advertising timeout in sec
+	0,							// Slow advertising interval, if > 0, fallback to
+								// slow interval on adv timeout and advertise until connected
+	0,		// Min. connection interval
+	0,		// Max. connection interval
+	-1,		// Led port nuber
+	-1,		// Led pin number
+	0,
+	0,		// Tx power
+	NULL	// RTOS Softdevice handler
+};
+
+void BlePeriphEvtUserHandler(ble_evt_t * p_ble_evt)
+{
+    if (p_ble_evt->header.evt_id == BLE_GAP_EVT_TIMEOUT)
+    {
+    		// for re-advertisement
+//    		g_AdvCnt++;
+
+//    		BleAppAdvManDataSet((uint8_t*)&g_AdvCnt, sizeof(g_AdvCnt));
+//    		BleAppAdvStart(BLEAPP_ADVMODE_FAST);
+    }
+}
+
+void BleAppAdvTimeoutHandler()
+{
+	g_AdvCnt++;
+
+	BleAppAdvManDataSet((uint8_t*)&g_AdvCnt, sizeof(g_AdvCnt), NULL, 0);
+	BleAppAdvStart(BLEAPP_ADVMODE_FAST);
+}
+
+//
+// Print a greeting message on standard output and exit.
+//
+// On embedded platforms this might require semi-hosting or similar.
+//
+// For example, for toolchains derived from GNU Tools for Embedded,
+// to enable semi-hosting, the following was added to the linker:
+//
+// --specs=rdimon.specs -Wl,--start-group -lgcc -lc -lm -lrdimon -Wl,--end-group
+//
+// Adjust it for other toolchains.
+//
+
+int main()
+{
+    BleAppInit((const BLEAPP_CFG *)&s_BleAppCfg, true);
+
+    BleAppRun();
+
+	return 0;
+}
+
---
+++ ARM/Nordic/exemples/TimerDemo.cpp
@@ -0,0 +1,156 @@
+/**-------------------------------------------------------------------------
+@example	TimerDemo.cpp
+
+
+@brief	Timer class usage demo.
+
+
+@author	Hoang Nguyen Hoan
+@date	Sep. 7, 2017
+
+@license
+
+Copyright (c) 2017, I-SYST inc., all rights reserved
+
+Permission to use, copy, modify, and distribute this software for any purpose
+with or without fee is hereby granted, provided that the above copyright
+notice and this permission notice appear in all copies, and none of the
+names : I-SYST or its contributors may be used to endorse or
+promote products derived from this software without specific prior written
+permission.
+
+For info or contributing contact : hnhoan at i-syst dot com
+
+THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+----------------------------------------------------------------------------*/
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "nrf.h"
+
+#ifdef NORDIC_SDK
+#include "nrf_drv_timer.h"
+#include "bsp.h"
+#include "app_error.h"
+#else
+#include "timer_nrf5x.h"
+#include "timer_nrf_app_timer.h"
+#endif
+
+#include "coredev/iopincfg.h"
+#include "iopinctrl.h"
+#include "coredev/uart.h"
+#include "stddev.h"
+
+uint64_t g_TickCount = 0;
+uint32_t g_Diff = 0;
+
+
+#ifdef NORDIC_SDK
+const nrf_drv_timer_t TIMER_LED = NRF_DRV_TIMER_INSTANCE(0);
+
+/**
+ * @brief Handler for timer events.
+ */
+void timer_led_event_handler(nrf_timer_event_t event_type, void* p_context)
+{
+    static uint32_t i;
+    uint32_t led_to_invert = ((i++) % LEDS_NUMBER);
+
+    switch (event_type)
+    {
+        case NRF_TIMER_EVENT_COMPARE0:
+        	// Flip GPIO for oscilloscope measurement
+        	IOPinToggle(0, 22);
+            break;
+
+        default:
+            //Do nothing.
+            break;
+    }
+}
+#else
+
+void TimerHandler(Timer *pTimer, uint32_t Evt);
+
+const static TIMER_CFG s_TimerCfg = {
+    .DevNo = 0,
+	.ClkSrc = TIMER_CLKSRC_DEFAULT,
+	.Freq = 0,			// 0 => Default highest frequency
+	.IntPrio = 7,
+	.EvtHandler = TimerHandler
+};
+
+#if 1
+// Using RTC
+TimerLFnRF5x g_Timer;
+//TimerAppTimer g_Timer;
+#else
+// Using Timer
+TimerHFnRF5x g_Timer;
+#endif
+
+void TimerHandler(Timer *pTimer, uint32_t Evt)
+{
+    if (Evt & TIMER_EVT_TRIGGER(0))
+    {
+    	// Flip GPIO for oscilloscope measurement
+    	IOPinToggle(0, 22);
+#if 0
+    	uint64_t c = pTimer->nSecond();
+    	g_Diff = c - g_TickCount;
+    	g_TickCount = c;
+#endif
+    }
+}
+
+#endif
+
+/**
+ * @brief Function for main application entry.
+ */
+int main(void)
+{
+	IOPinConfig(0, 22, 0, IOPINDIR_OUTPUT, IOPINRES_NONE, IOPINTYPE_NORMAL);
+
+#ifdef NORDIC_SDK
+    uint32_t time_ms = 500; //Time(in miliseconds) between consecutive compare events.
+    uint32_t time_ticks;
+    uint32_t err_code = NRF_SUCCESS;
+
+//    bsp_board_leds_init();
+
+	//Configure TIMER_LED for generating simple light effect - leds on board will invert his state one after the other.
+    nrf_drv_timer_config_t timer_cfg = NRF_DRV_TIMER_DEFAULT_CONFIG;
+    err_code = nrf_drv_timer_init(&TIMER_LED, &timer_cfg, timer_led_event_handler);
+    APP_ERROR_CHECK(err_code);
+
+    time_ticks = nrf_drv_timer_ms_to_ticks(&TIMER_LED, 500UL);
+
+    nrf_drv_timer_extended_compare(
+         &TIMER_LED, NRF_TIMER_CC_CHANNEL0, time_ticks, NRF_TIMER_SHORT_COMPARE0_CLEAR_MASK, true);
+
+    nrf_drv_timer_enable(&TIMER_LED);
+#else
+    g_Timer.Init(s_TimerCfg);
+	uint64_t period = g_Timer.EnableTimerTrigger(0, 500000000ULL, TIMER_TRIG_TYPE_CONTINUOUS);
+
+	//printf("Period = %u\r\n", (uint32_t)period);
+#endif
+    while (1)
+    {
+        __WFE();
+//        printf("Count = %u, Diff = %u\r\n", (uint32_t)g_TickCount, g_Diff);
+    }
+}
+/** @} */
