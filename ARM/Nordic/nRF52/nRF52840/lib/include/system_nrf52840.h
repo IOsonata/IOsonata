ac871c31d37ae139e70b71b267a3ef35d421ccc7
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2019-07-27 12:49:40 -0400

    nRF52840 initial commit

---
+++ ARM/Nordic/nRF52/nRF52840/lib/include/system_nrf52840.h
@@ -0,0 +1,20 @@
+/*
+
+{NordicLicense}
+
+*/
+
+#ifndef SYSTEM_NRF52840_H
+#define SYSTEM_NRF52840_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "system_nrf.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SYSTEM_NRF52840_H */
