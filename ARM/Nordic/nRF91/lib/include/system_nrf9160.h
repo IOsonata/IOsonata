0fc9cfaea7271edb8116eceeeeaeba98829e0214
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2019-09-05 08:35:20 -0400

    nRF9160 inital ground work layout

---
+++ ARM/Nordic/nRF91/lib/include/system_nrf9160.h
@@ -0,0 +1,20 @@
+/*
+
+{NordicLicense}
+
+*/
+
+#ifndef SYSTEM_9160_H
+#define SYSTEM_9160_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "system_nrf.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SYSTEM_9160_H */
