315adbdcf442312da71d485e5bdd2379d3705564
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2019-12-08 09:39:30 -0500

    Remove unsued protypes

--- ARM/Nordic/include/ble_app.h
+++ ARM/Nordic/include/ble_app.h
@@ -232,13 +232,12 @@ void BleAppAdvStop();
 void BleAppDisconnect();
 
 bool BleAppScanInit(BLEAPP_SCAN_CFG *pCfg);
-//bool BleAppScanInit(ble_uuid128_t * const pBaseUid, ble_uuid_t * const pServUid);
-//bool BleAppScanStart();
 void BleAppScan();
 void BleAppScanStop();
 bool BleAppConnect(ble_gap_addr_t * const pDevAddr, ble_gap_conn_params_t * const pConnParam);
 bool BleAppEnableNotify(uint16_t ConnHandle, uint16_t CharHandle);
 bool BleAppWrite(uint16_t ConnHandle, uint16_t CharHandle, uint8_t *pData, uint16_t DatLen);
+int8_t GetValidTxPower(int TxPwr);
 
 #ifdef __cplusplus
 }
