a8540c341b149d971366e5452a0a533176ad86f0
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2020-03-15 22:11:37 -0400

    SDK examples nRF52840

---
+++ ARM/Nordic/nRF52/nRF52840/exemples/radio_test/src/main.c
@@ -0,0 +1,254 @@
+/**
+ * Copyright (c) 2014-2018 - 2019, Nordic Semiconductor ASA
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form, except as embedded into a Nordic
+ *    Semiconductor ASA integrated circuit in a product or a software update for
+ *    such product, must reproduce the above copyright notice, this list of
+ *    conditions and the following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ *
+ * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * 4. This software, with or without modification, must only be used with a
+ *    Nordic Semiconductor ASA integrated circuit.
+ *
+ * 5. Any software provided in binary form under this license must not be reverse
+ *    engineered, decompiled, modified and/or disassembled.
+ *
+ * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+/** @file
+ *
+ * @defgroup nrf_radio_test_example_main main.c
+ * @{
+ * @ingroup nrf_radio_test_example
+ * @brief Radio Test Example application main file.
+ *
+ * This file contains the source code for a sample application that uses the NRF_RADIO and is controlled through the serial port.
+ *
+ */
+
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "bsp.h"
+#include "nrf.h"
+#include "radio_test.h"
+#include "app_uart.h"
+#include "app_error.h"
+#include "nordic_common.h"
+#include "nrf_drv_clock.h"
+#include "app_usbd_core.h"
+#include "app_usbd.h"
+#include "app_usbd_string_desc.h"
+#include "app_usbd_cdc_acm.h"
+#include "nrf_cli.h"
+//#include "nrf_cli_uart.h"
+#include "nrf_cli_cdc_acm.h"
+#include "nrf_drv_usbd.h"
+
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+
+#if 0
+NRF_CLI_UART_DEF(m_cli_uart_transport, 0, 64, 16);
+NRF_CLI_DEF(m_cli_uart,
+            "uart_cli:~$ ",
+            &m_cli_uart_transport.transport,
+            '\r',
+            CLI_EXAMPLE_LOG_QUEUE_SIZE);
+#else
+NRF_CLI_CDC_ACM_DEF(m_cli_cdc_acm_transport);
+NRF_CLI_DEF(m_cli_cdc_acm,
+            "usb_cli:~$ ",
+            &m_cli_cdc_acm_transport.transport,
+            '\r',
+            CLI_EXAMPLE_LOG_QUEUE_SIZE);
+#endif
+/**
+ * @brief Enable power USB detection
+ *
+ * Configure if example supports USB port connection
+ */
+#ifndef USBD_POWER_DETECTION
+#define USBD_POWER_DETECTION true
+#endif
+
+
+static void usbd_user_ev_handler(app_usbd_event_type_t event)
+{
+    switch (event)
+    {
+        case APP_USBD_EVT_STOPPED:
+            app_usbd_disable();
+            break;
+        case APP_USBD_EVT_POWER_DETECTED:
+            if (!nrf_drv_usbd_is_enabled())
+            {
+                app_usbd_enable();
+            }
+            break;
+        case APP_USBD_EVT_POWER_REMOVED:
+            app_usbd_stop();
+            break;
+        case APP_USBD_EVT_POWER_READY:
+            app_usbd_start();
+            break;
+        default:
+            break;
+    }
+}
+
+
+/**@brief Function for starting a command line interface that works on the UART transport layer.
+ */
+static void cli_start(void)
+{
+    ret_code_t ret;
+
+    ret = nrf_cli_start(&m_cli_cdc_acm);
+    APP_ERROR_CHECK(ret);
+}
+
+
+/**@brief Function for configuring UART for CLI.
+ */
+static void cli_init(void)
+{
+    ret_code_t ret;
+
+    ret = nrf_cli_init(&m_cli_cdc_acm, NULL, true, true, NRF_LOG_SEVERITY_INFO);
+    APP_ERROR_CHECK(ret);
+#if 0
+    nrf_drv_uart_config_t uart_config = NRF_DRV_UART_DEFAULT_CONFIG;
+
+    uart_config.pseltxd = TX_PIN_NUMBER;
+    uart_config.pselrxd = RX_PIN_NUMBER;
+    uart_config.hwfc    = NRF_UART_HWFC_DISABLED;
+    ret                 = nrf_cli_init(&m_cli_uart, &uart_config, true, true, NRF_LOG_SEVERITY_INFO);
+    APP_ERROR_CHECK(ret);
+#endif
+}
+
+static void usbd_init(void)
+{
+    ret_code_t ret;
+    static const app_usbd_config_t usbd_config = {
+        .ev_handler = app_usbd_event_execute,
+        .ev_state_proc = usbd_user_ev_handler
+    };
+    ret = app_usbd_init(&usbd_config);
+    APP_ERROR_CHECK(ret);
+
+    app_usbd_class_inst_t const * class_cdc_acm =
+            app_usbd_cdc_acm_class_inst_get(&nrf_cli_cdc_acm);
+    ret = app_usbd_class_append(class_cdc_acm);
+    APP_ERROR_CHECK(ret);
+
+    if (USBD_POWER_DETECTION)
+    {
+        ret = app_usbd_power_events_enable();
+        APP_ERROR_CHECK(ret);
+    }
+    else
+    {
+        NRF_LOG_INFO("No USB power detection enabled\nStarting USB now");
+
+        app_usbd_enable();
+        app_usbd_start();
+    }
+
+    /* Give some time for the host to enumerate and connect to the USB CDC port */
+    //nrf_delay_ms(1000);
+}
+
+/**@brief Function for initializing logging.
+ */
+static void log_init(void)
+{
+    ret_code_t err_code = NRF_LOG_INIT(app_timer_cnt_get);
+
+    APP_ERROR_CHECK(err_code);
+}
+
+
+/** @brief Function for configuring all peripherals used in this example.
+ */
+static void init(void)
+{
+    NRF_RNG->TASKS_START = 1;
+
+#ifdef NVMC_ICACHECNF_CACHEEN_Msk
+    NRF_NVMC->ICACHECNF  = NVMC_ICACHECNF_CACHEEN_Enabled << NVMC_ICACHECNF_CACHEEN_Pos;
+#endif // NVMC_ICACHECNF_CACHEEN_Msk
+
+    // Start 64 MHz crystal oscillator.
+    NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
+    NRF_CLOCK->TASKS_HFCLKSTART    = 1;
+
+    // Wait for the external oscillator to start up.
+    while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0)
+    {
+        // Do nothing.
+    }
+}
+
+
+/** @brief Function for the main application entry.
+ */
+int main(void)
+{
+    uint32_t err_code;
+
+    log_init();
+
+    err_code = nrf_drv_clock_init();
+    APP_ERROR_CHECK(err_code);
+    nrf_drv_clock_lfclk_request(NULL);
+
+    err_code = app_timer_init();
+    APP_ERROR_CHECK(err_code);
+
+    init();
+
+    cli_init();
+    usbd_init();
+
+    cli_start();
+
+    NVIC_EnableIRQ(TIMER0_IRQn);
+    __enable_irq();
+
+    NRF_LOG_RAW_INFO("Radio test example started.\r\n");
+
+    while (true)
+    {
+        UNUSED_RETURN_VALUE(NRF_LOG_PROCESS());
+        nrf_cli_process(&m_cli_cdc_acm);
+    }
+}
+
+
+/** @} */
