44536222f383334ffaf100188fbf8882286da34b
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2020-04-12 19:41:55 -0400

    E-Ink display interface implementation

---
+++ include/display/eink_intrf.h
@@ -0,0 +1,172 @@
+/**-------------------------------------------------------------------------
+@file	eink_inrf.h
+
+@brief	Implementation of E-Ink display serial interface
+
+This driver operates in BSI low mode.  BSI can be hardware pulled down or
+connected MCU. If connected to MCU BSI will be force to low by this driver.
+This mode allows to support both SPI interface or bit banging
+
+@author	Hoang Nguyen Hoan
+@date	Apr. 5, 2020
+
+@license
+
+MIT License
+
+Copyright (c) 2020 I-SYST inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+----------------------------------------------------------------------------*/
+
+#ifndef __EINK_INTRF_H__
+#define __EINK_INTRF_H__
+
+#include "coredev/spi.h"
+#include "device.h"
+
+/// E-Ink interface type
+typedef enum __EInk_Intrt_Type {
+	EIINTRF_TYPE_SPI,				//1< 3 wire SPI
+	EIINTRF_TYPE_BITBANG,			//!< Bit banging
+} EIINTRF_TYPE;
+
+/// Pin map array indexes. Pin map array must follow the order indicated by these indexes
+#define EIINTRF_DC_PIN_IDX			0	//!< Output Data (1)/Command(0) select (valid only in SPI mode)
+#define EIINTRF_BUSY_PIN_IDX		1	//!< Input display busy flag, 0 : busy
+#define EIINTRF_RST_PIN_IDX			2	//!< Display reset, active low
+#define EIINTRF_BSI_PIN_IDX			3	//!< serial interface select 0 : SPI, 1 : Bitbang
+#define EIINTRF_SCL_PIN_IDX			4	//!< Clock
+#define EIINTRF_SDA_PIN_IDX			5	//!< Data
+#define EIINTRF_CS_PIN_IDX			6	//!< Chip select
+
+/// E-Ink interface configuration data
+typedef struct __EInk_Intrf_Cfg {
+	EIINTRF_TYPE Type;				//!< Interface type SPI or Bitbanging
+	const IOPINCFG *pIOPinMap;		//!< Control pins
+	int NbIOPins;					//!< Total number of pin to use
+	SPIDEV * const pSpiDev;			//!< Pointer to SPI device data if SPI mode is used
+	int SpiCsIdx;					//!< CS index in the SPI driver
+} EIINTRFCFG;
+
+/// E-Ink interface driver data
+typedef struct __EInk_Intrf_Device {
+	EIINTRF_TYPE Type;
+	const IOPINCFG *pIOPinMap;
+	int NbIOPins;
+	DEVINTRF DevIntrf;
+	SPIDEV *pSpiDev;
+	int SpiCsIdx;
+} EIINTRFDEV;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief	E-Ink interface initialization
+ *
+ * @param	pDev	 : Pointer E-Ink device data to be initialized
+ * @param	pCfgData : Pointer to configuration data.
+ *
+ * @return	true - success
+ */
+bool EInkIntrfInit(EIINTRFDEV * const pDev, const EIINTRFCFG *pCfgData);
+
+/**
+ * @brief	Select data/cmd mode
+ *
+ * @param	pDev : Pointer to E-Ink device data (this driver data)
+ * @param	bDataMode : true - Data mode, false : Command mode
+ */
+void EInkIntrfSetDataMode(EIINTRFDEV *pDev, bool bDataMode);
+static inline void EInkIntrfReset(EIINTRFDEV * const pDev) { DeviceIntrfReset(&pDev->DevIntrf);}
+static inline int EInkIntrfRx(EIINTRFDEV * const pDev, uint8_t *pBuff, int BuffLen) {
+	return DeviceIntrfRx(&pDev->DevIntrf, pDev->SpiCsIdx, pBuff, BuffLen);
+}
+static inline int EInkIntrfTx(EIINTRFDEV * const pDev, uint8_t *pData, int DataLen) {
+	return DeviceIntrfTx(&pDev->DevIntrf, pDev->SpiCsIdx, pData, DataLen);
+}
+
+#ifdef __cplusplus
+}
+
+/// E-Ink interface class
+class EInkIntrf : public DeviceIntrf {
+public:
+	EInkIntrf() { memset((void*)&vDevData, 0, (int)sizeof(vDevData));}
+	EInkIntrf(EInkIntrf&);	// copy ctor not allowed
+
+	/**
+	 * @brief	E-Ink interface initialization
+	 *
+	 * @param	pCfgData : Pointer to configuration data.
+	 *
+	 * @return	true - success
+	 */
+	bool Init(const EIINTRFCFG &Cfg);
+	operator DEVINTRF * const () { return &vDevData.DevIntrf; }
+	operator EIINTRFDEV& () { return vDevData; };			// Get config data
+	operator EIINTRFDEV * const () { return &vDevData; };	// Get pointer to device data
+	int Rate(int RateHz) { return vDevData.DevIntrf.SetRate(&vDevData.DevIntrf, RateHz); }
+	int Rate(void) { return vDevData.DevIntrf.GetRate(&vDevData.DevIntrf); }	// Get rate in Hz
+	void Enable(void) { DeviceIntrfEnable(&vDevData.DevIntrf); }
+	void Disable(void) { DeviceIntrfDisable(&vDevData.DevIntrf); }
+
+	/**
+	 * @brief	Select data/cmd mode
+	 *
+	 * @param	pDev : Pointer to E-Ink device data (this driver data)
+	 * @param	bDataMode : true - Data mode, false : Command mode
+	 */
+	void SetDataMode(bool bDataMode) { EInkIntrfSetDataMode(&vDevData, bDataMode); }
+
+	// DevCs is the ordinal starting from 0 of device connected to the SPI bus.
+	// It is translated to CS index in the I/O pin map
+	virtual bool StartRx(int DevCs) {
+		return DeviceIntrfStartRx(&vDevData.DevIntrf, DevCs);
+	}
+
+	// Receive Data only, no Start/Stop condition
+	virtual int RxData(uint8_t *pBuff, int BuffLen) {
+		return DeviceIntrfRxData(&vDevData.DevIntrf, pBuff, BuffLen);
+	}
+	virtual void StopRx(void) { DeviceIntrfStopRx(&vDevData.DevIntrf); }
+
+	// DevAddr is the ordinal starting from 0 of device connected to the SPI bus.
+	// It is translated to CS index in the I/O pin map
+	virtual bool StartTx(int DevCs) {
+		return DeviceIntrfStartTx(&vDevData.DevIntrf, DevCs);
+	}
+
+	// Send Data only, no Start/Stop condition
+	virtual int TxData(uint8_t *pData, int DataLen) {
+		return DeviceIntrfTxData(&vDevData.DevIntrf, pData, DataLen);
+	}
+	virtual void StopTx(void) { DeviceIntrfStopTx(&vDevData.DevIntrf); }
+
+protected:
+private:
+	EIINTRFDEV vDevData;	//!< This driver data
+};
+
+#endif
+
+#endif // __EINK_INTRF_H__
---
+++ src/display/eink_intrf.cpp
@@ -0,0 +1,289 @@
+/**-------------------------------------------------------------------------
+@file	eink_inrf.cpp
+
+@brief	Implementation of E-Ink display serial interface
+
+This driver operates in BSI low mode.  BSI can be hardware pulled down or
+connected MCU. If connected to MCU BSI will be force to low by this driver.
+This mode allows to support both SPI interface or bit banging
+
+@author	Hoang Nguyen Hoan
+@date	Apr. 5, 2020
+
+@license
+
+MIT License
+
+Copyright (c) 2020 I-SYST inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+----------------------------------------------------------------------------*/
+#include "idelay.h"
+#include "iopinctrl.h"
+#include "display/eink_intrf.h"
+
+static bool EInkIntrfWaitBusy(EIINTRFDEV * const pDev, uint32_t Timeout = 0)
+{
+	while ((--Timeout > 0) &&
+		   (IOPinRead(pDev->pIOPinMap[EIINTRF_BUSY_PIN_IDX].PortNo, pDev->pIOPinMap[EIINTRF_BUSY_PIN_IDX].PinNo) == 0));
+
+	return Timeout > 0;
+}
+
+static void EInkIntrfDisable(DEVINTRF * const pDev)
+{
+
+}
+
+static void EInkIntrfEnable(DEVINTRF * const pDev)
+{
+
+}
+
+static int EInkIntrfGetRate(DEVINTRF * const pDev)
+{
+	return 1;
+}
+
+static int EInkIntrfSetRate(DEVINTRF * const pDev, int Rate)
+{
+	return 1;
+}
+
+static bool EInkIntrfStartRx(DEVINTRF * const pDev, int DevAddr)
+{
+	EIINTRFDEV *dev = (EIINTRFDEV*)pDev->pDevData;
+
+	EInkIntrfSetDataMode(dev, true);
+
+	if (dev->Type == EIINTRF_TYPE_BITBANG)
+	{
+		IOPinClear(dev->pIOPinMap[EIINTRF_CS_PIN_IDX].PortNo, dev->pIOPinMap[EIINTRF_CS_PIN_IDX].PinNo);
+	    IOPinSetDir(dev->pIOPinMap[EIINTRF_SDA_PIN_IDX].PortNo, dev->pIOPinMap[EIINTRF_SDA_PIN_IDX].PinNo, IOPINDIR_INPUT);
+	}
+	else
+	{
+		SPIStartRx(dev->pSpiDev, dev->SpiCsIdx);
+	}
+
+	return true;
+}
+
+static int EInkIntrfRxData(DEVINTRF * const pDev, uint8_t *pBuff, int BuffLen)
+{
+	EIINTRFDEV *dev = (EIINTRFDEV*)pDev->pDevData;
+	int cnt = 0;
+
+	if (dev->Type == EIINTRF_TYPE_SPI)
+	{
+		return SPIRxData(dev->pSpiDev, pBuff, BuffLen);
+	}
+
+	while (BuffLen > 0)
+	{
+		*pBuff = 0;
+		for (int i = 0; i < 8; i++)
+		{
+			*pBuff <<=1;
+			IOPinSet(dev->pIOPinMap[EIINTRF_SCL_PIN_IDX].PortNo, dev->pIOPinMap[EIINTRF_SCL_PIN_IDX].PinNo);
+			IOPinClear(dev->pIOPinMap[EIINTRF_SCL_PIN_IDX].PortNo, dev->pIOPinMap[EIINTRF_SCL_PIN_IDX].PinNo);
+			*pBuff |= IOPinRead(dev->pIOPinMap[EIINTRF_SDA_PIN_IDX].PortNo, dev->pIOPinMap[EIINTRF_SDA_PIN_IDX].PinNo);
+		}
+		BuffLen--;
+		pBuff++;
+	}
+
+	return cnt;
+}
+
+static void EInkIntrfStopRx(DEVINTRF * const pDev)
+{
+	EIINTRFDEV *dev = (EIINTRFDEV*)pDev->pDevData;
+
+	if (dev->Type == EIINTRF_TYPE_BITBANG)
+	{
+	    IOPinSetDir(dev->pIOPinMap[EIINTRF_SDA_PIN_IDX].PortNo, dev->pIOPinMap[EIINTRF_SDA_PIN_IDX].PinNo, IOPINDIR_OUTPUT);
+		IOPinSet(dev->pIOPinMap[EIINTRF_CS_PIN_IDX].PortNo, dev->pIOPinMap[EIINTRF_CS_PIN_IDX].PinNo);
+	}
+	else
+	{
+		SPIStopRx(dev->pSpiDev);
+	}
+}
+
+static bool EInkIntrfStartTx(DEVINTRF * const pDev, int DevAddr)
+{
+	EIINTRFDEV *dev = (EIINTRFDEV*)pDev->pDevData;
+
+	if (dev->Type == EIINTRF_TYPE_BITBANG)
+	{
+		IOPinClear(dev->pIOPinMap[EIINTRF_CS_PIN_IDX].PortNo, dev->pIOPinMap[EIINTRF_CS_PIN_IDX].PinNo);
+	}
+	else
+	{
+		SPIStartTx(dev->pSpiDev, dev->SpiCsIdx);
+	}
+
+	return true;
+}
+
+static int EInkIntrfTxData(DEVINTRF * const pDev, uint8_t *pData, int DataLen)
+{
+	EIINTRFDEV *dev = (EIINTRFDEV*)pDev->pDevData;
+	int cnt = 0;
+
+	if (dev->Type == EIINTRF_TYPE_SPI)
+	{
+		return SPITxData(dev->pSpiDev, pData, DataLen);
+	}
+
+	while (DataLen > 0)
+	{
+		uint8_t bit = 0x80;
+
+		while (bit != 0)
+		{
+			if (bit & *pData)
+			{
+				IOPinSet(dev->pIOPinMap[EIINTRF_SDA_PIN_IDX].PortNo, dev->pIOPinMap[EIINTRF_SDA_PIN_IDX].PinNo);
+			}
+			else
+			{
+				IOPinClear(dev->pIOPinMap[EIINTRF_SDA_PIN_IDX].PortNo, dev->pIOPinMap[EIINTRF_SDA_PIN_IDX].PinNo);
+			}
+			IOPinSet(dev->pIOPinMap[EIINTRF_SCL_PIN_IDX].PortNo, dev->pIOPinMap[EIINTRF_SCL_PIN_IDX].PinNo);
+			//usDelay(1);
+			IOPinClear(dev->pIOPinMap[EIINTRF_SCL_PIN_IDX].PortNo, dev->pIOPinMap[EIINTRF_SCL_PIN_IDX].PinNo);
+
+			bit >>= 1;
+		}
+
+		DataLen--;
+		pData++;
+		cnt++;
+	}
+
+	return cnt;
+}
+
+static void EInkIntrfStopTx(DEVINTRF * const pDev)
+{
+	EIINTRFDEV *dev = (EIINTRFDEV*)pDev->pDevData;
+
+	if (dev->Type == EIINTRF_TYPE_BITBANG)
+	{
+		IOPinSet(dev->pIOPinMap[EIINTRF_CS_PIN_IDX].PortNo, dev->pIOPinMap[EIINTRF_CS_PIN_IDX].PinNo);
+	}
+	else
+	{
+		SPIStopTx(dev->pSpiDev);
+	}
+}
+
+static void EInkIntrfReset(DEVINTRF * const pDev)
+{
+	EIINTRFDEV *dev = (EIINTRFDEV*)pDev->pDevData;
+
+	IOPinClear(dev->pIOPinMap[EIINTRF_RST_PIN_IDX].PortNo, dev->pIOPinMap[EIINTRF_RST_PIN_IDX].PinNo);
+	usDelay(500);
+	IOPinSet(dev->pIOPinMap[EIINTRF_RST_PIN_IDX].PortNo, dev->pIOPinMap[EIINTRF_RST_PIN_IDX].PinNo);
+}
+
+static void EInkIntrfPowerOff(DEVINTRF * const pDev)
+{
+
+}
+
+void EInkIntrfSetDataMode(EIINTRFDEV *pDev, bool bDataMode)
+{
+	if (bDataMode)
+	{
+		IOPinSet(pDev->pIOPinMap[EIINTRF_DC_PIN_IDX].PortNo, pDev->pIOPinMap[EIINTRF_DC_PIN_IDX].PinNo);
+	}
+	else
+	{
+		IOPinClear(pDev->pIOPinMap[EIINTRF_DC_PIN_IDX].PortNo, pDev->pIOPinMap[EIINTRF_DC_PIN_IDX].PinNo);
+	}
+}
+
+bool EInkIntrfInit(EIINTRFDEV * const pDev, const EIINTRFCFG *pCfgData)
+{
+	IOPinCfg(pCfgData->pIOPinMap, pCfgData->NbIOPins);
+	IOPinSet(pDev->pIOPinMap[EIINTRF_RST_PIN_IDX].PortNo, pDev->pIOPinMap[EIINTRF_RST_PIN_IDX].PinNo);
+
+	if (pCfgData->Type == EIINTRF_TYPE_SPI)
+	{
+		if (pCfgData->pSpiDev == NULL)
+		{
+			return false;
+		}
+		pDev->pSpiDev = pCfgData->pSpiDev;
+		pDev->SpiCsIdx = pCfgData->SpiCsIdx;
+	}
+	else
+	{
+		if (pCfgData->NbIOPins <= EIINTRF_CS_PIN_IDX)
+		{
+			return false;
+		}
+		IOPinClear(pDev->pIOPinMap[EIINTRF_SCL_PIN_IDX].PortNo, pDev->pIOPinMap[EIINTRF_SCL_PIN_IDX].PinNo);
+		IOPinSet(pDev->pIOPinMap[EIINTRF_SDA_PIN_IDX].PortNo, pDev->pIOPinMap[EIINTRF_SDA_PIN_IDX].PinNo);
+		IOPinSet(pDev->pIOPinMap[EIINTRF_CS_PIN_IDX].PortNo, pDev->pIOPinMap[EIINTRF_CS_PIN_IDX].PinNo);
+	}
+
+
+	if (pCfgData->NbIOPins > EIINTRF_BSI_PIN_IDX)
+	{
+		IOPinClear(pDev->pIOPinMap[EIINTRF_BSI_PIN_IDX].PortNo, pDev->pIOPinMap[EIINTRF_BSI_PIN_IDX].PinNo);
+	}
+
+	pDev->Type = pCfgData->Type;
+	pDev->pIOPinMap = pCfgData->pIOPinMap;
+	pDev->NbIOPins = pCfgData->NbIOPins;
+	pDev->DevIntrf.pDevData = (void*)pDev;
+
+	pDev->DevIntrf.Type = DEVINTRF_TYPE_UNKOWN;
+	pDev->DevIntrf.Disable = EInkIntrfDisable;
+	pDev->DevIntrf.Enable = EInkIntrfEnable;
+	pDev->DevIntrf.GetRate = EInkIntrfGetRate;
+	pDev->DevIntrf.SetRate = EInkIntrfSetRate;
+	pDev->DevIntrf.StartRx = EInkIntrfStartRx;
+	pDev->DevIntrf.RxData = EInkIntrfRxData;
+	pDev->DevIntrf.StopRx = EInkIntrfStopRx;
+	pDev->DevIntrf.StartTx = EInkIntrfStartTx;
+	pDev->DevIntrf.TxData = EInkIntrfTxData;
+	pDev->DevIntrf.StopTx = EInkIntrfStopTx;
+	pDev->DevIntrf.IntPrio = 6;
+	pDev->DevIntrf.EvtCB = nullptr;
+	pDev->DevIntrf.EnCnt = 1;
+	pDev->DevIntrf.MaxRetry = 1;
+	pDev->DevIntrf.bDma = false;
+	pDev->DevIntrf.PowerOff = EInkIntrfPowerOff;
+	pDev->DevIntrf.Reset = EInkIntrfReset;
+
+	atomic_flag_clear(&pDev->DevIntrf.bBusy);
+
+	return true;
+}
+
+bool EInkIntrf::Init(const EIINTRFCFG &Cfg)
+{
+	return EInkIntrfInit(&vDevData, &Cfg);
+}
