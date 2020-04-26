f6d1987f3da037d985e4e7e44f27f68b9117eaf3
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2020-04-25 23:31:27 -0400

    Update to MIT license

--- ARM/Nordic/src/ble_service.cpp
+++ ARM/Nordic/src/ble_service.cpp
@@ -1,37 +1,40 @@
-/*--------------------------------------------------------------------------
-File   : ble_service.c
+/**-------------------------------------------------------------------------
+@file	ble_service.c
 
-Author : Hoang Nguyen Hoan          Mar. 25, 2014
+@brief	Implement Bluetooth LE service and characteristic
 
-Desc   : Implementation allow the creation of generic custom Bluetooth Smart
-		 service with multiple user defined characteristics.
-		 This implementation is to be used with Nordic SDK
+Implementation allow the creation of generic custom Bluetooth Smart service
+with multiple user defined characteristics.
 
-Copyright (c) 2014, I-SYST inc., all rights reserved
+This implementation is to be used with Nordic SDK
 
-Permission to use, copy, modify, and distribute this software for any purpose
-with or without fee is hereby granted, provided that the above copyright
-notice and this permission notice appear in all copies, and none of the
-names : I-SYST or its contributors may be used to endorse or
-promote products derived from this software without specific prior written
-permission.
+@author	Hoang Nguyen Hoan
+@date	Mar. 25, 2014
 
-For info or contributing contact : hnhoan at i-syst dot com
+@license
 
-THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
-EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+MIT License
 
-----------------------------------------------------------------------------
-Modified by          Date              	Description
-Hoan				Mar. 11, 2017		Renaming
+Copyright (c) 2014-2020 I-SYST inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
 ----------------------------------------------------------------------------*/
 #include <stdint.h>
 #include <string.h>
