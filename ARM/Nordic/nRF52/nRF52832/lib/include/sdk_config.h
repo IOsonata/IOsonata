aa48b297d2ef53e8a1420c5617654f414e0b338d
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2020-09-22 09:18:50 -0400

    Enable APP_TIMER for BLE compilation

--- ARM/Nordic/nRF52/nRF52832/lib/include/sdk_config.h
+++ ARM/Nordic/nRF52/nRF52832/lib/include/sdk_config.h
@@ -7558,7 +7558,7 @@
 // <e> APP_TIMER_ENABLED - app_timer - Application timer functionality
 //==========================================================
 #ifndef APP_TIMER_ENABLED
-#define APP_TIMER_ENABLED 0
+#define APP_TIMER_ENABLED 1
 #endif
 // <o> APP_TIMER_CONFIG_RTC_FREQUENCY  - Configure RTC prescaler.
  
