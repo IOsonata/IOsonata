f44781f3d7718a416254b8a1ec400f066b49af95
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2022-03-27 22:23:24 -0400

    Wrong width

--- src/display/fonts/FreeSans/FreeSans24pt.c
+++ src/display/fonts/FreeSans/FreeSans24pt.c
@@ -3308,7 +3308,7 @@ static const CharDesc_t s_FreeSans24ptCharDesc[] = {
 // Font information for FreeSans 24pt
 const FontDesc_t iFontFreeSans24pt = {
 	FONT_TYPE_VAR_WIDTH,
-	29,
+	30,
 	32,
 	{ .pCharDesc = s_FreeSans24ptCharDesc }
 };
