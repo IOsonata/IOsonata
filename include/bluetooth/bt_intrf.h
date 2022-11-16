ae21f2e4a6eb1b9b91c1b8b03c0147a1a0caca73
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2022-11-15 22:51:54 -0500

    Renaming to be more generic

---
+++ include/bluetooth/bt_intrf.h
@@ -0,0 +1,163 @@
+/**-------------------------------------------------------------------------
+@file	bt_intrf.h
+
+@brief	Implementation allow the creation of generic serial interface of
+a custom Bluetooth service with multiple user defined characteristics.
+
+
+@author	Hoang Nguyen Hoan
+@date	Feb. 6, 2017
+
+@license
+
+MIT License
+
+Copyright (c) 2017, I-SYST inc., all rights reserved
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+----------------------------------------------------------------------------*/
+
+#ifndef __BT_INTRF_H__
+#define __BT_INTRF_H__
+
+#include "bluetooth/bt_gatt.h"
+#include "device_intrf.h"
+#include "cfifo.h"
+
+/** @addtogroup Bluetooth
+  * @{
+  */
+
+#define BTINTRF_TRANSBUFF_MAXLEN       512
+
+/**
+ * This structure define the CFIFO data packet
+ * It is a variable length structure defined by user
+ *
+ * NOTE : Data element can be more than 1 byte. DO NOT use sizeof() to calculate
+ * the size of this structure.  It should be mapped to appropriate buffer memory
+ */
+#pragma pack(push, 1)
+
+typedef struct __Bt_Interf_Packet {
+    uint16_t    Len;    // Valid data length
+    uint8_t     Data[1];// Data container array
+} BtIntrfPkt_t;
+
+#pragma pack(pop)
+
+/**
+ * Calculate require mem
+ */
+#define BTINTRF_PKHDR_LEN							(sizeof(BtIntrfPkt_t) - 1)
+#define BTINTRF_CFIFO_TOTAL_MEMSIZE(npk, pksize)	CFIFO_TOTAL_MEMSIZE(npk, pksize + BTINTRF_PKHDR_LEN)
+
+#pragma pack(push, 4)
+
+typedef struct __Bt_Interf_Config {
+	BtGattSrvc_t *pSrvc;		//!< BLE Service
+    int RxCharIdx;			//!< Write characteristic index (From BLE)
+    int TxCharIdx;			//!< Read characteristic index (to BLE)
+    int PacketSize;			//!< BLE packet size
+    bool bBlocking;			//!< true - Blocking Fifo, false - Non blocking
+	int RxFifoMemSize;		//!< Total memory size for CFIFO
+	uint8_t *pRxFifoMem;	//!< Pointer to memory to be used by CFIFO
+	int TxFifoMemSize;		//!< Total memory size for CFIFO
+	uint8_t *pTxFifoMem;	//!< Pointer to memory to be used by CFIFO
+	DevIntrfEvtHandler_t EvtCB;//!< Event callback
+} BtIntrfCfg_t;
+
+// BLE interf instance data
+typedef struct __Bt_Dev_Interf {
+	DevIntrf_t	DevIntrf;	//!< Base Device Interface
+	BtGattSrvc_t	*pSrvc;	//!< BLE Service
+    int			RxCharIdx;	//!< Write characteristic index (from BLE)
+    int			TxCharIdx;	//!< Read characteristic index (to BLE)
+    int			PacketSize;	//!< BLE packet size
+    HCFIFO		hRxFifo;
+    HCFIFO		hTxFifo;
+    uint32_t	RxDropCnt;
+    uint32_t	TxDropCnt;
+    uint8_t     TransBuff[BTINTRF_TRANSBUFF_MAXLEN];  //
+    int         TransBuffLen;   //!< Data length
+} BtDevIntrf_t;
+
+#pragma pack(pop)
+
+#ifdef __cplusplus
+
+class BtIntrf : public DeviceIntrf {
+public:
+	bool Init(const BtIntrfCfg_t &Cfg);
+
+	operator DevIntrf_t * const () { return &vBtDevIntrf.DevIntrf; }	// Get device interface data
+	operator BtGattSrvc_t * const () { return vBtDevIntrf.pSrvc; }
+	// Set data rate in bits/sec (Hz)
+	virtual uint32_t Rate(uint32_t DataRate) { return DeviceIntrfSetRate(&vBtDevIntrf.DevIntrf, DataRate); }
+	// Get current data rate in bits/sec (Hz)
+	virtual uint32_t Rate(void) { return DeviceIntrfGetRate(&vBtDevIntrf.DevIntrf); }
+	// Disable device for power reduction, re-enable with Enable() without
+	// full init
+	virtual void Disable(void) { DeviceIntrfDisable(&vBtDevIntrf.DevIntrf); }
+	// Enable device
+	virtual void Enable(void) { DeviceIntrfEnable(&vBtDevIntrf.DevIntrf); }
+
+	// Initiate receive
+	virtual bool StartRx(uint32_t DevAddr) { return DeviceIntrfStartRx(&vBtDevIntrf.DevIntrf, DevAddr); }
+	// Receive Data only, no Start/Stop condition
+	virtual int RxData(uint8_t *pBuff, int BuffLen) {
+		return DeviceIntrfRxData(&vBtDevIntrf.DevIntrf, pBuff, BuffLen);
+	}
+	// Stop receive
+	// BEWARE !!!!!
+	// This functions MUST ONLY be called if StartRx returns true.
+	virtual void StopRx(void) { DeviceIntrfStopRx(&vBtDevIntrf.DevIntrf); }
+	// Initiate transmit
+	virtual bool StartTx(uint32_t DevAddr) {
+		return DeviceIntrfStartTx(&vBtDevIntrf.DevIntrf, DevAddr);
+	}
+	// Transmit Data only, no Start/Stop condition
+	virtual int TxData(uint8_t *pData, int DataLen) {
+		return DeviceIntrfTxData(&vBtDevIntrf.DevIntrf, pData, DataLen);
+	}
+	// Stop transmit
+	// BEWARE !!!!!
+	// This functions MUST ONLY be called if StartTx returns true.
+	virtual void StopTx(void) { DeviceIntrfStopTx(&vBtDevIntrf.DevIntrf); }
+
+	virtual bool RequestToSend(int NbBytes);
+
+private:
+
+	BtDevIntrf_t vBtDevIntrf;
+};
+
+extern "C" {
+#endif
+
+bool BtIntrfInit(BtDevIntrf_t * const pBtDevIntrf, const BtIntrfCfg_t *pCfg);
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} end group Bluetooth */
+
+#endif // __BT_INTRF_H__
---
+++ src/bluetooth/bt_intrf.cpp
@@ -0,0 +1,433 @@
+/**-------------------------------------------------------------------------
+@file	bt_intrf.cpp
+
+@brief	Implementation allow the creation of generic serial interface of
+a custom Bluetooth service with multiple user defined characteristics.
+
+
+@author	Hoang Nguyen Hoan
+@date	Feb. 6, 2017
+
+@license
+
+MIT License
+
+Copyright (c) 2017, I-SYST inc., all rights reserved
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+----------------------------------------------------------------------------*/
+#include <string.h>
+
+#include "istddef.h"
+#include "cfifo.h"
+#include "bluetooth/bt_intrf.h"
+#include "bluetooth/ble_app.h"
+#include "interrupt.h"
+#include "bluetooth/ble_srvc.h"
+#include "bluetooth/bt_gatt.h"
+
+#define BTINTRF_PACKET_SIZE		(20)// + sizeof(BLEINTRF_PKT) - 1)
+#define BTINTRF_CFIFO_SIZE		BTINTRF_CFIFO_TOTAL_MEMSIZE(2, BTINTRF_PACKET_SIZE)
+
+alignas(4) static uint8_t s_BtDevIntrfRxFifoMem[BTINTRF_CFIFO_SIZE];
+alignas(4) static uint8_t s_BtDevIntrfTxFifoMem[BTINTRF_CFIFO_SIZE];
+
+/**
+ * @brief - Disable
+ * 		Turn off the interface.  If this is a physical interface, provide a
+ * way to turn off for energy saving. Make sure the turn off procedure can
+ * be turned back on without going through the full init sequence
+ *
+ * @param
+ * 		pDevIntrf : Pointer to an instance of the Device Interface
+ *
+ * @return None
+ */
+void BtIntrfDisable(DevIntrf_t *pDevIntrf)
+{
+	// TODO:
+}
+
+/**
+ * @brief - Enable
+ * 		Turn on the interface.
+ *
+ * @param
+ * 		pDevIntrf : Pointer to an instance of the Device Interface
+ *
+ * @return None
+ */
+void BtIntrfEnable(DevIntrf_t *pDevIntrf)
+{
+	// TODO:
+}
+
+/**
+ * @brief - GetRate
+ * 		Get data rate of the interface in Hertz.  This is not a clock frequency
+ * but rather the transfer frequency (number of transfers per second). It has meaning base on the
+ * implementation as bits/sec or bytes/sec or whatever the case
+ *
+ * @param
+ * 		pDevIntrf : Pointer to an instance of the Device Interface
+ *
+ * @return Transfer rate per second
+ */
+uint32_t BtIntrfGetRate(DevIntrf_t *pDevIntrf)
+{
+	return 0;	// BLE has no rate
+}
+
+/**
+ * @brief - SetRate
+ * 		Set data rate of the interface in Hertz.  This is not a clock frequency
+ * but rather the transfer frequency (number of transfers per second). It has meaning base on the
+ * implementation as bits/sec or bytes/sec or whatever the case
+ *
+ * @param
+ * 		pDevIntrf : Pointer to an instance of the Device Interface
+ * 		Rate 	  : Data rate to be set in Hertz (transfer per second)
+ *
+ * @return 	Actual transfer rate per second set.  It is the real capable rate
+ * 			closes to rate being requested.
+ */
+uint32_t BtIntrfSetRate(DevIntrf_t *pDevIntrf, uint32_t Rate)
+{
+	return 0; // BLE has no rate
+}
+
+/**
+ * @brief - StartRx
+ * 		Prepare start condition to receive data with subsequence RxData.
+ * This can be in case such as start condition for I2C or Chip Select for
+ * SPI or precondition for DMA transfer or whatever requires it or not
+ * This function must check & set the busy state for re-entrancy
+ *
+ * @param
+ * 		pDevIntrf : Pointer to an instance of the Device Interface
+ * 		DevAddr   : The device selection id scheme
+ *
+ * @return 	true - Success
+ * 			false - failed.
+ */
+bool BtIntrfStartRx(DevIntrf_t *pDevIntrf, uint32_t DevAddr)
+{
+	return true;
+}
+
+/**
+ * @brief - RxData : retrieve 1 packet of received data
+ * 		Receive data into pBuff passed in parameter.  Assuming StartRx was
+ * called prior calling this function to get the actual data. BufferLen
+ * to receive data must be at least 1 packet in size.  Otherwise remaining
+ * bytes are dropped.
+ *
+ * @param
+ * 		pDevIntrf : Pointer to an instance of the Device Interface
+ * 		pBuff 	  : Pointer to memory area to receive data.
+ * 		BuffLen   : Length of buffer memory in bytes. Must be at least 1 packet
+ * 		            in size.  Otherwise remaining bytes are dropped.
+ *
+ * @return	Number of bytes read
+ */
+int BtIntrfRxData(DevIntrf_t *pDevIntrf, uint8_t *pBuff, int BuffLen)
+{
+	BtDevIntrf_t *intrf = (BtDevIntrf_t*)pDevIntrf->pDevData;
+	BtIntrfPkt_t *pkt;
+	int cnt = 0;
+
+	pkt = (BtIntrfPkt_t *)CFifoGet(intrf->hRxFifo);
+	if (pkt != NULL)
+	{
+	    cnt = min(BuffLen, pkt->Len);
+		memcpy(pBuff, pkt->Data, cnt);
+	}
+
+	return cnt;
+}
+
+/**
+ * @brief - StopRx
+ * 		Completion of read data phase. Do require post processing
+ * after data has been received via RxData
+ * This function must clear the busy state for re-entrancy
+ *
+ * @param
+ * 		pDevIntrf : Pointer to an instance of the Device Interface
+ *
+ * @return	None
+ */
+void BtIntrfStopRx(DevIntrf_t *pSerDev)
+{
+	// TODO:
+}
+
+/**
+ * @brief - StartTx
+ * 		Prepare start condition to transfer data with subsequence TxData.
+ * This can be in case such as start condition for I2C or Chip Select for
+ * SPI or precondition for DMA transfer or whatever requires it or not
+ * This function must check & set the busy state for re-entrancy
+ *
+ * @param
+ * 		pDevIntrf : Pointer to an instance of the Device Interface
+ * 		DevAddr   : The device selection id scheme
+ *
+ * @return 	true - Success
+ * 			false - failed
+ */
+bool BtIntrfStartTx(DevIntrf_t *pDevIntrf, uint32_t DevAddr)
+{
+	return true;
+}
+
+bool BtIntrfNotify(BtDevIntrf_t *pIntrf)
+{
+	BtIntrfPkt_t *pkt = NULL;
+    uint32_t res = 0;
+
+    if (pIntrf->TransBuffLen > 0)
+    {
+        res = BleSrvcCharNotify(pIntrf->pSrvc, pIntrf->TxCharIdx, pIntrf->TransBuff, pIntrf->TransBuffLen);
+    }
+
+    while (res == 0)
+	{
+		pIntrf->TransBuffLen = 0;
+		uint32_t state = DisableInterrupt();
+		pkt = (BtIntrfPkt_t *)CFifoGet(pIntrf->hTxFifo);
+		EnableInterrupt(state);
+		if (pkt == NULL)
+		{
+			return true;
+		}
+		res = BleSrvcCharNotify(pIntrf->pSrvc, pIntrf->TxCharIdx, pkt->Data, pkt->Len);
+	}
+
+	if (pkt != NULL)
+	{
+		memcpy(pIntrf->TransBuff, pkt->Data, pkt->Len);
+		pIntrf->TransBuffLen = pkt->Len;
+	}
+
+	return false;
+}
+
+/**
+ * @brief - TxData
+ * 		Transfer data from pData passed in parameter.  Assuming StartTx was
+ * called prior calling this function to send the actual data
+ *
+ * @param
+ * 		pDevIntrf : Pointer to an instance of the Device Interface
+ * 		pData 	: Pointer to memory area of data to send.
+ * 		DataLen : Length of data memory in bytes
+ *
+ * @return	Number of bytes sent
+ */
+int BtIntrfTxData(DevIntrf_t *pDevIntrf, uint8_t *pData, int DataLen)
+{
+	BtDevIntrf_t *intrf = (BtDevIntrf_t*)pDevIntrf->pDevData;
+	BtIntrfPkt_t *pkt;
+    int maxlen = intrf->PacketSize - BTINTRF_PKHDR_LEN;
+	int cnt = 0;
+
+	while (DataLen > 0)
+	{
+	    uint32_t state = DisableInterrupt();
+		pkt = (BtIntrfPkt_t *)CFifoPut(intrf->hTxFifo);
+		EnableInterrupt(state);
+		if (pkt == NULL)
+		{
+			intrf->TxDropCnt++;
+			break;
+		}
+		int l = min(DataLen, maxlen);
+		memcpy(pkt->Data, pData, l);
+		pkt->Len = l;
+		DataLen -= l;
+		pData += l;
+		cnt += l;
+	}
+
+    BtIntrfNotify(intrf);
+
+	return cnt;
+}
+
+/**
+ * @brief - StopTx
+ * 		Completion of sending data via TxData.  Do require post processing
+ * after all data was transmitted via TxData.
+ * This function must clear the busy state for re-entrancy
+ *
+ * @param
+ * 		pDevIntrf : Pointer to an instance of the Device Interface
+ *
+ * @return	None
+ */
+void BtIntrfStopTx(DevIntrf_t *pDevIntrf)
+{
+
+}
+
+/**
+ * @brief - Reset
+ *      This function perform a reset of interface.  Must provide empty
+ * function of not used.
+ *
+ * @param
+ *      pDevIntrf : Pointer to an instance of the Device Interface
+ *
+ * @return  None
+ */
+void BtIntrfReset(DevIntrf_t *pDevIntrf)
+{
+
+}
+
+/**
+ *
+ *
+ */
+void BtIntrfTxComplete(BtGattChar_t *pChar, int CharIdx)
+{
+    BtIntrfNotify((BtDevIntrf_t*)pChar->pSrvc->pContext);
+}
+
+void BtIntrfRxWrCB(BtGattChar_t *pChar, uint8_t *pData, int Offset, int Len)
+{
+	BtDevIntrf_t *intrf = (BtDevIntrf_t*)pChar->pSrvc->pContext;
+	BtIntrfPkt_t *pkt;
+    //int maxlen = intrf->hTxFifo->BlkSize - sizeof(pkt->Len);
+
+	while (Len > 0) {
+		pkt = (BtIntrfPkt_t *)CFifoPut(intrf->hRxFifo);
+		if (pkt == NULL)
+		{
+			intrf->RxDropCnt++;
+			break;
+		}
+		int l = min(intrf->PacketSize - (int)BTINTRF_PKHDR_LEN, Len);
+		memcpy(pkt->Data, pData, l);
+		pkt->Len = l;
+		Len -= l;
+		pData += l;
+	}
+
+	if (CFifoUsed(intrf->hRxFifo) > 0 && intrf->DevIntrf.EvtCB != NULL)
+	{
+		intrf->DevIntrf.EvtCB(&intrf->DevIntrf, DEVINTRF_EVT_RX_DATA, NULL, 0);
+	}
+}
+
+bool BtIntrfInit(BtDevIntrf_t *pIntrf, const BtIntrfCfg_t *pCfg)
+{
+	if (pIntrf == NULL || pCfg == NULL)
+		return false;
+
+	if (pCfg->PacketSize <= 0)
+	{
+		pIntrf->PacketSize = BTINTRF_PACKET_SIZE + BTINTRF_PKHDR_LEN;
+	}
+	else
+	{
+		pIntrf->PacketSize = pCfg->PacketSize + BTINTRF_PKHDR_LEN;
+	}
+
+	if (pCfg->pRxFifoMem == NULL || pCfg->pTxFifoMem == NULL)
+	{
+		pIntrf->hRxFifo = CFifoInit(s_BtDevIntrfRxFifoMem, BTINTRF_CFIFO_SIZE, pIntrf->PacketSize, pCfg->bBlocking);
+		pIntrf->hTxFifo = CFifoInit(s_BtDevIntrfRxFifoMem, BTINTRF_CFIFO_SIZE, pIntrf->PacketSize, pCfg->bBlocking);
+	}
+	else
+	{
+		pIntrf->hRxFifo = CFifoInit(pCfg->pRxFifoMem, pCfg->RxFifoMemSize, pIntrf->PacketSize, pCfg->bBlocking);
+		pIntrf->hTxFifo = CFifoInit(pCfg->pTxFifoMem, pCfg->TxFifoMemSize, pIntrf->PacketSize, pCfg->bBlocking);
+	}
+
+	pIntrf->DevIntrf.pDevData = (void*)pIntrf;
+	pIntrf->pSrvc = pCfg->pSrvc;
+	pIntrf->pSrvc->pContext = pIntrf;
+
+	pIntrf->RxCharIdx = pCfg->RxCharIdx;
+	pIntrf->TxCharIdx = pCfg->TxCharIdx;
+
+	pIntrf->pSrvc->pCharArray[pIntrf->RxCharIdx].WrCB = BtIntrfRxWrCB;
+	pIntrf->pSrvc->pCharArray[pIntrf->TxCharIdx].TxCompleteCB = BtIntrfTxComplete;
+	//pIntrf->pSrvc->pCharArray[pIntrf->RxCharIdx].pSrvc = pIntrf->pSrvc;
+	//pIntrf->pSrvc->pCharArray[pIntrf->TxCharIdx].pSrvc = pIntrf->pSrvc;
+
+	pIntrf->DevIntrf.Type = DEVINTRF_TYPE_BLE;
+	pIntrf->DevIntrf.Enable = BtIntrfEnable;
+	pIntrf->DevIntrf.Disable = BtIntrfDisable;
+	pIntrf->DevIntrf.GetRate = BtIntrfGetRate;
+	pIntrf->DevIntrf.SetRate = BtIntrfSetRate;
+	pIntrf->DevIntrf.StartRx = BtIntrfStartRx;
+	pIntrf->DevIntrf.RxData = BtIntrfRxData;
+	pIntrf->DevIntrf.StopRx = BtIntrfStopRx;
+	pIntrf->DevIntrf.StartTx = BtIntrfStartTx;
+	pIntrf->DevIntrf.TxData = BtIntrfTxData;
+	pIntrf->DevIntrf.StopTx = BtIntrfStopTx;
+	pIntrf->DevIntrf.MaxRetry = 0;
+	pIntrf->DevIntrf.EvtCB = pCfg->EvtCB;
+	pIntrf->TransBuffLen = 0;
+	pIntrf->RxDropCnt = 0;
+	pIntrf->TxDropCnt = 0;
+	atomic_flag_clear(&pIntrf->DevIntrf.bBusy);
+
+	DeviceIntrfEnable(&pIntrf->DevIntrf);
+
+	BtGattUpdate(pIntrf->pSrvc->pCharArray[pIntrf->RxCharIdx].ValHdl,
+				&pIntrf->pSrvc->pCharArray[pIntrf->RxCharIdx], sizeof(BtGattChar_t));
+
+	return true;
+}
+
+bool BtIntrf::Init(const BtIntrfCfg_t &Cfg)
+{
+	return BtIntrfInit(&vBtDevIntrf, &Cfg);
+}
+
+bool BtIntrf::RequestToSend(int NbBytes)
+{
+	bool retval = false;
+
+	// ****
+	// Some threaded application firmware may stop sending when queue full
+	// causing lockup
+	// Try to send to free up the queue before validating.
+	//
+	BtIntrfNotify(&vBtDevIntrf);
+
+	if (vBtDevIntrf.hTxFifo)
+	{
+		int avail = CFifoAvail(vBtDevIntrf.hTxFifo);
+		if ((avail * (vBtDevIntrf.PacketSize - BTINTRF_PKHDR_LEN)) > NbBytes)
+			retval = true;
+	}
+	else
+	{
+		retval = true;
+	}
+
+	return retval;
+}
+
