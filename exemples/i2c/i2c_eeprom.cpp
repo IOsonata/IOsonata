9ef6f5311d10f07ffb607eb42e22b658956a1bef
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2023-01-25 21:07:50 -0500

    eeprom demo

--- exemples/i2c/i2c_eeprom.cpp
+++ exemples/i2c/i2c_eeprom.cpp
@@ -1,12 +1,13 @@
 /**-------------------------------------------------------------------------
-@example	i2c_eeprom.c
+@example	i2c_eeprom.cpp
 
-@brief	I2C EEPROM example
+@brief		I2C EEPROM example
 
 This example shows how to use I2C driver to read/write EEPROM
+Demo showing how to use Eprom with i2c interface in both C and C++
 
-@author	Hoang Nguyen Hoan
-@date	Sep. 19, 2020
+@author		Hoang Nguyen Hoan
+@date		Sep. 19, 2020
 
 @license
 
@@ -44,6 +45,8 @@ SOFTWARE.
 
 #include "board.h"
 
+//#define C_CODE	// Define for C code, comment out for C++ object
+
 static const IOPinCfg_t s_Pins[] = {
 	{I2C_SDA_PORT, I2C_SDA_PIN, I2C_SDA_PINOP, IOPINDIR_BI, IOPINRES_PULLUP, IOPINTYPE_NORMAL},	// SDA
 	{I2C_SCL_PORT, I2C_SCL_PIN, I2C_SCL_PINOP, IOPINDIR_OUTPUT, IOPINRES_PULLUP, IOPINTYPE_NORMAL},	// SCL
@@ -55,7 +58,7 @@ static const I2CCfg_t s_I2cCfg = {
 	.Mode = I2CMODE_MASTER,
 	.pIOPinMap = s_Pins,
 	.NbIOPins = sizeof(s_Pins) / sizeof(IOPinCfg_t),
-	.Rate = 400000,				// Rate
+	.Rate = 100000,				// Rate
 	.MaxRetry = 5,				// Retry
 	.AddrType = I2CADDR_TYPE_NORMAL,
 	.NbSlaveAddr = 0,			// Number of slave addresses
@@ -66,7 +69,11 @@ static const I2CCfg_t s_I2cCfg = {
 	.EvtCB = NULL				// Event callback
 };
 
+#ifdef C_CODE
 I2CDev_t g_I2CDev;
+#else
+I2C g_I2c;
+#endif
 
 static const SeepCfg_t s_SeepCfg = {
 	.DevAddr = 0x50,
@@ -77,7 +84,50 @@ static const SeepCfg_t s_SeepCfg = {
 	.WrProtPin = {-1, -1,},
 };
 
+#ifdef C_CODE
 SeepDev_t g_SeepDev;
+#else
+Seep g_Seep;
+#endif
+
+bool EepromTest()
+{
+	uint8_t x[512], y[512];
+	bool retval = true;
+
+	for (int i = 0; i < 256; i++)
+	{
+		x[i] = i;
+		x[256+i] = 256-i;
+	}
+
+#ifdef C_CODE
+	SeepWrite(&g_SeepDev, 256, x, 512);
+#else
+	g_Seep.Write(0, x, 512);
+#endif
+
+	memset(y, 0xa5, 512);
+
+#ifdef C_CODE
+	SeepRead(&g_SeepDev, 256, y, 512);
+#else
+	g_Seep.Read(0, y, 512);
+#endif
+
+	if (memcmp(x, y, 512) != 0)
+	{
+		printf("Failed\r\n");
+		retval = false;
+	}
+	else
+	{
+		printf("Eeprom test passed.\n");
+		retval = true;
+	}
+
+	return retval;
+}
 
 //
 // Print a greeting message on standard output and exit.
@@ -96,43 +146,15 @@ int main()
 {
 	// Configure
 
+#ifdef C_CODE
 	I2CInit(&g_I2CDev, &s_I2cCfg);
-
 	SeepInit(&g_SeepDev, &s_SeepCfg, &g_I2CDev.DevIntrf);
+#else
+	g_I2c.Init(s_I2cCfg);
+	g_Seep.Init(s_SeepCfg, &g_I2c);
+#endif
 
-	uint8_t x[512];
-	uint8_t y[512];
-
-
-	// Filling test pattern
-	for (int i = 0; i < 256; i++)
-	{
-		x[i] = 255 - i;
-		x[i + 256] = i;
-	}
-
-	SeepWrite(&g_SeepDev, 0, x, 512);
-
-	memset(y, 0, 512);
-
-	SeepRead(&g_SeepDev, 0, y, 512);
-
-	if (memcmp(x, y, 512) == 0)
-	{
-		printf("Success\n");
-	}
-	else
-	{
-		for (int i = 0; i < 512; i++)
-		{
-			if (x[i] != y[i])
-			{
-				printf("Bad %d : %x %x\r\n", i, x[i], y[i]);
-			}
-		}
-	}
-	memset(y, 0, 512);
-	SeepWrite(&g_SeepDev, 0, y, 512);
+	EepromTest();
 
 	while (1);
 
