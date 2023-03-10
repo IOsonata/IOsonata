e550fef079c81b70160a29b583e19d037ea1ca82
JosephThinhTran <tranthinh1989@gmail.com>
2023-03-10 17:53:39 -0500

    Change I2C nrfx driver for clock stretching

--- ARM/Nordic/src/i2c_nrfx.cpp
+++ ARM/Nordic/src/i2c_nrfx.cpp
@@ -373,7 +373,7 @@ int nRFxI2CRxDataDMA(DevIntrf_t * const pDev, uint8_t *pBuff, int BuffLen)
 		dev->pDmaReg->TASKS_RESUME = 1;
 		dev->pDmaReg->TASKS_STARTRX = 1;
 
-		if (nRFxI2CWaitRxComplete(dev, 1000000) == false)
+		if (nRFxI2CWaitRxComplete(dev, 100000) == false)
 		    break;
 
 		BuffLen -= l;
@@ -548,6 +548,32 @@ void nRFxI2CStopTx(DevIntrf_t * const pDev)
     nRFxI2CWaitStop(dev, 1000);
 }
 
+void nRFxI2CSuspend(DevIntrf_t * const pDev)
+{
+	nRFTwiDev_t *dev = (nRFTwiDev_t*)pDev->pDevData;
+
+	if (dev->pI2cDev->Cfg.Mode == I2CMODE_SLAVE)
+	{
+#ifdef TWIS_PRESENT
+		dev->pDmaSReg->TASKS_SUSPEND = 1;
+		dev->pDmaSReg->EVENTS_READ = 0;
+#endif
+	}
+}
+
+void nRFxI2CResume(DevIntrf_t *const pDev)
+{
+	nRFTwiDev_t *dev = (nRFTwiDev_t*) pDev->pDevData;
+	if (dev->pI2cDev->Cfg.Mode == I2CMODE_SLAVE)
+	{
+#ifdef TWIS_PRESENT
+		dev->pDmaSReg->TASKS_PREPARETX = 1;
+		dev->pDmaSReg->TASKS_RESUME = 1;
+
+#endif
+	}
+}
+
 void nRFxI2CReset(DevIntrf_t * const pDev)
 {
 	I2CDev_t *dev = (I2CDev_t*)((nRFTwiDev_t*)pDev->pDevData)->pI2cDev;
@@ -595,23 +621,25 @@ void I2C_IRQHandler(int DevNo, DevIntrf_t * const pDev)
     if (dev->pI2cDev->Cfg.Mode == I2CMODE_SLAVE)
     {
     	// Slave mode
-#ifdef TWIM_PRESENT
+#ifdef TWIS_PRESENT
     	if (dev->pDmaSReg->EVENTS_READ)
     	{
     		// Read command received
-
     		if (dev->pI2cDev->DevIntrf.EvtCB)
     		{
     			int len = dev->pDmaSReg->EVENTS_RXSTARTED ? dev->pDmaSReg->RXD.AMOUNT : 0;
 
     			dev->pI2cDev->DevIntrf.EvtCB(&dev->pI2cDev->DevIntrf, DEVINTRF_EVT_READ_RQST, NULL, len);
     		}
+
     		dev->pDmaSReg->EVENTS_RXSTARTED = 0;
     		dev->pDmaSReg->EVENTS_READ = 0;
-    		dev->pDmaSReg->TXD.PTR = (uint32_t)dev->pI2cDev->pRRData[dev->pDmaSReg->MATCH];
+
+    		dev->pDmaSReg->TXD.PTR = (uint32_t) dev->pI2cDev->pRRData[dev->pDmaSReg->MATCH];
     		dev->pDmaSReg->TXD.MAXCNT = dev->pI2cDev->RRDataLen[dev->pDmaSReg->MATCH] & 0xFF;
-    		dev->pDmaSReg->TASKS_PREPARETX = 1;
-    		dev->pDmaSReg->TASKS_RESUME = 1;
+
+    		dev->pDmaSReg->TASKS_PREPARETX = dev->pI2cDev->Cfg.bClkStretch ? 0 : 1;
+    		dev->pDmaSReg->TASKS_RESUME = dev->pI2cDev->Cfg.bClkStretch ? 0 : 1;
     	}
 
     	if (dev->pDmaSReg->EVENTS_WRITE)
@@ -625,7 +653,7 @@ void I2C_IRQHandler(int DevNo, DevIntrf_t * const pDev)
     		dev->pDmaSReg->EVENTS_WRITE = 0;
     		dev->pDmaSReg->RXD.PTR = (uint32_t)dev->pI2cDev->pTRBuff[dev->pDmaSReg->MATCH];
     		dev->pDmaSReg->RXD.MAXCNT = dev->pI2cDev->TRBuffLen[dev->pDmaSReg->MATCH];
-    		dev->pDmaSReg->SHORTS = 0;
+//    		dev->pDmaSReg->SHORTS = 0;
     		dev->pDmaSReg->TASKS_PREPARERX = 1;
     		dev->pDmaSReg->TASKS_RESUME = 1;
     	}
@@ -654,6 +682,14 @@ void I2C_IRQHandler(int DevNo, DevIntrf_t * const pDev)
     	{
     		dev->pDmaSReg->EVENTS_ERROR = 0;
     	}
+
+    	if (dev->pDmaSReg->EVENTS_TXSTARTED)
+    	{
+    		int x = 0;//dummy for testing
+    		//dev->pDmaSReg->EVENTS_TXSTARTED = 0;
+    	}
+
+
 #endif
     }
     else
@@ -792,6 +828,9 @@ bool I2CInit(I2CDev_t * const pDev, const I2CCfg_t *pCfgData)
         	}
         }
 
+        pDev->DevIntrf.Resume = nRFxI2CResume;
+        pDev->DevIntrf.Suspend = nRFxI2CSuspend;
+
 		sreg->SHORTS = TWIS_SHORTS_READ_SUSPEND_Msk | TWIS_SHORTS_WRITE_SUSPEND_Msk;
         sreg->EVENTS_READ = 0;
         sreg->EVENTS_WRITE = 0;
@@ -901,3 +940,4 @@ bool I2CInit(I2CDev_t * const pDev, const I2CCfg_t *pCfgData)
 
 	return true;
 }
+
--- include/coredev/i2c.h
+++ include/coredev/i2c.h
@@ -239,6 +239,9 @@ static inline int I2CTxData(I2CDev_t * const pDev, uint8_t *pData, int Datalen) 
 static inline void I2CStopTx(I2CDev_t * const pDev) { DeviceIntrfStopTx(&pDev->DevIntrf); }
 static inline I2CDev_t *I2CGetDevHandle(DevIntrf_t * const pDevIntrf) { return (I2CDev_t *)DeviceIntrfGetHandle(pDevIntrf); }
 
+static inline void I2CSuspend(I2CDev_t * const pDev) { DeviceIntrfSuspend(&pDev->DevIntrf); }
+static inline void I2CResume(I2CDev_t * const pDev) { DeviceIntrfResume(&pDev->DevIntrf); }
+
 /**
  * @brief	Set I2C slave data for read command.
  *
@@ -310,6 +313,13 @@ public:
 	}
 	virtual void StopTx(void) { DeviceIntrfStopTx(&vDevData.DevIntrf); }
 	
+	// Suspend the operation of I2C interface due by triggering clock stretching
+	virtual void Suspend(void) {DeviceIntrfSuspend(&vDevData.DevIntrf); }
+
+	// Resume the operation of I2C interface after clock stretching
+	virtual void Resume(void) {DeviceIntrfResume(&vDevData.DevIntrf); }
+
+
 	/**
 	 * @brief	Set I2C slave data for read command.
 	 *
