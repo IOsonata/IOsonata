4b858a16c6069ac6eeabc4f425b8d5fea01f8390
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2024-08-30 15:13:26 -0400

    Add sensor group

--- include/sensors/mag_bmm350.h
+++ include/sensors/mag_bmm350.h
@@ -38,6 +38,10 @@ SOFTWARE.
 #include <stdint.h>
 #include "sensors/mag_sensor.h"
 
+/** @addtogroup Sensors
+  * @{
+  */
+
 #define BMM350_I2C_7BITS_DEVADDR							0x14
 #define BMM350_I2C_7BITS_DEVADDR2							0x15	// When ADSEL = high
 
@@ -119,27 +123,26 @@ SOFTWARE.
 #define BMM350_INT_STATUS_REG			0x30
 #define BMM350_INT_STATUS_DRDY								(1<<2)	//!< Interrupt status data ready
 
-#define BMM350_MAG_X_XLSB_REG				0x31
-#define BMM350_MAG_X_LSB_REG				0x32
-#define BMM350_MAG_X_MSB_REG				0x33
+#define BMM350_MAG_X_XLSB_REG			0x31
+#define BMM350_MAG_X_LSB_REG			0x32
+#define BMM350_MAG_X_MSB_REG			0x33
 
-#define BMM350_MAG_Y_XLSB_REG				0x34
-#define BMM350_MAG_Y_LSB_REG				0x35
-#define BMM350_MAG_Y_MSB_REG				0x36
+#define BMM350_MAG_Y_XLSB_REG			0x34
+#define BMM350_MAG_Y_LSB_REG			0x35
+#define BMM350_MAG_Y_MSB_REG			0x36
+#define BMM350_MAG_Z_XLSB_REG			0x37
+#define BMM350_MAG_Z_LSB_REG			0x38
+#define BMM350_MAG_Z_MSB_REG			0x39
 
-#define BMM350_MAG_Z_XLSB_REG				0x37
-#define BMM350_MAG_Z_LSB_REG				0x38
-#define BMM350_MAG_Z_MSB_REG				0x39
+#define BMM350_TEMP_XLSB_REG			0x3A
+#define BMM350_TEMP_LSB_REG				0x3B
+#define BMM350_TEMP_MSB_REG				0x3C
 
-#define BMM350_TEMP_XLSB_REG				0x3A
-#define BMM350_TEMP_LSB_REG					0x3B
-#define BMM350_TEMP_MSB_REG					0x3C
+#define BMM350_SENSORTIME_XLSB_REG		0x3D
+#define BMM350_SENSORTIME_LSB_REG		0x3E
+#define BMM350_SENSORTIME_MSB_REG		0x3F
 
-#define BMM350_SENSORTIME_XLSB_REG			0x3D
-#define BMM350_SENSORTIME_LSB_REG			0x3E
-#define BMM350_SENSORTIME_MSB_REG			0x3F
-
-#define BMM350_OTP_CMD_REG					0x50
+#define BMM350_OTP_CMD_REG				0x50
 #define BMM350_OTP_CMD_WORD_ADDR_MASK						(0x1F<<0)
 #define BMM350_OTP_CMD_MASK									(0x7<<5)
 #define BMM350_OTP_CMD_DIR_READ								(1<<5)
@@ -149,10 +152,10 @@ SOFTWARE.
 #define BMM350_OTP_CMD_EXT_READ								(5<<5)
 #define BMM350_OTP_CMD_EXT_PRGM								(6<<5)
 
-#define BMM350_OTP_DATA_MSB_REG				0x52
-#define BMM350_OTP_DATA_LSB_REG				0x53
+#define BMM350_OTP_DATA_MSB_REG			0x52
+#define BMM350_OTP_DATA_LSB_REG			0x53
 
-#define BMM350_OTP_STATUS_REG				0x55
+#define BMM350_OTP_STATUS_REG			0x55
 #define BMM350_OTP_STATUS_CMD_DONE							(1<<0)	//!< Command done flag
 #define BMM350_OTP_STATUS_CUR_PAGE_ADDR_MASK				(0xF<<1)
 #define BMM350_OTP_STATUS_ERR_MASK							(7<<5)
@@ -163,7 +166,7 @@ SOFTWARE.
 #define BMM350_OTP_STATUS_ERR_SIGN							(4<<5)
 #define BMM350_OTP_STATUS_ERR_INV_CMD						(5<<5)
 
-#define BMM350_TMR_SELTEST_USER_REG			0x60
+#define BMM350_TMR_SELTEST_USER_REG		0x60
 #define BMM350_TMR_SELTEST_USER_ST_IGEN_EN					(1<<0)	//!< Enable selftest internal current gen
 #define BMM350_TMR_SELTEST_USER_ST_N						(1<<1)	//!< Configure execution of neg field self test
 #define BMM350_TMR_SELTEST_USER_ST_P						(1<<2)	//!< Configure execution of positive field self test
@@ -171,15 +174,15 @@ SOFTWARE.
 #define BMM350_TMR_SELTEST_USER_IST_EN_Y					(1<<3)	//!< Activate internally generated self test field Y
 #define BMM350_TMR_SELTEST_USER_IST_EN_Z					(1<<3)	//!< Activate internally generated self test field Z
 
-#define BMM350_CTRL_USER_REG				0x61
+#define BMM350_CTRL_USER_REG			0x61
 #define BMM350_CTRL_USER_CFG_SENS_TIM_AON					(1<<0)	//!< Force sensor timer always running, even in suspend mode
 
-#define BMM350_CMD_REG						0x7E
+#define BMM350_CMD_REG					0x7E
 #define BMM350_CMD_SOFTRESET								(0xB6)
 
-#define BMM350_ADC_RANGE			((1<<23) - 1)	// 24 bits
+#define BMM350_ADC_RANGE				((1<<23) - 1)	// 24 bits
 
-#define BMM350_FLUX_DENSITY			2000000 //!< max flux density in nT (2000 uT)
+#define BMM350_FLUX_DENSITY				2000000 //!< max flux density in nT (2000 uT)
 
 
 #ifdef __cplusplus
@@ -195,10 +198,13 @@ public:
 	virtual bool UpdateData();
 };
 
+extern "C" {
 #endif // __cplusplus
 
+#ifdef __cplusplus
+}
+#endif // __cplusplus
 
-
-
+/** @} End of group Sensors */
 
 #endif	// __MAG_BMM350_H__
