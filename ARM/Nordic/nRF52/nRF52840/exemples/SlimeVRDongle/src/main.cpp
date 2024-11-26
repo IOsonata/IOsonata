a5e9376e4dced332228d4ea06bb9775dedf8d8b8
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2024-11-26 00:06:25 -0500

    Add FDS

--- ARM/Nordic/nRF52/nRF52840/exemples/SlimeVRDongle/src/main.cpp
+++ ARM/Nordic/nRF52/nRF52840/exemples/SlimeVRDongle/src/main.cpp
@@ -16,6 +16,7 @@
 #include "sdk_common.h"
 #include "nrf.h"
 #include "nrf_esb.h"
+#include "fds.h"
 
 #include "istddef.h"
 #include "idelay.h"
@@ -27,6 +28,37 @@
 #include "SlimeVRDongle.h"
 #include "board.h"
 #include "stddev.h"
+
+#define FIRMWARE_VERSION	0
+#define DEVICE_NAME			"SlimeVRDongle"
+
+#define FDS_DATA_FILE     (0xF010)
+#define FDS_DATA_REC_KEY  (0x7010)
+
+typedef struct __App_Data {
+	uint8_t NbTracker;
+	uint64_t TrackerAddr[MAX_TRACKERS];
+} AppData_t;
+
+__attribute__ ((section(".Version"), used))
+const AppInfo_t g_AppInfo = {
+	DEVICE_NAME, {FIRMWARE_VERSION, 0, BUILDN},
+	{'I', 'O', 's', 'o', 'n', 'a', 't', 'a', 'S', 'l', 'i', 0x55, 0xA5, 0x5A, 0xA5, 0x5A},
+};
+
+AppData_t g_AppData = { (uint8_t)-1, };
+
+alignas(4) static fds_record_t const g_AppDataRecord =
+{
+    .file_id           = FDS_DATA_FILE,
+    .key               = FDS_DATA_REC_KEY,
+    .data = {
+		.p_data = &g_AppData,
+    /* The length of a record is always expressed in 4-byte units (words). */
+		.length_words = (sizeof(AppData_t) + 3) >> 2,
+    }
+};
+volatile bool g_FdsInitialized = false;
 
 int nRFUartEvthandler(UARTDev_t *pDev, UART_EVT EvtId, uint8_t *pBuffer, int BufferLen);
 
@@ -55,28 +87,71 @@ const UARTCfg_t g_UartCfg = {
 // UART object instance
 UART g_Uart;
 
-uint8_t stored_trackers = 0;
-uint64_t stored_tracker_addr[MAX_TRACKERS] = {0};
+static void fds_evt_handler(fds_evt_t const * p_evt)
+{
+    switch (p_evt->id)
+    {
+        case FDS_EVT_INIT:
+            if (p_evt->result == NRF_SUCCESS)
+            {
+            	g_FdsInitialized = true;
+            }
+            break;
+
+        case FDS_EVT_WRITE:
+        {
+            if (p_evt->result == NRF_SUCCESS)
+            {
+            }
+        } break;
+
+        case FDS_EVT_DEL_RECORD:
+        {
+            if (p_evt->result == NRF_SUCCESS)
+            {
+            }
+        } break;
+
+        default:
+            break;
+    }
+}
+
+void UpdateRecord()
+{
+    fds_record_desc_t desc = {0};
+    fds_find_token_t  tok  = {0};
+
+    uint32_t rc = fds_record_find(FDS_DATA_FILE, FDS_DATA_REC_KEY, &desc, &tok);
+
+    if (rc == NRF_SUCCESS)
+    {
+    	rc = fds_record_update(&desc, &g_AppDataRecord);
+    	APP_ERROR_CHECK(rc);
+    }
+}
 
 uint16_t AddTracker(uint64_t Addr)
 {
-	uint16_t retid = stored_trackers;
+	uint16_t retid = g_AppData.NbTracker;
 
-	for (int i = 0; i < stored_trackers; i++) // Check if the device is already stored
+	for (int i = 0; i < g_AppData.NbTracker; i++) // Check if the device is already stored
 	{
-		if (Addr != 0 && stored_tracker_addr[i] == Addr)
+		if (Addr != 0 && g_AppData.TrackerAddr[i] == Addr)
 		{
 			//LOG_INF("Found device linked to id %d with address %012llX", i, found_addr);
 			return i;
 		}
 	}
 
-	if (stored_trackers < MAX_TRACKERS)
+	if (g_AppData.NbTracker < MAX_TRACKERS)
 	{
-		stored_tracker_addr[stored_trackers] = Addr;
+		g_AppData.TrackerAddr[g_AppData.NbTracker] = Addr;
 
-		stored_trackers++;
+		g_AppData.NbTracker++;
 	}
+
+	UpdateRecord();
 
 	return retid;
 }
@@ -176,11 +251,67 @@ int nRFUartEvthandler(UARTDev_t *pDev, UART_EVT EvtId, uint8_t *pBuffer, int Buf
 void HardwareInit()
 {
 	g_Uart.Init(g_UartCfg);
-	//UARTRetargetEnable(g_Uart, STDOUT_FILENO);
-
 	IOPinConfig(BUT_PORT, BUT_PIN, BUT_PINOP, IOPINDIR_OUTPUT, IOPINRES_NONE, IOPINTYPE_NORMAL);
 
 	printf("UART BLE Central Demo\r\n");
+
+    (void) fds_register(fds_evt_handler);
+    uint32_t rc = fds_init();
+    APP_ERROR_CHECK(rc);
+
+    while (g_FdsInitialized != true)
+    {
+        __WFE();
+    }
+
+    fds_record_desc_t desc = {0};
+    fds_find_token_t  tok  = {0};
+
+    rc = fds_record_find(FDS_DATA_FILE, FDS_DATA_REC_KEY, &desc, &tok);
+
+    if (rc == NRF_SUCCESS)
+    {
+    	AppData_t data;
+
+    	do {
+			/* A config file is in flash. Let's update it. */
+			fds_flash_record_t appdata = {0};
+
+			/* Open the record and read its contents. */
+			rc = fds_record_open(&desc, &appdata);
+			APP_ERROR_CHECK(rc);
+
+			memcpy(&data, appdata.p_data, sizeof(AppData_t));
+			/* Close the record when done reading. */
+			rc = fds_record_close(&desc);
+			APP_ERROR_CHECK(rc);
+
+			rc = fds_record_find(FDS_DATA_FILE, FDS_DATA_REC_KEY, &desc, &tok);
+    	} while (rc == NRF_SUCCESS);
+
+    	uint8_t *p = (uint8_t *)&data;
+		uint8_t cs = 0;
+
+		for (int i = 0; i < sizeof(AppData_t); i++, p++)
+		{
+			cs += *p;
+		}
+
+		if (cs == 0)
+		{
+			memcpy(&g_AppData, &data, sizeof(AppData_t));
+		}
+		else
+		{
+			UpdateRecord();
+		}
+
+    }
+    else
+    {
+        rc = fds_record_write(&desc, &g_AppDataRecord);
+        APP_ERROR_CHECK(rc);
+    }
 }
 
 int main(void)
