ab23981b535e34f5f6285ac067d2ae397eb05d90
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2024-11-30 22:52:01 -0500

    nRF net utility functions

---
+++ ARM/Nordic/include/nrf_mac.h
@@ -0,0 +1,46 @@
+/**-------------------------------------------------------------------------
+@file	nrf_mac.h
+
+@brief	Network MAC utility functions specific to Nordic nRF series
+
+
+@author	Hoang Nguyen Hoan
+@date	Nov. 29, 2024
+
+@license
+
+MIT License
+
+Copyright (c) 2024, I-SYST inc., all rights reserved
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+----------------------------------------------------------------------------*/
+#ifndef __NRF_MAC_H__
+#define __NRF_MAC_H__
+
+#include <inttypes.h>
+
+#include "nrf.h"
+
+static inline uint64_t nrf_get_mac_address() {
+	return (*(uint64_t *)NRF_FICR->DEVICEADDR & 0xFFFFFFFFFFFFUL) | 0xC00000000000UL;
+};
+
+#endif // __NRF_MAC_H__
