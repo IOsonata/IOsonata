257b8bc735fd64eb7be3d044d8acd8dbddcccb28
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2024-12-24 00:43:26 -0500

    missing .h

---
+++ ARM/Nordic/include/nrf_mpsl.h
@@ -0,0 +1,50 @@
+/**-------------------------------------------------------------------------
+@file	nrf_mpsl.h
+
+@brief	MPSL configuration
+
+MPSL configuration.
+
+@author	Nguyen Hoan Hoang
+@date	Dec. 21, 2024
+
+@license
+
+MIT License
+
+Copyright (c) 2024 I-SYST inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Softare.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+----------------------------------------------------------------------------*/
+#ifndef __NRF_MPSL_H__
+#define __NRF_MPSL_H__
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+bool nrf_mpsl_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	// __NRF_MPSL_H__
