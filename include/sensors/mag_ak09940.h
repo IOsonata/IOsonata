cb8cf964c31d8fc66285227f20893af74b821fe4
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2025-05-03 19:27:54 -0400

    Preliminary AK09940 driver

---
+++ include/sensors/mag_ak09940.h
@@ -0,0 +1,167 @@
+/**-------------------------------------------------------------------------
+@file	mag_ak09940.h
+
+@brief	Implementation of Asahi Kasei AK09940x mag sensor
+
+
+@author	Hoang Nguyen Hoan
+@date	May 3, 2025
+
+@license
+
+MIT License
+
+Copyright (c) 2025 I-SYST inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+----------------------------------------------------------------------------*/
+
+#ifndef __MAG_AK09940_H__
+#define __MAG_AK09940_H__
+
+#include <stdint.h>
+
+#include "coredev/iopincfg.h"
+#include "sensors/mag_sensor.h"
+
+/** @addtogroup Sensors
+  * @{
+  */
+
+#define AK09940_I2C_7BITS_DEVADDR0             0xC
+#define AK09940_I2C_7BITS_DEVADDR1             0xD
+#define AK09940_I2C_7BITS_DEVADDR2             0xE
+#define AK09940_I2C_7BITS_DEVADDR3             0xF
+
+#define AK09940_WIA1_REG    0
+#define AK09940_WIA1_COMPANY_ID               0x48
+
+#define AK09940_WIA2_REG    1
+#define AK09940_WIA2_DEVICE_ID                0xA3
+
+#define AK09940_COMPANY_DEVICE_ID             0xA348   // Combine company & device id
+
+#define AK09940_ST_REG			0x0F  //!< Status register
+#define AK09940_ST_DRDY								(1<<0)	//!< Data ready
+#define AK09940_ST_DOR                   			(1<<1)	//!< Data overrun
+
+#define AK09940_ST1_REG			0x10  //!< Status register 1
+#define AK09940_ST1_DRDY							(1<<0)	//!< Data ready
+#define AK09940_ST1_FNUM_MASK						(0xF<<1)	//!< Fifo status mask, count of fifo set
+
+#define AK09940_HXL_REG			0x11	//!< X data bit 0-7
+#define AK09940_HXM_REG			0x12	//!< X data bit 8-15
+#define AK09940_HXH_REG			0x13	//!< X data bit 16-17
+#define AK09940_HYL_REG			0x14
+#define AK09940_HYM_REG			0x15
+#define AK09940_HYH_REG			0x16
+#define AK09940_HZL_REG			0x17
+#define AK09940_HZM_REG			0x18
+#define AK09940_HZH_REG			0x19
+
+#define AK09940_TMPS_REG		0x1A	//!< Temperature data. T (C) = 30 - TMPS / 1.7
+
+#define AK09940_ST2_REG			0x1B  	//!< Status register 2
+#define AK09940_ST2_DOR								(1<<0)	//!< Data overrun
+#define AK09940_ST2_INV								(1<<1)	//!< Invalid data
+
+// Self test data
+#define AK09940_SXL_REG			0x20	//!< X data bits 0-7
+#define AK09940_SXH_REG			0x21	//!< X data bits 8-10
+#define AK09940_SYL_REG			0x22
+#define AK09940_SYH_REG			0x23
+#define AK09940_SZL_REG			0x24
+#define AK09940_SZH_REG			0x25
+
+#define AK09940_CTRL1_REG		0x30
+#define AK09940_CTRL1_WM_MASK						(7<<0)	//!< Watermark mask
+#define AK09940_CTRL1_DTSET							(1<<5)	//!< 0 - DRDY pin output. 1 - TRG pin : ext trigger pulse input
+#define AK09940_CTRL1_MT2_EN						(1<<7)	//!< Ultralow power
+
+#define AK09940_CTRL2_REG		0x31
+#define AK09940_CTRL2_TEM_EN						(1<<6)	//!< Enable temperature measurement
+
+#define AK09940_CTRL3_REG		0x32
+#define AK09940_CTRL3_MODE_MASK						(0x1F<<0)	//!< Operating mode mask
+#define AK09940_CTRL3_MODE_PWRDWN					(0<<0)	//!< Powerdown
+#define AK09940_CTRL3_MODE_SINGLE					(1<<0)	//!< Single measurement mode
+#define AK09940_CTRL3_MODE_CONTINUOUS_10HZ			(2<<0)	//!< Continuous measurement mode 10 Hz
+#define AK09940_CTRL3_MODE_CONTINUOUS_20HZ			(4<<0)	//!< Continuous measurement mode 20 Hz
+#define AK09940_CTRL3_MODE_CONTINUOUS_50HZ			(6<<0)	//!< Continuous measurement mode 50 Hz
+#define AK09940_CTRL3_MODE_CONTINUOUS_100HZ			(8<<0)	//!< Continuous measurement mode 100 Hz
+#define AK09940_CTRL3_MODE_CONTINUOUS_200HZ			(10<<0)	//!< Continuous measurement mode 200 Hz
+#define AK09940_CTRL3_MODE_CONTINUOUS_400HZ			(12<<0)	//!< Continuous measurement mode 400 Hz
+#define AK09940_CTRL3_MODE_CONTINUOUS_1000HZ		(14<<0)	//!< Continuous measurement mode 1 KHz
+#define AK09940_CTRL3_MODE_CONTINUOUS_2500HZ		(15<<0)	//!< Continuous measurement mode 2.5 KHz
+#define AK09940_CTRL3_MODE_EXTTRIG					(0x18<<0)	//!< External trigger measurement mode. Must have AK09940_CTRL1_DTSET set
+#define AK09940_CTRL3_MODE_SELFTEST					(10<<0)	//!< Self test mode
+#define AK09940_CTRL3_MT_MASK						(3<<5)	//!< Sensor drive mask
+#define AK09940_CTRL3_MT_LP1						(0<<5)	//!< Low power drive 1
+#define AK09940_CTRL3_MT_LP2						(1<<5)	//!< Low power drive 2
+#define AK09940_CTRL3_MT_LN1						(2<<5)	//!< Low noise drive 1
+#define AK09940_CTRL3_MT_LN2						(3<<5)	//!< Low noise drive 2
+#define AK09940_CTRL3_FIFO_EN						(1<<7)	//!< FIFO enable
+
+#define AK09940_CTRL4_REG		0x33
+#define AK09940_CTRL4_SRST							(1<<0)	//!< Soft reset
+
+#define AK09940_I2CDIS_REG		0x36
+#define AK09940_I2CDIS_DISABLE						(0x1B)	//!< Write this value to disable I2C. Once disable, reset is required to re-enable I2C
+
+
+#define AK09940_ADC_RANGE			131070
+#define AK09940_FLUX_DENSITY        1310700	//!< Max flux density in nT
+#define AK09940_SENSITIVITY			10	//!< Sensitivity in nT
+
+#pragma pack(push, 1)
+
+
+#pragma pack(pop)
+
+#ifdef __cplusplus
+
+class MagAk09940 : public MagSensor {
+public:
+  virtual bool Init(const MagSensorCfg_t &Cfg, DeviceIntrf * const pIntrf, Timer * const pTimer = NULL);
+  virtual uint32_t SamplingFrequency(uint32_t Freq);
+
+  /**
+   * @brief Power off the device completely.
+   *
+   * If supported, this will put the device in complete power down.
+   * Full re-initialization is required to re-enable the device.
+   */
+  virtual void PowerOff();
+  virtual bool Enable();
+  virtual void Disable();
+  virtual void Reset();
+  virtual bool UpdateData();
+
+private:
+  virtual bool StartSampling(void) { return false; };
+};
+
+
+#endif // __cplusplus
+
+/** @} End of group Sensors */
+
+#endif // __AG_AK09940_H__
+
---
+++ src/sensors/mag_ak09940.cpp
@@ -0,0 +1,211 @@
+/**-------------------------------------------------------------------------
+@file	mag_ak09940.cpp
+
+@brief	Implementation of Asahi Kasei AK09940x mag sensor
+
+
+@author	Hoang Nguyen Hoan
+@date	May 3, 2025
+
+@license
+
+MIT License
+
+Copyright (c) 2025 I-SYST inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+----------------------------------------------------------------------------*/
+
+#include "sensors/mag_ak09940.h"
+
+bool MagAk09940::Init(const MagSensorCfg_t &Cfg, DeviceIntrf * const pIntrf, Timer * const pTimer)
+{
+	uint8_t regaddr = AK09940_WIA1_REG;
+	uint16_t d = 0;
+
+	MagSensor::Type(SENSOR_TYPE_MAG);
+#if 1
+	Interface(pIntrf);
+
+	if (pIntrf->Type() == DEVINTRF_TYPE_SPI)
+	{
+		DeviceAddress(Cfg.DevAddr);
+	}
+	else
+	{
+		DeviceAddress(AK09940_I2C_7BITS_DEVADDR0);
+	}
+
+	if (pTimer != NULL)
+	{
+		vpTimer = pTimer;
+	}
+#endif
+	Read(AK09940_I2C_7BITS_DEVADDR0, &regaddr, 1, (uint8_t*)&d, 2);
+
+	if (d != AK09940_COMPANY_DEVICE_ID)
+	{
+		return false;
+	}
+
+	Reset();
+
+	SamplingFrequency(Cfg.Freq);
+
+	Range(AK09940_ADC_RANGE);
+
+	vSensitivity[0] = AK09940_SENSITIVITY;
+	vSensitivity[1] = vSensitivity[0];
+	vSensitivity[2] = vSensitivity[0];
+
+	vData.Sensitivity[0] = vSensitivity[0];
+	vData.Sensitivity[1] = vSensitivity[1];
+	vData.Sensitivity[2] = vSensitivity[2];
+
+	ClearCalibration();
+
+	return true;
+}
+
+uint32_t MagAk09940::SamplingFrequency(uint32_t Freq)
+{
+	uint8_t regaddr = AK09940_CTRL3_REG;
+	uint8_t d = 0;
+
+	Read(AK09940_I2C_7BITS_DEVADDR0, &regaddr, 1, &d, 1);
+
+	d &= AK09940_CTRL3_MODE_MASK;
+
+	Write(AK09940_I2C_7BITS_DEVADDR0, &regaddr, 1, &d, 1);
+
+	if (Freq < 20000)
+	{
+		d |= AK09940_CTRL3_MODE_CONTINUOUS_10HZ;
+		Freq = 10000;
+	}
+	else if (Freq < 50000)
+	{
+		d |= AK09940_CTRL3_MODE_CONTINUOUS_20HZ;
+		Freq = 20000;
+	}
+	else if (Freq < 100000)
+	{
+		d |= AK09940_CTRL3_MODE_CONTINUOUS_50HZ;
+		Freq = 50000;
+	}
+	else if (Freq < 200000)
+	{
+		d |= AK09940_CTRL3_MODE_CONTINUOUS_100HZ;
+		Freq = 100000;
+	}
+	else if (Freq < 400000)
+	{
+		d |= AK09940_CTRL3_MODE_CONTINUOUS_200HZ;
+		Freq = 200000;
+	}
+	else if (Freq < 1000000)
+	{
+		d |= AK09940_CTRL3_MODE_CONTINUOUS_400HZ;
+		Freq = 400000;
+	}
+	else if (Freq < 2500000)
+	{
+		d |= AK09940_CTRL3_MODE_CONTINUOUS_1000HZ;
+		Freq = 1000000;
+	}
+	else
+	{
+		d |= AK09940_CTRL3_MODE_CONTINUOUS_2500HZ;
+		Freq = 2500000;
+	}
+
+	Write(AK09940_I2C_7BITS_DEVADDR0, &regaddr, 1, &d, 1);
+
+	d = 0;
+	Read(AK09940_I2C_7BITS_DEVADDR0, &regaddr, 1, &d, 1);
+
+	return MagSensor::SamplingFrequency(Freq);
+}
+
+/**
+ * @brief	Power off the device completely.
+ *
+ * If supported, this will put the device in complete power down.
+ * Full re-intialization is required to re-enable the device.
+ */
+void MagAk09940::PowerOff()
+{
+	uint8_t regaddr = AK09940_CTRL2_REG;
+	uint8_t d = 0;
+
+	Write(AK09940_I2C_7BITS_DEVADDR0, &regaddr, 1, &d, 1);
+}
+
+bool MagAk09940::Enable()
+{
+	SamplingFrequency(Sensor::vSampFreq);
+
+	return true;
+}
+
+void MagAk09940::Disable()
+{
+	uint8_t regaddr = AK09940_CTRL3_REG;
+	uint8_t d = 0;
+
+	Read(AK09940_I2C_7BITS_DEVADDR0, &regaddr, 1, &d, 1);
+	d &= AK09940_CTRL3_MODE_MASK;
+
+	Write(AK09940_I2C_7BITS_DEVADDR0, &regaddr, 1, &d, 1);
+}
+
+void MagAk09940::Reset()
+{
+	uint8_t regaddr = AK09940_CTRL4_REG;
+	uint8_t d = AK09940_CTRL4_SRST;
+
+	Write(AK09940_I2C_7BITS_DEVADDR0, &regaddr, 1, (uint8_t*)&d, 1);
+}
+
+bool MagAk09940::UpdateData()
+{
+	uint8_t regaddr = AK09940_ST1_REG;
+	uint8_t d;
+
+	Read(AK09940_I2C_7BITS_DEVADDR0, &regaddr, 1, &d, 1);
+
+	if (d & AK09940_ST1_DRDY)
+	{
+		regaddr = AK09940_HXL_REG;
+		Read(AK09940_I2C_7BITS_DEVADDR0, &regaddr, 1, (uint8_t*)vData.Val, 6);
+
+		if (vpTimer)
+		{
+			vData.Timestamp = vpTimer->nSecond() * 1000ULL;
+		}
+
+		regaddr = AK09940_ST2_REG;
+		Read(AK09940_I2C_7BITS_DEVADDR0, &regaddr, 1, &d, 1);
+
+		return true;
+	}
+
+	return false;
+}
