721c2cc19b2799bd08463c132b5953119ace4404
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2025-08-27 15:40:11 -0400

    Preliminary reset implementation on risc-v

--- RISCV/src/ResetEntry.c
+++ RISCV/src/ResetEntry.c
@@ -32,16 +32,12 @@ SOFTWARE.
 
 ----------------------------------------------------------------------------*/
 #include <stdint.h>
+#include <stddef.h>
 #include <string.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <errno.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
-#if defined ( __ARMCC_VERSION )
-extern int Image$$ER_ZI$$Length;
-extern char Image$$ER_ZI$$Base[];
-#else
 extern unsigned long __etext;	// Begin of data in FLASH location
 extern unsigned long __data_loc__;
 extern unsigned long __data_start__;	// RAM data start
@@ -53,193 +49,81 @@ extern unsigned long __bss_size__;
 extern unsigned long __heap_start__;
 extern unsigned long __heap_size__;
 extern unsigned long __heap_end__;
-#endif
+extern uint32_t _stack_top;
 
-#ifdef __ICCARM__
-extern void __iar_init_core( void );
-extern void __iar_init_vfp( void );
-extern void __iar_data_init3(void);
-extern void _call_main(void);
-#endif
-
-extern void _start(void);
-extern void _rtos_start(void);
-extern int main (void);
-extern void __libc_init_array(void);
+/* Forward declarations */
 extern void SystemInit(void);
 extern void SystemCoreClockUpdate(void);
-extern uint32_t SystemCoreClock;
+extern void _start(void);
 
-extern	void (* const __Vectors[])(void);
-
-// Just to make the linker to keep the __Vectors
-__attribute__ ((used)) static uint32_t s_StackPtr = (uint32_t)__Vectors;
-
-// Nop count for usDelay base on ARM NOP instruction timing on 16MHz clock
-uint32_t SystemMicroSecLoopCnt = 1;
-
-/**
- *	This is entry point after reset
- */
-#if defined ( __ARMCC_VERSION )
-__attribute__ ((section (".AppStart")))
-void Reset_Handler (void)
+__attribute__((section(".reset"), used, noreturn))
+void ResetEntry(void)
 {
-	/*
-	 * Clear the ".bss" segment.
-	 */
-	memset(Image$$ER_ZI$$Base, 0, (size_t)&Image$$ER_ZI$$Length);
-#else
-__attribute__ ((section (".AppStart")))
-void ResetEntry (void)
-{
-#ifdef __ICCARM__
-	__iar_init_core();
-	__iar_init_vfp();
-	__iar_data_init3();
-#else
+    uint32_t *src, *dst;
 
-	asm volatile("la gp, global_pointer");
-	asm volatile("csrw satp, zero");
-	asm volatile("la sp, stack_top");
+    /* Initialize stack pointer */
+    asm volatile("mv sp, %0" : : "r"(&_stack_top));
 
   	/*
 	 * Copy the initialized data of the ".data" segment
 	 * from the flash to ram.
 	 */
-
-	if (&__data_start__ != &__data_loc__)
-	{
-		size_t dsize = (size_t)&__data_size__;	// this is a workaround for m0+ compiler in debug build
-		memcpy((void *)&__data_start__, (void *)&__data_loc__, dsize);
-	}
+    src = &__data_loc__;
+    dst = &__data_start__;
+    while (dst < &__data_end__)
+    {
+        *dst++ = *src++;
+    }
 
 	/*
 	 * Clear the ".bss" segment.
 	 */
-	size_t sz = (uint32_t)&__bss_size__; // this is a workaround for m0+ compiler in debug build
-	memset((void *)&__bss_start__, 0, sz);
-#endif
-#endif
-
-	/*
-	 * Core clock initialization using CMSIS
-	 */
-	SystemInit();
-
-	/*
-	 * Call C++ library initialization
-	 */
-//	__libc_init_array();
+    dst = &__bss_start__;
+    while (dst < &__bss_end__)
+    {
+        *dst++ = 0;
+    }
 
 	/*
 	 * Now memory has been initialized
 	 * Update core clock data
 	 */
-	SystemCoreClockUpdate();
+    SystemInit();
+    SystemCoreClockUpdate();
 
-	// Update count for usDelay
-	SystemMicroSecLoopCnt = (SystemCoreClock + 8000000) / 16000000;
+    _start();
 
-	/*
-	 * We are ready to enter main application
-	 */
-#ifndef __CMSIS_RTOS
-	// Bare bone app
-#if defined ( __ARMCC_VERSION )
-	main();
-#else
-#ifdef __ICCARM__
-	   //__iar_program_start();
-	   _call_main();
-#else
-	_start();
-#endif
-#endif
-#else
-	// RTX based app
-	_rtos_start();
-#endif
-
-	/*
-	 * Embedded system don't return to OS.  main() should not normally
-	 * returns.  In case it does, just loop here.
-	 */
-	while(1);
-
+    /* Loop forever if main returns */
+    while(1) { }
 }
 
-#if 1
-
-__attribute__((weak)) void _exit(int Status)
+/* --- Minimal heap (_sbrk) implementation --- */
+__attribute__((weak))
+caddr_t _sbrk(ptrdiff_t incr)
 {
-	while(1);
+    static uint8_t *heap_ptr = NULL;
+    uint8_t *prev_heap;
+
+    if (heap_ptr == NULL)
+        heap_ptr = (uint8_t *)&_heap_start__;
+
+    prev_heap = heap_ptr;
+
+    if ((heap_ptr + incr) > (uint8_t *)&_heap_end__) {
+        errno = ENOMEM;
+        return (caddr_t)-1;
+    }
+
+    heap_ptr += incr;
+    return (caddr_t)prev_heap;
 }
 
-__attribute__((weak)) caddr_t _sbrk(int incr)
-{
-	static uint32_t top = (uint32_t)&__heap_start__;
-	uint32_t heap = top;
-
-	top += incr;
-
-	if (top > (uint32_t)&__heap_end__)
-	{
-		top = heap;
-		errno = ENOMEM;
-
-		return (caddr_t)-1; // error - no more memory
-	}
-
-	return (caddr_t) heap;
-}
-
-__attribute__ ((weak)) int _open(const char * const pPathName, int Flags, int Mode)
-{
-	return -1;
-}
-
-__attribute__ ((weak)) int _close(int Fd)
-{
-	return -1;
-}
-
-__attribute__ ((weak)) int _lseek(int Fd, int Offset)
-{
-	return -1;
-}
-
-
-__attribute__ ((weak)) int _read (int Fd, char *pBuff, size_t Len)
-{
-	return -1;
-}
-
-__attribute__ ((weak)) int _write (int Fd, char *pBuff, size_t Len)
-{
-	return -1;
-}
-
-__attribute__ ((weak)) int _fstat(int file, struct stat *st)
-{
-	st->st_mode = S_IFCHR;
-
-	return 0;
-}
-
-__attribute__ ((weak)) int _isatty(int file)
-{
-	return 1;
-}
-
-__attribute__ ((weak)) void _kill(int pid, int sig)
-{
-	return;
-}
-
-__attribute__ ((weak)) int _getpid(void)
-{
-	return -1;
-}
-
-#endif
+__attribute__((weak)) int _close(int fd)      { return -1; }
+__attribute__((weak)) int _fstat(int fd, struct stat *st) { st->st_mode = S_IFCHR; return 0; }
+__attribute__((weak)) int _isatty(int fd)    { return 1; }
+__attribute__((weak)) int _lseek(int fd, int offset, int whence) { return -1; }
+__attribute__((weak)) int _read(int fd, char *buf, size_t len)    { return -1; }
+__attribute__((weak)) int _write(int fd, char *buf, size_t len)   { return -1; }
+__attribute__((weak)) void _exit(int status) { while(1); }
+__attribute__((weak)) void _kill(int pid, int sig) {}
+__attribute__((weak)) int _getpid(void) { return -1; }
