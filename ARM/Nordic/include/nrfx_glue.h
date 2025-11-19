b63a89117ab0de47a045cfcb747f1d893365602b
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2025-11-18 19:44:49 -0500

    fixed Nordic nrfx 4.0 bug

--- ARM/Nordic/include/nrfx_glue.h
+++ ARM/Nordic/include/nrfx_glue.h
@@ -38,6 +38,11 @@
 extern "C" {
 #endif
 
+// Fixed declaration bug in Nordic nrfx 4.0
+#ifndef NRF_STATIC_INLINE
+#define NRF_STATIC_INLINE __STATIC_INLINE
+#endif
+
 /**
  * @defgroup nrfx_glue nrfx_glue.h
  * @{
@@ -196,12 +201,12 @@ static inline bool NRFX_IRQ_IS_PENDING(IRQn_Type irq_number)
  *
  * @param us_time Number of microseconds to wait.
  */
-#include <soc/nrfx_coredep.h>
+#include <lib/nrfx_coredep.h>
 
 #define NRFX_DELAY_US(us_time)	nrfx_coredep_delay_us(us_time)
 
 //------------------------------------------------------------------------------
-#include <soc/nrfx_atomic.h>
+#include <lib/nrfx_atomic.h>
 
 /** @brief Atomic 32-bit unsigned type. */
 #define nrfx_atomic_t	nrfx_atomic_u32_t
