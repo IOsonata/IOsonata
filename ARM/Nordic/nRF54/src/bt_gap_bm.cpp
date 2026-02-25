427bc09dbfc630736c9e3b75ce6af206e2891bbd
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2026-02-24 23:30:51 -0500

    fixed compile error

--- ARM/Nordic/nRF54/src/bt_gap_bm.cpp
+++ ARM/Nordic/nRF54/src/bt_gap_bm.cpp
@@ -1,12 +1,15 @@
 /**-------------------------------------------------------------------------
 @file	bt_gap_bm.cpp
 
-@brief	Implement Bluetooth Generic Access Profile (GAP) using Nordic baremetal SDK (S145)
+@brief	Implement Bluetooth Generic Access Profile (GAP)
+
+Implementation for Nordic baremetal sdk (sdk-nrf-bm) with SoftDevice S145
+on nRF54L15.
 
 Core Bluetooth Vol.1, Part A, 6.2
 
 @author	Hoang Nguyen Hoan
-@date	Feb. 24, 2026
+@date	Feb. 25, 2026
 
 @license
 
@@ -40,194 +43,200 @@ SOFTWARE.
 #include "nrf_error.h"
 #include "ble.h"
 #include "ble_gap.h"
-#include "ble_srv_common.h"
 
+#include "convutil.h"
 #include "bluetooth/bt_gap.h"
 
-#ifndef UNIT_0_625_MS
-#define UNIT_0_625_MS                          625
-#endif
+#define BT_GAP_CONN_CFG_TAG			CONFIG_NRF_SDH_BLE_CONN_TAG
 
-#ifndef UNIT_1_25_MS
-#define UNIT_1_25_MS                           1250
-#endif
-
-#ifndef UNIT_10_MS
-#define UNIT_10_MS                             10000
-#endif
-
-#ifndef MSEC_TO_UNITS
-#define MSEC_TO_UNITS(MS, UNIT)                (((MS) * 1000) / (UNIT))
-#endif
-
-#if defined(CONFIG_NRF_SDH_BLE_CONN_TAG)
-#define BT_GAP_CONN_CFG_TAG                    CONFIG_NRF_SDH_BLE_CONN_TAG
-#else
-#define BT_GAP_CONN_CFG_TAG                    1
-#endif
-
-static ble_gap_scan_params_t s_ScanParams =
-{
-#if (NRF_SD_BLE_API_VERSION >= 6)
-    .extended = 1,
-    .report_incomplete_evts = 0,
-    .active = 1,
-    .filter_policy = BLE_GAP_SCAN_FP_ACCEPT_ALL,
-    .scan_phys = BLE_GAP_PHY_1MBPS,
-#elif (NRF_SD_BLE_API_VERSION >= 3)
-    .active = 1,
-    .use_whitelist = 0,
-    .interval = MSEC_TO_UNITS(BT_GAP_SCAN_INTERVAL, UNIT_0_625_MS),
-    .window = MSEC_TO_UNITS(BT_GAP_SCAN_WINDOW, UNIT_0_625_MS),
-    .timeout = BT_GAP_SCAN_TIMEOUT,
-    .adv_dir_report = 1,
-#else
-    .active = 1,
-    .selective = 0,
-    .p_whitelist = NULL,
-    .interval = MSEC_TO_UNITS(BT_GAP_SCAN_INTERVAL, UNIT_0_625_MS),
-    .window = MSEC_TO_UNITS(BT_GAP_SCAN_WINDOW, UNIT_0_625_MS),
-    .timeout = BT_GAP_SCAN_TIMEOUT,
-#endif
+static ble_gap_scan_params_t s_ScanParams = {
+	.active = 1,
+	.interval = MSEC_TO_0_625(BT_GAP_SCAN_INTERVAL),
+	.window = MSEC_TO_0_625(BT_GAP_SCAN_WINDOW),
+	.timeout = BT_GAP_SCAN_TIMEOUT,
+	.filter_policy = BLE_GAP_SCAN_FP_ACCEPT_ALL,
+	.scan_phys = BLE_GAP_PHY_1MBPS,
 };
 
 static ble_gap_conn_sec_mode_t s_GapConnMode;
 
+static inline uint16_t mSecTo10Ms(uint32_t t)
+{
+	return (uint16_t)((t + 5U) / 10U);
+}
+
+static void BtGapSetSecMode(ble_gap_conn_sec_mode_t *pSecMode, BTGAP_SECTYPE SecType)
+{
+	switch (SecType)
+	{
+		case BTGAP_SECTYPE_STATICKEY_NO_MITM:
+			BLE_GAP_CONN_SEC_MODE_SET_ENC_NO_MITM(pSecMode);
+			break;
+		case BTGAP_SECTYPE_STATICKEY_MITM:
+			BLE_GAP_CONN_SEC_MODE_SET_ENC_WITH_MITM(pSecMode);
+			break;
+		case BTGAP_SECTYPE_LESC_MITM:
+			BLE_GAP_CONN_SEC_MODE_SET_LESC_ENC_WITH_MITM(pSecMode);
+			break;
+		case BTGAP_SECTYPE_SIGNED_NO_MITM:
+		case BTGAP_SECTYPE_SIGNED_MITM:
+			// Signed-only security mode is not used for GAP device name in this port.
+			BLE_GAP_CONN_SEC_MODE_SET_OPEN(pSecMode);
+			break;
+		case BTGAP_SECTYPE_NONE:
+		default:
+			BLE_GAP_CONN_SEC_MODE_SET_OPEN(pSecMode);
+			break;
+	}
+}
+
 void BtGapParamInit(const BtGapCfg_t *pCfg)
 {
-    if (pCfg == nullptr)
-    {
-        return;
-    }
+	if (pCfg == nullptr)
+	{
+		return;
+	}
 
-    switch (pCfg->SecType)
-    {
-        case BTGAP_SECTYPE_NONE:
-            BLE_GAP_CONN_SEC_MODE_SET_OPEN(&s_GapConnMode);
-            break;
-        case BTGAP_SECTYPE_STATICKEY_NO_MITM:
-            BLE_GAP_CONN_SEC_MODE_SET_ENC_NO_MITM(&s_GapConnMode);
-            break;
-        case BTGAP_SECTYPE_STATICKEY_MITM:
-            BLE_GAP_CONN_SEC_MODE_SET_ENC_WITH_MITM(&s_GapConnMode);
-            break;
-        case BTGAP_SECTYPE_LESC_MITM:
-            BLE_GAP_CONN_SEC_MODE_SET_LESC_ENC_WITH_MITM(&s_GapConnMode);
-            break;
-        case BTGAP_SECTYPE_SIGNED_NO_MITM:
-            BLE_GAP_CONN_SEC_MODE_SET_SIGNED_NO_MITM(&s_GapConnMode);
-            break;
-        case BTGAP_SECTYPE_SIGNED_MITM:
-            BLE_GAP_CONN_SEC_MODE_SET_SIGNED_WITH_MITM(&s_GapConnMode);
-            break;
-        default:
-            BLE_GAP_CONN_SEC_MODE_SET_OPEN(&s_GapConnMode);
-            break;
-    }
+	BtGapSetSecMode(&s_GapConnMode, (BTGAP_SECTYPE)pCfg->SecType);
 
-    if (pCfg->Role & BT_GAP_ROLE_PERIPHERAL)
-    {
-        ble_gap_conn_params_t gap_conn_params = {};
+	if (pCfg->Role & BT_GAP_ROLE_PERIPHERAL)
+	{
+		ble_gap_conn_params_t gap_conn_params;
+		memset(&gap_conn_params, 0, sizeof(gap_conn_params));
 
-        gap_conn_params.min_conn_interval = MSEC_TO_UNITS(pCfg->ConnIntervalMin, UNIT_1_25_MS);
-        gap_conn_params.max_conn_interval = MSEC_TO_UNITS(pCfg->ConnIntervalMax, UNIT_1_25_MS);
-        gap_conn_params.slave_latency = BT_GAP_CONN_SLAVE_LATENCY;
-        gap_conn_params.conn_sup_timeout = MSEC_TO_UNITS(BT_GAP_CONN_SUP_TIMEOUT, UNIT_10_MS);
+		gap_conn_params.min_conn_interval = mSecTo1_25(pCfg->ConnIntervalMin);
+		gap_conn_params.max_conn_interval = mSecTo1_25(pCfg->ConnIntervalMax);
+		gap_conn_params.slave_latency = BT_GAP_CONN_SLAVE_LATENCY;
+		gap_conn_params.conn_sup_timeout = mSecTo10Ms(BT_GAP_CONN_SUP_TIMEOUT);
 
-        (void)sd_ble_gap_ppcp_set(&gap_conn_params);
-    }
+		(void)sd_ble_gap_ppcp_set(&gap_conn_params);
+	}
 }
 
 void BtGapSetDevName(const char *pDeviceName)
 {
-    if (pDeviceName == nullptr)
-    {
-        return;
-    }
+	if (pDeviceName == nullptr)
+	{
+		return;
+	}
 
-    (void)sd_ble_gap_device_name_set(&s_GapConnMode,
-                                     (const uint8_t *)pDeviceName,
-                                     strlen(pDeviceName));
+	(void)sd_ble_gap_device_name_set(&s_GapConnMode,
+									(const uint8_t *)pDeviceName,
+									(uint16_t)strlen(pDeviceName));
 }
 
 bool BtGapConnect(BtGapPeerAddr_t * const pPeerAddr, BtGapConnParams_t * const pConnParam)
 {
-    if (pPeerAddr == nullptr || pConnParam == nullptr)
-    {
-        return false;
-    }
+	if (pPeerAddr == nullptr || pConnParam == nullptr)
+	{
+		return false;
+	}
 
-    ble_gap_conn_params_t cparam = {};
-    ble_gap_addr_t addr = {
-        .addr_id_peer = 0,
-        .addr_type = pPeerAddr->Type,
-    };
+	ble_gap_addr_t addr;
+	memset(&addr, 0, sizeof(addr));
+	addr.addr_id_peer = 0;
+	addr.addr_type = pPeerAddr->Type;
+	memcpy(addr.addr, pPeerAddr->Addr, sizeof(addr.addr));
 
-    memcpy(addr.addr, pPeerAddr->Addr, sizeof(addr.addr));
+	ble_gap_conn_params_t cparam;
+	memset(&cparam, 0, sizeof(cparam));
+	cparam.min_conn_interval = mSecTo1_25(pConnParam->IntervalMin);
+	cparam.max_conn_interval = mSecTo1_25(pConnParam->IntervalMax);
+	cparam.slave_latency = pConnParam->Latency;
+	cparam.conn_sup_timeout = mSecTo10Ms(pConnParam->Timeout);
 
-    cparam.min_conn_interval = MSEC_TO_UNITS(pConnParam->IntervalMin, UNIT_1_25_MS);
-    cparam.max_conn_interval = MSEC_TO_UNITS(pConnParam->IntervalMax, UNIT_1_25_MS);
-    cparam.slave_latency = pConnParam->Latency;
-    cparam.conn_sup_timeout = MSEC_TO_UNITS(pConnParam->Timeout, UNIT_10_MS);
+	uint32_t err_code = sd_ble_gap_connect(&addr, &s_ScanParams, &cparam,
+										   BT_GAP_CONN_CFG_TAG);
 
-    ret_code_t err_code = sd_ble_gap_connect(&addr, &s_ScanParams, &cparam, BT_GAP_CONN_CFG_TAG);
+	return err_code == NRF_SUCCESS;
+}
 
-    return err_code == NRF_SUCCESS;
+static bool IsAllZero(const uint8_t *pData, size_t Len)
+{
+	for (size_t i = 0; i < Len; i++)
+	{
+		if (pData[i] != 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
 }
 
 bool BtGapScanInit(BtGapScanCfg_t * const pCfg)
 {
-    if (pCfg == nullptr)
-    {
-        return false;
-    }
+	if (pCfg == nullptr)
+	{
+		return false;
+	}
 
-#if (NRF_SD_BLE_API_VERSION >= 6)
-    s_ScanParams.active = (pCfg->Type == BTSCAN_TYPE_ACTIVE) ? 1 : 0;
-    s_ScanParams.scan_phys = pCfg->Param.Phy;
-#else
-    s_ScanParams.active = (pCfg->Type == BTSCAN_TYPE_ACTIVE) ? 1 : 0;
+	s_ScanParams.active = (pCfg->Type == BTSCAN_TYPE_ACTIVE) ? 1 : 0;
+	s_ScanParams.interval = mSecTo0_625((float)pCfg->Param.Interval);
+	s_ScanParams.window = mSecTo0_625((float)pCfg->Param.Duration);
+	s_ScanParams.timeout = (uint16_t)pCfg->Param.Timeout;
+	s_ScanParams.filter_policy = BLE_GAP_SCAN_FP_ACCEPT_ALL;
+	s_ScanParams.scan_phys = BLE_GAP_PHY_1MBPS;
+
+	if (pCfg->Param.Phy & BT_GAP_PHY_CODED)
+	{
+#ifdef BLE_GAP_PHY_CODED
+		s_ScanParams.scan_phys = BLE_GAP_PHY_CODED;
 #endif
+	}
+	else if (pCfg->Param.Phy & BT_GAP_PHY_2MBITS)
+	{
+#ifdef BLE_GAP_PHY_2MBPS
+		s_ScanParams.scan_phys = BLE_GAP_PHY_2MBPS;
+#else
+		s_ScanParams.scan_phys = BLE_GAP_PHY_1MBPS;
+#endif
+	}
 
-    s_ScanParams.timeout = pCfg->Param.Timeout;
-    s_ScanParams.window = MSEC_TO_UNITS(pCfg->Param.Duration, UNIT_0_625_MS);
-    s_ScanParams.interval = MSEC_TO_UNITS(pCfg->Param.Interval, UNIT_0_625_MS);
+	if (!IsAllZero(pCfg->BaseUid, sizeof(pCfg->BaseUid)))
+	{
+		uint8_t uidtype = BLE_UUID_TYPE_VENDOR_BEGIN;
+		ble_uuid128_t uid;
+		memcpy(uid.uuid128, pCfg->BaseUid, sizeof(uid.uuid128));
+		(void)sd_ble_uuid_vs_add(&uid, &uidtype);
+	}
 
-    uint8_t uuid_type = BLE_UUID_TYPE_VENDOR_BEGIN;
-    ble_uuid128_t uuid;
-
-    memcpy(uuid.uuid128, pCfg->BaseUid, sizeof(uuid.uuid128));
-
-    ret_code_t err_code = sd_ble_uuid_vs_add(&uuid, &uuid_type);
-
-    return err_code == NRF_SUCCESS;
+	return true;
 }
 
 bool BtGapScanStart(uint8_t * const pBuff, uint16_t Len)
 {
-    static ble_data_t rep_data;
+	if (pBuff == nullptr || Len == 0)
+	{
+		return false;
+	}
 
-    rep_data.len = Len;
-    rep_data.p_data = pBuff;
+	ble_data_t repdata;
+	repdata.p_data = pBuff;
+	repdata.len = Len;
 
-    uint32_t err_code = sd_ble_gap_scan_start(&s_ScanParams, &rep_data);
+	uint32_t err_code = sd_ble_gap_scan_start(&s_ScanParams, &repdata);
 
-    return err_code == NRF_SUCCESS;
+	return err_code == NRF_SUCCESS;
 }
 
 bool BtGapScanNext(uint8_t * const pBuff, uint16_t Len)
 {
-    static ble_data_t rep_data;
+	if (pBuff == nullptr || Len == 0)
+	{
+		return false;
+	}
 
-    rep_data.len = Len;
-    rep_data.p_data = pBuff;
+	ble_data_t repdata;
+	repdata.p_data = pBuff;
+	repdata.len = Len;
 
-    uint32_t err_code = sd_ble_gap_scan_start(nullptr, &rep_data);
+	uint32_t err_code = sd_ble_gap_scan_start(nullptr, &repdata);
 
-    return err_code == NRF_SUCCESS;
+	return err_code == NRF_SUCCESS;
 }
 
-void BtGapScanStop(void)
+void BtGapScanStop()
 {
-    (void)sd_ble_gap_scan_stop();
+	(void)sd_ble_gap_scan_stop();
 }
