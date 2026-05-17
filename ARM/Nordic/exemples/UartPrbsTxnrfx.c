ebdb893b31e8e553cd0e65ea8ad514a0165a62f9
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2026-05-17 15:06:41 -0400

    fixed nrfx uart with nrfc 4.3

--- ARM/Nordic/exemples/UartPrbsTxnrfx.c
+++ ARM/Nordic/exemples/UartPrbsTxnrfx.c
@@ -36,90 +36,122 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ----------------------------------------------------------------------------*/
 
-#include <stdio.h>
-#include <memory.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
 
 #include "nrfx_uarte.h"
 
 #include "prbs.h"
 
-// This include contain i/o definition the board in use
+// This include contains the I/O definition for the board in use.
 #include "board.h"
 
 //#define BYTE_MODE
-#define TEST_BUFSIZE		16
+#define TEST_BUFSIZE        16
 
 static volatile bool s_bTxDone = false;
 
-uint8_t s_CacheBuff[TEST_BUFSIZE];
+static uint8_t s_CacheBuff[TEST_BUFSIZE];
 
-const nrfx_uarte_t g_Uarte = NRFX_UARTE_INSTANCE(NRFX_UART_INST);
-const nrfx_uarte_config_t g_UarteCfg = {
-	.rxd_pin = (UART_RX_PORT << 5) | UART_RX_PIN,
-	.txd_pin = (UART_TX_PORT << 5) | UART_TX_PIN,
-	.tx_cache = { s_CacheBuff, TEST_BUFSIZE},
-	.config = {.hwfc = NRF_UARTE_HWFC_DISABLED, 0, 1, 0 },
-	.baudrate = NRF_UARTE_BAUDRATE_1000000,
-	.interrupt_priority = 1
-};
+/*
+ * nrfx 4.3 keeps mutable driver state inside nrfx_uarte_t.
+ * The instance must not be const.
+ *
+ * nRF52832 has one UARTE peripheral: NRF_UARTE0.
+ * NRFX_UARTE_INSTANCE() now takes the peripheral register pointer.
+ */
+static nrfx_uarte_t g_Uarte = NRFX_UARTE_INSTANCE(NRF_UARTE0);
 
-void uart_event_handler(nrfx_uarte_event_t const * p_event, void *ctx)
+static void uart_event_handler(nrfx_uarte_event_t const *p_event, void *ctx)
 {
-//	printf("Uarte Handler\r\n");
-	switch(p_event->type)
-	{
-		case NRFX_UARTE_EVT_TX_DONE: ///< Requested TX transfer completed.
-			s_bTxDone = true;
-			break;
-		case NRFX_UARTE_EVT_RX_DONE: ///< Requested RX transfer completed.
-		 	 break;
-		case NRFX_UARTE_EVT_ERROR:   ///< Error reported by UART peripheral.
-			break;
-	}
+    (void)ctx;
+
+    switch (p_event->type)
+    {
+        case NRFX_UARTE_EVT_TX_DONE:
+            s_bTxDone = true;
+            break;
+
+        case NRFX_UARTE_EVT_RX_DONE:
+            break;
+
+        case NRFX_UARTE_EVT_ERROR:
+            break;
+
+        default:
+            break;
+    }
 }
 
-int main()
+/*
+ * Do not define UARTE0_UART0_IRQHandler() here when nrfx_prs.c is linked.
+ * nrfx_prs owns the shared IRQ handler and dispatches it to nrfx_uarte.
+ */
+
+static int UartInit(void)
 {
-    uint32_t err_code;
-	uint8_t d = 0xff;
-	uint8_t buff[TEST_BUFSIZE];
+    nrfx_uarte_config_t cfg =
+        NRFX_UARTE_DEFAULT_CONFIG(
+            (UART_TX_PORT << 5) | UART_TX_PIN,
+            (UART_RX_PORT << 5) | UART_RX_PIN
+        );
 
+    cfg.tx_cache.p_buffer = s_CacheBuff;
+    cfg.tx_cache.length = sizeof(s_CacheBuff);
 
-    err_code = nrfx_uarte_init(&g_Uarte, &g_UarteCfg, uart_event_handler);
-    if (err_code != NRFX_SUCCESS)
+    cfg.baudrate = NRF_UARTE_BAUDRATE_1000000;
+    cfg.interrupt_priority = 1;
+
+    cfg.config.hwfc = NRF_UARTE_HWFC_DISABLED;
+    cfg.config.parity = NRF_UARTE_PARITY_EXCLUDED;
+
+    return nrfx_uarte_init(&g_Uarte, &cfg, uart_event_handler);
+}
+
+int main(void)
+{
+    int err_code;
+    uint8_t d = 0xff;
+    uint8_t buff[TEST_BUFSIZE];
+
+    err_code = UartInit();
+    if (err_code != 0)
     {
-    	//printf("Error %x\n\r", err_code);
+        while (1)
+        {
+        }
     }
 
-    //sprintf(buff, "UART PRBS Test\n\r");
-    //while (nrfx_uarte_tx(&g_Uarte, buff, strlen(buff)) != NRFX_SUCCESS);
-
-	while(1)
-	{
+    while (1)
+    {
 #ifdef BYTE_MODE
-		if (nrfx_uarte_tx(&g_Uarte, &d, 1, NRFX_UARTE_TX_BLOCKING) == NRFX_SUCCESS)
-		{
-			// If success send next code
-			d = Prbs8(d);
-		}
+        if (nrfx_uarte_tx(&g_Uarte, &d, 1, NRFX_UARTE_TX_BLOCKING) == 0)
+        {
+            d = Prbs8(d);
+        }
 #else
-		for (int i = 0; i < TEST_BUFSIZE; i++)
-		{
-			d = Prbs8(d);
-			buff[i] = d;
-		}
+        for (int i = 0; i < TEST_BUFSIZE; i++)
+        {
+            d = Prbs8(d);
+            buff[i] = d;
+        }
 
-		s_bTxDone = false;
-		if (nrfx_uarte_tx(&g_Uarte, buff, TEST_BUFSIZE, 0) == NRFX_SUCCESS)//NRFX_UARTE_TX_LINK) == NRFX_SUCCESS)
-		{
-			while (s_bTxDone == false);
-		}
+        s_bTxDone = false;
+
+        if (nrfx_uarte_tx(&g_Uarte, buff, sizeof(buff), 0) == 0)
+        {
+            while (s_bTxDone == false)
+            {
+            }
+        }
 #endif
-	}
-	return 0;
+    }
 }
 
-void _exit()
+void _exit(void)
 {
-	while(1);
+    while (1)
+    {
+    }
 }
