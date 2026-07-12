7e36cce0a2220b50ddd8d5900c2fcf4f39a81d8c
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2026-07-12 06:05:29 -0400

    fixed compile error

--- ARM/Nordic/nRF52/nRF52840/lib/include/psa/crypto.h
+++ ARM/Nordic/nRF52/nRF52840/lib/include/psa/crypto.h
@@ -1,6 +1,6 @@
 #ifndef IOSONATA_NRF52840_PSA_CRYPTO_FORWARD_H
 #define IOSONATA_NRF52840_PSA_CRYPTO_FORWARD_H
 
-#include "../../../../../../../../external/sdk-mbedtls/include/psa/crypto.h"
+#include "../../../../../external/TF-PSA-Crypto/include/psa/crypto.h"
 
 #endif
