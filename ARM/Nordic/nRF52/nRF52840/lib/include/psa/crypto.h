d11bc9512bf7d8d76919e22e9a86e2d3a9ed66e4
hnhoan <hnhoan@i-syst.com>
2026-07-12 01:11:49 -0400

    Expose sdk-mbedtls PSA header to nRF52840 SDC build

---
+++ ARM/Nordic/nRF52/nRF52840/lib/include/psa/crypto.h
@@ -0,0 +1,6 @@
+#ifndef IOSONATA_NRF52840_PSA_CRYPTO_FORWARD_H
+#define IOSONATA_NRF52840_PSA_CRYPTO_FORWARD_H
+
+#include "../../../../../../../../external/sdk-mbedtls/include/psa/crypto.h"
+
+#endif
