5e2fd5634200738897ee6564c74ac9ac63dbf2f3
hnhoan <hnhoan@i-syst.com>
2026-07-12 18:30:54 -0400

    retire separate CC3xx RNG translation unit

--- ARM/Nordic/src/cc3xx_rng.c
+++ ARM/Nordic/src/cc3xx_rng.c
@@ -1,1 +1,6 @@
-../../src/cc3xx_rng.cpp
+/*
+ * Compatibility translation unit for the existing nRF52840 Eclipse project.
+ *
+ * The CC3xx RNG entry points are implemented in ARM/src/crypto_cc3xx.cpp.
+ * Remove this linked resource when the Eclipse project is regenerated.
+ */
