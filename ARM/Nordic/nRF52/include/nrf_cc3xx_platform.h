223331435289539dbe5c113053c5ead03271e1f7
hnhoan <hnhoan@i-syst.com>
2026-07-12 02:48:01 -0400

    Fix nRF52840 SDC CC3xx forwarding path

---
+++ ARM/Nordic/nRF52/include/nrf_cc3xx_platform.h
@@ -0,0 +1,6 @@
+#ifndef IOSONATA_NRF52840_NRFXLIB_CC3XX_PLATFORM_FORWARD_H
+#define IOSONATA_NRF52840_NRFXLIB_CC3XX_PLATFORM_FORWARD_H
+
+#include "../../../../../external/sdk-nrfxlib/crypto/nrf_cc310_platform/include/nrf_cc3xx_platform.h"
+
+#endif
