36607db27a86872d4b9f1e927551cc8bd9b1d288
hnhoan <hnhoan@i-syst.com>
2026-08-02 10:22:37 -0400

    tests: add bounded SMP diagnostic trace

--- tests/bluetooth/compliance/smp/bt_smp_test_stubs.cpp
+++ tests/bluetooth/compliance/smp/bt_smp_test_stubs.cpp
@@ -1,7 +1,11 @@
 #include <signal.h>
+#include <stdarg.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
 
 #include "bluetooth/bt_gatt.h"
+#include "syslog.h"
 
 namespace {
 
@@ -20,6 +24,29 @@ extern "C" {
 void BtGattCccdRestoreBonded(uint16_t ConnHdl)
 {
 	(void)ConnHdl;
+}
+
+SysLog_t *SysLogGet(void)
+{
+	static SysLog_t log = {};
+	return &log;
+}
+
+int SysLogPrintf(SysLog_t * const pLog, const char *pFormat, ...)
+{
+	(void)pLog;
+	static unsigned count;
+	if (count++ >= 24 || pFormat == nullptr)
+	{
+		return 0;
+	}
+
+	fprintf(stderr, "[smp-worker %ld] ", (long)getpid());
+	va_list args;
+	va_start(args, pFormat);
+	int result = vfprintf(stderr, pFormat, args);
+	va_end(args);
+	return result;
 }
 
 } // extern "C"
