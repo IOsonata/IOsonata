f890d8da00a24391e847b863c95e87531ba177c8
hnhoan <hnhoan@i-syst.com>
2026-08-03 11:27:33 -0400

    Fix nRF54 GATT UUID and per-link state

--- ARM/Nordic/nRF54/src/bt_gatt_bm.cpp
+++ ARM/Nordic/nRF54/src/bt_gatt_bm.cpp
@@ -49,12 +49,9 @@ SOFTWARE.
 #include "bluetooth/bt_peer.h"
 
 /******** For DEBUG ************/
-#define BM_DEBUG_ENABLE
-
-#ifdef BM_DEBUG_ENABLE
-#include "coredev/uart.h"
-extern UART g_Uart;
-#define DEBUG_PRINTF(...)		g_Uart.printf(__VA_ARGS__)
+#if !defined(NDEBUG) && defined(DEBUG_ENABLE)
+#include "syslog.h"
+#define DEBUG_PRINTF(...)		SysLogPrintf(SysLogGet(), __VA_ARGS__)
 #else
 #define DEBUG_PRINTF(...)
 #endif
@@ -86,20 +83,32 @@ typedef struct {
 #endif
 
 alignas(4) static uint8_t s_LongWrScratch[BT_GATT_LONG_WRITE_SCRATCH_MAX];
-static uint16_t s_ConnHandle = BLE_CONN_HANDLE_INVALID;
+static uint8_t s_EmptyValue;
 
-bool BtGattCharNotify(uint16_t ConnHdl, BtGattChar_t *pChar,
-						  void * const pVal, size_t Len)
+static bool BtGattBmValueLenValid(const BtDevice_t *pConn, size_t Len)
 {
-	if (pChar == nullptr || (Len > 0 && pVal == nullptr) ||
-		Len > UINT16_MAX || Len > pChar->MaxDataLen ||
-		pChar->ValHdl == BT_ATT_HANDLE_INVALID)
+	if (pConn == nullptr)
 	{
 		return false;
 	}
 
-	uint16_t hdl = ConnHdl != BLE_CONN_HANDLE_INVALID ? ConnHdl : s_ConnHandle;
-	if (hdl == BLE_CONN_HANDLE_INVALID)
+	uint16_t mtu = pConn->Conn.MaxMtu >= BT_ATT_MTU_MIN ?
+			pConn->Conn.MaxMtu : BT_ATT_MTU_MIN;
+	return Len <= (size_t)mtu - 3U;
+}
+
+bool BtGattCharNotify(uint16_t ConnHdl, BtGattChar_t *pChar,
+						  void * const pVal, size_t Len)
+{
+	if (pChar == nullptr || ConnHdl == BLE_CONN_HANDLE_INVALID ||
+		(Len > 0 && pVal == nullptr) || Len > UINT16_MAX ||
+		Len > pChar->MaxDataLen || pChar->ValHdl == BT_ATT_HANDLE_INVALID)
+	{
+		return false;
+	}
+
+	BtDevice_t *pConn = BtPeerFindByHdl(ConnHdl);
+	if (!BtGattBmValueLenValid(pConn, Len))
 	{
 		return false;
 	}
@@ -108,43 +117,37 @@ bool BtGattCharNotify(uint16_t ConnHdl, BtGattChar_t *pChar,
 	memset(&params, 0, sizeof(params));
 	params.type = BLE_GATT_HVX_NOTIFICATION;
 	params.handle = pChar->ValHdl;
-	params.p_data = (uint8_t *)pVal;
+	params.p_data = Len > 0 ? (uint8_t *)pVal : &s_EmptyValue;
 	uint16_t len = (uint16_t)Len;
 	params.p_len = &len;
 
-	if (!BtGattTxPendingAdd(hdl, pChar))
+	if (!BtGattTxPendingAdd(ConnHdl, pChar))
 	{
 		return false;
 	}
 
-	uint32_t err = sd_ble_gatts_hvx(hdl, &params);
-	if (err == NRF_SUCCESS)
+	uint32_t err = sd_ble_gatts_hvx(ConnHdl, &params);
+	if (err == NRF_SUCCESS && len == Len)
 	{
 		return true;
 	}
 
-	BtGattTxPendRelease(hdl);
+	BtGattTxPendRelease(ConnHdl);
 	return false;
 }
 
 bool BtGattCharIndicate(uint16_t ConnHdl, BtGattChar_t *pChar,
 							void * const pVal, size_t Len)
 {
-	if (pChar == nullptr || (Len > 0 && pVal == nullptr) ||
-		Len > UINT16_MAX || Len > pChar->MaxDataLen ||
-		pChar->ValHdl == BT_ATT_HANDLE_INVALID)
+	if (pChar == nullptr || ConnHdl == BLE_CONN_HANDLE_INVALID ||
+		(Len > 0 && pVal == nullptr) || Len > UINT16_MAX ||
+		Len > pChar->MaxDataLen || pChar->ValHdl == BT_ATT_HANDLE_INVALID)
 	{
 		return false;
 	}
 
-	uint16_t hdl = ConnHdl != BLE_CONN_HANDLE_INVALID ? ConnHdl : s_ConnHandle;
-	if (hdl == BLE_CONN_HANDLE_INVALID)
-	{
-		return false;
-	}
-
-	BtDevice_t *pConn = BtPeerFindByHdl(hdl);
-	if (pConn == nullptr || pConn->Conn.bIndCfmPending)
+	BtDevice_t *pConn = BtPeerFindByHdl(ConnHdl);
+	if (!BtGattBmValueLenValid(pConn, Len) || pConn->Conn.bIndCfmPending)
 	{
 		return false;
 	}
@@ -153,12 +156,12 @@ bool BtGattCharIndicate(uint16_t ConnHdl, BtGattChar_t *pChar,
 	memset(&params, 0, sizeof(params));
 	params.type = BLE_GATT_HVX_INDICATION;
 	params.handle = pChar->ValHdl;
-	params.p_data = (uint8_t *)pVal;
+	params.p_data = Len > 0 ? (uint8_t *)pVal : &s_EmptyValue;
 	uint16_t len = (uint16_t)Len;
 	params.p_len = &len;
 
-	uint32_t err = sd_ble_gatts_hvx(hdl, &params);
-	if (err != NRF_SUCCESS)
+	uint32_t err = sd_ble_gatts_hvx(ConnHdl, &params);
+	if (err != NRF_SUCCESS || len != Len)
 	{
 		return false;
 	}
@@ -182,10 +185,10 @@ bool BtGattCharSetValue(BtGattChar_t *pChar, void * const pVal, size_t Len)
 	memset(&value, 0, sizeof(value));
 	value.offset = 0;
 	value.len = (uint16_t)Len;
-	value.p_value = (uint8_t *)pVal;
+	value.p_value = Len > 0 ? (uint8_t *)pVal : &s_EmptyValue;
 
-	return sd_ble_gatts_value_set(s_ConnHandle, pChar->ValHdl, &value) ==
-			NRF_SUCCESS;
+	return sd_ble_gatts_value_set(BLE_CONN_HANDLE_INVALID,
+			pChar->ValHdl, &value) == NRF_SUCCESS;
 }
 
 void BtGattSrvcDisconnected(BtGattSrvc_t *pSrvc)
@@ -302,28 +305,19 @@ void BtGattSrvcEvtHandler(BtGattSrvc_t * const pSrvc, uint32_t Evt,
 
 	switch (pBleEvt->header.evt_id)
 	{
-		case BLE_GAP_EVT_CONNECTED:
-			s_ConnHandle = pBleEvt->evt.gap_evt.conn_handle;
-			break;
-
 		case BLE_GAP_EVT_DISCONNECTED:
-			s_ConnHandle = BLE_CONN_HANDLE_INVALID;
-			for (int i = 0; i < pSrvc->NbChar; i++)
-			{
-				pSrvc->pCharArray[i].bNotify = false;
-				pSrvc->pCharArray[i].bIndic = false;
-			}
+			BtGattCccdClear(pBleEvt->evt.gap_evt.conn_handle);
 			break;
 
 		case BLE_GATTS_EVT_WRITE:
 		{
 			ble_gatts_evt_write_t *pWrite =
 				&pBleEvt->evt.gatts_evt.params.write;
+			uint16_t connHdl = pBleEvt->evt.gatts_evt.conn_handle;
 
 			if (pWrite->op == BLE_GATTS_OP_EXEC_WRITE_REQ_NOW)
 			{
-				BtDevice_t *pConn = BtPeerFindByHdl(
-					pBleEvt->evt.gatts_evt.conn_handle);
+				BtDevice_t *pConn = BtPeerFindByHdl(connHdl);
 				const uint8_t *pQueue = pConn != nullptr ?
 					pConn->Conn.pLongWrBuff : nullptr;
 				uint16_t queueSize = pConn != nullptr ?
@@ -356,23 +350,7 @@ void BtGattSrvcEvtHandler(BtGattSrvc_t * const pSrvc, uint32_t Evt,
 				{
 					uint16_t cccd = (uint16_t)(pWrite->data[0] |
 						((uint16_t)pWrite->data[1] << 8));
-					bool notify = (cccd &
-						BT_DESC_CLIENT_CHAR_CONFIG_NOTIFICATION) != 0;
-					bool indicate = (cccd &
-						BT_DESC_CLIENT_CHAR_CONFIG_INDICATION) != 0;
-					pChar->bNotify = notify;
-					pChar->bIndic = indicate;
-
-					if (pChar->SetNotifCB != nullptr)
-					{
-						pChar->SetNotifCB(pChar, notify,
-							pBleEvt->evt.gatts_evt.conn_handle);
-					}
-					if (pChar->SetIndCB != nullptr)
-					{
-						pChar->SetIndCB(pChar, indicate,
-							pBleEvt->evt.gatts_evt.conn_handle);
-					}
+					(void)BtGattCccdSet(connHdl, pChar->CccdHdl, cccd);
 					break;
 				}
 
@@ -390,7 +368,6 @@ void BtGattSrvcEvtHandler(BtGattSrvc_t * const pSrvc, uint32_t Evt,
 			break;
 
 		case BLE_GATTS_EVT_HVN_TX_COMPLETE:
-			// The global handler drains the per-link send-order ring once.
 			break;
 
 		default:
@@ -412,14 +389,6 @@ static void BtSrvcEncSec(ble_gap_conn_sec_mode_t *pSecMode,
 		case BTSRVC_SECTYPE_LESC_MITM:
 			BLE_GAP_CONN_SEC_MODE_SET_LESC_ENC_WITH_MITM(pSecMode);
 			break;
-		case BTSRVC_SECTYPE_SIGNED_NO_MITM:
-			// S145 does not expose signed-write verification. Require encrypted
-			// access rather than silently leaving the attribute open.
-			BLE_GAP_CONN_SEC_MODE_SET_ENC_NO_MITM(pSecMode);
-			break;
-		case BTSRVC_SECTYPE_SIGNED_MITM:
-			BLE_GAP_CONN_SEC_MODE_SET_ENC_WITH_MITM(pSecMode);
-			break;
 		case BTSRVC_SECTYPE_NONE:
 		default:
 			BLE_GAP_CONN_SEC_MODE_SET_OPEN(pSecMode);
@@ -428,7 +397,8 @@ static void BtSrvcEncSec(ble_gap_conn_sec_mode_t *pSecMode,
 }
 
 static uint32_t BtGattCharAdd(BtGattSrvc_t *pSrvc, BtGattChar_t *pChar,
-									 BTSRVC_SECTYPE SecType)
+									 BTSRVC_SECTYPE SecType,
+									 uint8_t SoftDeviceUuidType)
 {
 	ble_gatts_char_md_t charMd;
 	ble_gatts_attr_md_t cccdMd;
@@ -487,12 +457,9 @@ static uint32_t BtGattCharAdd(BtGattSrvc_t *pSrvc, BtGattChar_t *pChar,
 		BLE_GAP_CONN_SEC_MODE_SET_NO_ACCESS(&attrMd.write_perm);
 	}
 
-	uuid.type = pSrvc->Uuid.BaseIdx;
+	uuid.type = SoftDeviceUuidType;
 	uuid.uuid = pChar->Uuid;
 	attrMd.vloc = BLE_GATTS_VLOC_STACK;
-	attrMd.rd_auth =
-		(pChar->Property & BT_GATT_CHAR_PROP_AUTH_SIGNED) != 0;
-	attrMd.wr_auth = attrMd.rd_auth;
 	attrMd.vlen = 1;
 
 	attrValue.p_uuid = &uuid;
@@ -500,7 +467,8 @@ static uint32_t BtGattCharAdd(BtGattSrvc_t *pSrvc, BtGattChar_t *pChar,
 	attrValue.init_offs = 0;
 	attrValue.max_len = pChar->MaxDataLen;
 	attrValue.init_len = pChar->ValueLen;
-	attrValue.p_value = (uint8_t *)pChar->pValue;
+	attrValue.p_value = pChar->pValue != nullptr ?
+		(uint8_t *)pChar->pValue : &s_EmptyValue;
 
 	ble_gatts_char_handles_t handles;
 	memset(&handles, 0, sizeof(handles));
@@ -516,6 +484,7 @@ static uint32_t BtGattCharAdd(BtGattSrvc_t *pSrvc, BtGattChar_t *pChar,
 	pChar->DescHdl = handles.user_desc_handle;
 	pChar->CccdHdl = handles.cccd_handle;
 	pChar->SccdHdl = handles.sccd_handle;
+	pChar->BaseUuidIdx = pSrvc->Uuid.BaseIdx;
 	pChar->pSrvc = pSrvc;
 	return NRF_SUCCESS;
 }
@@ -528,35 +497,62 @@ bool BtGattSrvcAdd(BtGattSrvc_t *pSrvc)
 		return false;
 	}
 
-	// GAP and GATT are built into S145.
+	for (BtGattSrvc_t *p = BtGattGetSrvcList(); p != nullptr; p = p->pNext)
+	{
+		if (p == pSrvc)
+		{
+			return true;
+		}
+	}
+
 	if (pSrvc->UuidSrvc == BT_UUID_GATT_SERVICE_GENERIC_ACCESS ||
 		pSrvc->UuidSrvc == BT_UUID_GATT_SERVICE_GENERIC_ATTRIBUTE)
 	{
 		return true;
 	}
 
+	for (int i = 0; i < pSrvc->NbChar; i++)
+	{
+		BtGattChar_t *pChar = &pSrvc->pCharArray[i];
+		uint8_t sec = pChar->SecType != BT_GAP_SECTYPE_NONE ?
+			pChar->SecType : pSrvc->SecType;
+		if ((pChar->Property & BT_GATT_CHAR_PROP_AUTH_SIGNED) != 0 ||
+			sec == BT_GAP_SECTYPE_SIGNED_NO_MITM ||
+			sec == BT_GAP_SECTYPE_SIGNED_MITM ||
+			pChar->ValueLen > pChar->MaxDataLen ||
+			(pChar->ValueLen > 0 && pChar->pValue == nullptr))
+		{
+			return false;
+		}
+	}
+
 	uint32_t err;
 	ble_uuid_t uuid;
+	uint8_t softDeviceUuidType = BLE_UUID_TYPE_BLE;
 	pSrvc->Uuid.Type = BT_UUID_TYPE_16;
 	pSrvc->Uuid.Uuid16 = pSrvc->UuidSrvc;
 
 	if (pSrvc->bCustom)
 	{
-		(void)BtUuidAddBase(pSrvc->UuidBase);
-		uint8_t type = 0;
-		err = sd_ble_uuid_vs_add((ble_uuid128_t *)pSrvc->UuidBase, &type);
+		int baseIdx = BtUuidAddBase(pSrvc->UuidBase);
+		if (baseIdx < 0)
+		{
+			return false;
+		}
+		pSrvc->Uuid.BaseIdx = (uint8_t)baseIdx;
+		err = sd_ble_uuid_vs_add((ble_uuid128_t *)pSrvc->UuidBase,
+			&softDeviceUuidType);
 		if (err != NRF_SUCCESS)
 		{
 			return false;
 		}
-		pSrvc->Uuid.BaseIdx = type;
 	}
 	else
 	{
-		pSrvc->Uuid.BaseIdx = BLE_UUID_TYPE_BLE;
+		pSrvc->Uuid.BaseIdx = 0;
 	}
 
-	uuid.type = pSrvc->Uuid.BaseIdx;
+	uuid.type = softDeviceUuidType;
 	uuid.uuid = pSrvc->UuidSrvc;
 	err = sd_ble_gatts_service_add(
 		BLE_GATTS_SRVC_TYPE_PRIMARY, &uuid, &pSrvc->Hdl);
@@ -571,7 +567,8 @@ bool BtGattSrvcAdd(BtGattSrvc_t *pSrvc)
 		BTSRVC_SECTYPE sec = (BTSRVC_SECTYPE)(
 			pChar->SecType != BT_GAP_SECTYPE_NONE ?
 			pChar->SecType : pSrvc->SecType);
-		if (BtGattCharAdd(pSrvc, pChar, sec) != NRF_SUCCESS)
+		if (BtGattCharAdd(pSrvc, pChar, sec,
+			softDeviceUuidType) != NRF_SUCCESS)
 		{
 			return false;
 		}
