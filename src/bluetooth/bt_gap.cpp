19cb1c47add70da82578537442a92d92e3a065ff
hnhoan <hnhoan@i-syst.com>
2026-08-03 14:49:29 -0400

    Track GAP service initialization failures

--- src/bluetooth/bt_gap.cpp
+++ src/bluetooth/bt_gap.cpp
@@ -11,7 +11,7 @@ requirements of a Bluetooth device. For instance, for BR/EDR, it defines a
 Bluetooth device to include the Radio, Baseband, Link Manager, L2CAP, and the
 Service Discovery protocol functionality; for LE, it defines the Physical Layer,
 Link Layer, L2CAP, Security Manager, Attribute Protocol and Generic Attribute Profile.
-This ties all the various layers together to form the basic requirements for a
+This ties all the various layers together to form the basic requirements of a
 Bluetooth device. It also describes the behaviors and methods for device discovery,
 connection establishment, security, authentication, association models and
 service discovery.
@@ -29,8 +29,8 @@ Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+copies of the Software, and to permit persons to whom the Software is furnished
+to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
@@ -47,6 +47,7 @@ SOFTWARE.
 #include <memory.h>
 
 #include "bluetooth/bt_gatt.h"
+#include "bluetooth/bt_gatt_init.h"
 #include "bluetooth/bt_gap.h"
 #include "bluetooth/bt_dev.h"
 #include "bluetooth/bt_peer.h"
@@ -96,7 +97,11 @@ __attribute__((weak)) void BtGapSetDevName(const char *pName)
 		l = p->MaxDataLen - 1;
 	}
 
-	BtGattCharSetValue(p, (void*)pName, l);
+	if (!BtGattCharSetValue(p, (void*)pName, l))
+	{
+		BtGattInitStatusFail(BT_GATT_INIT_ERROR_VALUE_SET);
+		return;
+	}
 	if (p->pValue != nullptr)
 	{
 		((uint8_t*)p->pValue)[p->ValueLen] = '\0';
@@ -113,7 +118,10 @@ __attribute__((weak)) void BtGapSetAppearance(uint16_t Val)
 	uint8_t buf[2];
 	buf[0] = (uint8_t)(Val & 0xFF);
 	buf[1] = (uint8_t)(Val >> 8);
-	BtGattCharSetValue(&s_BtGapChar[1], buf, 2);
+	if (!BtGattCharSetValue(&s_BtGapChar[1], buf, 2))
+	{
+		BtGattInitStatusFail(BT_GATT_INIT_ERROR_VALUE_SET);
+	}
 }
 
 __attribute__((weak)) void BtGapSetPreferedConnParam(BtGattPreferedConnParams_t *pVal)
@@ -122,7 +130,11 @@ __attribute__((weak)) void BtGapSetPreferedConnParam(BtGattPreferedConnParams_t 
 	{
 		return;
 	}
-	BtGattCharSetValue(&s_BtGapChar[2], pVal, sizeof(BtGattPreferedConnParams_t));
+	if (!BtGattCharSetValue(&s_BtGapChar[2], pVal,
+			sizeof(BtGattPreferedConnParams_t)))
+	{
+		BtGattInitStatusFail(BT_GATT_INIT_ERROR_VALUE_SET);
+	}
 }
 
 __attribute__((weak)) void BtGapParamInit(const BtGapCfg_t *pCfg)
@@ -132,15 +144,20 @@ __attribute__((weak)) void BtGapParamInit(const BtGapCfg_t *pCfg)
 
 void BtGapInit(const BtGapCfg_t *pCfg)
 {
+	BtGattInitStatusReset();
 	if (pCfg == nullptr)
 	{
+		BtGattInitStatusFail(BT_GATT_INIT_ERROR_INVALID_CFG);
 		return;
 	}
 
 	if (pCfg->Role & BT_GAP_ROLE_PERIPHERAL)
 	{
-		BtGattSrvcAdd(&s_BtGattSrvc);
-		BtGattSrvcAdd(&s_BtGapSrvc);
+		if (!BtGattSrvcAdd(&s_BtGattSrvc) ||
+			!BtGattSrvcAdd(&s_BtGapSrvc))
+		{
+			BtGattInitStatusFail(BT_GATT_INIT_ERROR_SERVICE_ADD);
+		}
 	}
 
 	if (pCfg->Role & (BT_GAP_ROLE_PERIPHERAL | BT_GAP_ROLE_CENTRAL))
