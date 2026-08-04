b0c921b443f71c5341869cda4d53e72f1b0ea037
hnhoan <hnhoan@i-syst.com>
2026-08-04 01:51:14 -0400

    Use public connection state API for stale bond repair

--- ARM/Nordic/nRF54/src/bt_app_bm.cpp
+++ ARM/Nordic/nRF54/src/bt_app_bm.cpp
@@ -49,7 +49,7 @@ static void BtPmStaleBondRepair(uint32_t Evt, void *pCtx)
 
 	EnableInterrupt(intState);
 
-	if (!found || !pm_conn_state_valid(connHdl))
+	if (!found || !ble_conn_state_valid(connHdl))
 	{
 		return;
 	}
