205a23eb2e19854ff80c3f8712c3ceaea2937160
hnhoan <hnhoan@i-syst.com>
2026-08-05 00:30:04 -0400

    Support nRF52 multi-link advertising

--- ARM/Nordic/nRF52/src/bt_adv_nrf52.cpp
+++ ARM/Nordic/nRF52/src/bt_adv_nrf52.cpp
@@ -57,6 +57,10 @@ Copyright (c) 2016, I-SYST inc., all rights reserved
 // by BtAppStackInit when calling sd_ble_cfg_set / sd_ble_gap_adv_start.
 #define BTAPP_CONN_CFG_TAG            1
 
+// Run before the application observer (priority 1). Advertising termination
+// state must be current before its timeout or connection callback runs.
+#define BT_ADV_NRF52_OBSERVER_PRIO    0
+
 // --- Adv packet buffers and SoftDevice adv data ---
 // File-scope: nothing outside bt_adv_nrf52.cpp needs these.
 
@@ -67,6 +71,9 @@ typedef struct __Bt_App_Nrf52_Data {
 #pragma pack(pop)
 
 static BtAppNrf52Data_t s_Nrf52Data = { {0} };
+static bool s_bAdvertising;
+static int s_PeripheralLinkCount;
+static uint16_t s_PendingPeripheralHdl = BT_CONN_HDL_INVALID;
 
 alignas(4) static uint8_t s_BleAppAdvBuff[256];
 alignas(4) static BtAdvPacket_t s_BleAppAdvPkt = { 255, 0, s_BleAppAdvBuff };
@@ -79,10 +86,116 @@ static ble_gap_adv_data_t s_BtAppAdvData = {
 	.scan_rsp_data = { s_BleAppSrBuff, 0 }
 };
 
+static bool BtAdvCanStart()
+{
+	uint8_t role = g_BtAppData.AppDevice.Conn.Role;
+
+	if ((role & (BTAPP_ROLE_PERIPHERAL | BTAPP_ROLE_BROADCASTER)) == 0)
+	{
+		return false;
+	}
+
+	if ((role & BTAPP_ROLE_PERIPHERAL) == 0)
+	{
+		return true;
+	}
+
+	uint16_t peripheralCount = 0;
+	uint16_t freeCount = 0;
+	uint16_t peerCount = BtPeerCount();
+
+	for (uint16_t i = 0; i < peerCount; i++)
+	{
+		BtDevice_t *pPeer = BtPeerSlot(i);
+		if (pPeer == nullptr)
+		{
+			continue;
+		}
+		if (pPeer->Conn.Hdl == BT_CONN_HDL_INVALID)
+		{
+			freeCount++;
+		}
+		else if (pPeer->Conn.Role == BT_CONN_ROLE_PERIPHERAL)
+		{
+			peripheralCount++;
+		}
+	}
+
+	// The priority-0 connection observer runs before BtPeerConnected. Reserve
+	// the slot the accepted peripheral link is about to consume and include it
+	// in the configured peripheral-link limit. If observer ordering changes and
+	// the record already exists, do not count it twice.
+	if (s_PendingPeripheralHdl != BT_CONN_HDL_INVALID &&
+		BtPeerFindByHdl(s_PendingPeripheralHdl) == nullptr)
+	{
+		peripheralCount++;
+		if (freeCount > 0)
+		{
+			freeCount--;
+		}
+	}
+
+	return s_PeripheralLinkCount > 0 && freeCount > 0 &&
+		peripheralCount < s_PeripheralLinkCount;
+}
+
+static void BtAdvStateUpdate()
+{
+	if (BtPeerIsConnected())
+	{
+		g_BtAppData.State = BTAPP_STATE_CONNECTED;
+	}
+	else
+	{
+		g_BtAppData.State = s_bAdvertising ?
+			BTAPP_STATE_ADVERTISING : BTAPP_STATE_IDLE;
+	}
+}
+
+static void BtAdvNrf52EvtHandler(ble_evt_t const *pEvt, void *pContext)
+{
+	(void)pContext;
+
+	switch (pEvt->header.evt_id)
+	{
+		case BLE_GAP_EVT_CONNECTED:
+			if (pEvt->evt.gap_evt.params.connected.role == BLE_GAP_ROLE_PERIPH)
+			{
+				// Connectable advertising stops when it creates a peripheral
+				// link. Restart before the application callback, while reserving
+				// the peer slot that callback is about to populate.
+				s_bAdvertising = false;
+				s_PendingPeripheralHdl = pEvt->evt.gap_evt.conn_handle;
+				BtAdvStateUpdate();
+				BtAdvStart();
+				s_PendingPeripheralHdl = BT_CONN_HDL_INVALID;
+			}
+			break;
+
+		case BLE_GAP_EVT_ADV_SET_TERMINATED:
+			s_bAdvertising = false;
+			BtAdvStateUpdate();
+			break;
+
+		default:
+			break;
+	}
+}
+
+NRF_SDH_BLE_OBSERVER(s_BtAdvObserver, BT_ADV_NRF52_OBSERVER_PRIO,
+	BtAdvNrf52EvtHandler, NULL);
+
 void BtAdvStart()
 {
-	if (g_BtAppData.State == BTAPP_STATE_ADVERTISING || BtPeerIsConnected())
+	if (s_bAdvertising)
 	{
+		BtAdvStateUpdate();
+		return;
+	}
+
+	if (!BtAdvCanStart())
+	{
+		BtAdvStateUpdate();
 		return;
 	}
 
@@ -94,20 +207,35 @@ void BtAdvStart()
 		return;
 	}
 
-	g_BtAppData.State = BTAPP_STATE_ADVERTISING;
+	s_bAdvertising = true;
+	BtAdvStateUpdate();
 }
 
 void BtAdvStop()
 {
-	sd_ble_gap_adv_stop(g_BtAppData.AdvHdl);
-	g_BtAppData.State = BTAPP_STATE_IDLE;
+	if (!s_bAdvertising)
+	{
+		BtAdvStateUpdate();
+		return;
+	}
+
+	uint32_t err_code = sd_ble_gap_adv_stop(g_BtAppData.AdvHdl);
+	if (err_code != NRF_SUCCESS && err_code != NRF_ERROR_INVALID_STATE)
+	{
+		DEBUG_PRINTF("BtAdvStop failed: 0x%08" PRIx32 "\r\n", err_code);
+		return;
+	}
+
+	s_bAdvertising = false;
+	BtAdvStateUpdate();
 }
 
 bool BtAppAdvManDataSet(uint8_t *pAdvData, int AdvLen, uint8_t *pSrData,
 		int SrLen)
 {
 	if (g_BtAppData.State != BTAPP_STATE_ADVERTISING &&
-		g_BtAppData.State != BTAPP_STATE_IDLE)
+		g_BtAppData.State != BTAPP_STATE_IDLE &&
+		g_BtAppData.State != BTAPP_STATE_CONNECTED)
 	{
 		return false;
 	}
@@ -179,14 +307,16 @@ bool BtAppAdvManDataSet(uint8_t *pAdvData, int AdvLen, uint8_t *pSrData,
 		}
 	}
 
-	bool restart = g_BtAppData.State == BTAPP_STATE_ADVERTISING;
+	bool restart = s_bAdvertising;
 	if (restart)
 	{
 		uint32_t stopErr = sd_ble_gap_adv_stop(g_BtAppData.AdvHdl);
-		if (stopErr != NRF_SUCCESS)
+		if (stopErr != NRF_SUCCESS && stopErr != NRF_ERROR_INVALID_STATE)
 		{
 			return false;
 		}
+		s_bAdvertising = false;
+		BtAdvStateUpdate();
 	}
 
 	uint32_t err = sd_ble_gap_adv_set_configure(&g_BtAppData.AdvHdl,
@@ -198,9 +328,8 @@ bool BtAppAdvManDataSet(uint8_t *pAdvData, int AdvLen, uint8_t *pSrData,
 
 	if (restart)
 	{
-		g_BtAppData.State = BTAPP_STATE_IDLE;
 		BtAdvStart();
-		return g_BtAppData.State == BTAPP_STATE_ADVERTISING;
+		return s_bAdvertising;
 	}
 
 	return true;
@@ -220,6 +349,9 @@ __WEAK bool BtAppAdvInit(const BtAppCfg_t *pCfg)
 	BtAdvPacket_t *advpkt = &s_BleAppAdvPkt;
 	BtAdvPacket_t *srpkt  = &s_BleAppSrPkt;
 
+	s_bAdvertising = false;
+	s_PeripheralLinkCount = pCfg->PeriLinkCount;
+	s_PendingPeripheralHdl = BT_CONN_HDL_INVALID;
 	memset(&s_Nrf52Data.AdvParam, 0, sizeof(ble_gap_adv_params_t));
 
 	bool scannable = false;
