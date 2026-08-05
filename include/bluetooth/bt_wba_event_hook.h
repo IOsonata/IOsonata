c6b1f10641eef3bc4d3b7c36d301be1945347ee2
hnhoan <hnhoan@i-syst.com>
2026-08-05 14:22:56 -0400

    Keep STM32WBA indication confirmation separate

--- include/bluetooth/bt_wba_event_hook.h
+++ include/bluetooth/bt_wba_event_hook.h
@@ -287,9 +287,7 @@ static SVCCTL_UserEvtFlowStatus_t BtWbaEventHookDispatch(void *pPayload)
 			case ACI_GATT_SERVER_CONFIRMATION_VSEVT_CODE:
 				if (available >= 2)
 				{
-					uint16_t connHdl = BtWbaEvtLe16(pAci->data);
-					BtGattHandleValueConfirm(connHdl);
-					BtGattSendCompleted(connHdl, 1);
+					BtGattHandleValueConfirm(BtWbaEvtLe16(pAci->data));
 				}
 				return SVCCTL_UserEvtFlowEnable;
 
