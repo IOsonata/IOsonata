55efe3e1ee1719c0ac5b71e2ed3fca06bec9db07
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2026-08-14 10:57:37 -0400

    Move BlePdmDemo to generic examples as pdm_ble
    The example already used the generic PDM device API. Relocate the source
    to exemples/bluetooth/pdm_ble.cpp and drop the SDK coupling that kept it
    under the Nordic tree.
    
    Remove the unused blueio_blesrvc.h and custom_board.h includes. No symbol
    from either is referenced; the ICS-41352 microphone pins come from the
    project board.h.
    
    Flatten the NRF_SD_BLE_API_VERSION conditional around APP_ADV_TIMEOUT.
    Both arms defined it to 0.
    
    Replace the NRF52 conditional model name with a fixed string and delete
    commented-out SoftDevice and app_scheduler fragments.
    
    Repoint the nRF52832 and nRF52840 BlePdmDemo projects at the new path.

---
+++ exemples/bluetooth/pdm_ble.cpp
@@ -0,0 +1,412 @@
+/**-------------------------------------------------------------------------
+@example	pdm_ble.cpp
+
+
+@brief	PDM BLE demo
+
+This application demo shows PDM over BLE custom service using IOsonata library.
+For evaluating power consumption of the UART, the button 1 is used to enable/disable it.
+
+@author	Hoang Nguyen Hoan
+@date	Nov. 17, 2021
+
+@license
+
+MIT License
+
+Copyright (c) 2021 I-SYST inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+----------------------------------------------------------------------------*/
+#include <assert.h>
+
+#include "istddef.h"
+#include "convutil.h"
+#include "bluetooth/bt_app.h"
+#include "bluetooth/bt_intrf.h"
+#include "bluetooth/bt_gap.h"
+#include "bluetooth/bt_gatt.h"
+#include "blueio_board.h"
+#include "coredev/uart.h"
+#include "coredev/iopincfg.h"
+#include "iopinctrl.h"
+#include "coredev/pdm.h"
+#include "board.h"
+
+void MicCharSetNotify(BtGattChar_t *pBleSvc, bool bEnable, uint16_t ConnHdl);
+void CfgSrvcCallback(BtGattChar_t *pBleSvc, uint8_t *pData, int Offset, int Len);
+
+#define DEVICE_NAME                     "BlePdmDemo"                          /**< Name of device. Will be included in the advertising data. */
+
+#define PACKET_SIZE						400
+
+#define MAX_MTU							512
+
+#define MANUFACTURER_NAME               "I-SYST inc."                       /**< Manufacturer. Will be passed to Device Information Service. */
+
+#define MODEL_NAME                      "Generic"                           /**< Model number. Will be passed to Device Information Service. */
+
+#define MANUFACTURER_ID                 ISYST_BLUETOOTH_ID                  /**< Manufacturer ID, part of System ID. Will be passed to Device Information Service. */
+#define ORG_UNIQUE_ID                   ISYST_BLUETOOTH_ID                  /**< Organizational Unique ID, part of System ID. Will be passed to Device Information Service. */
+
+#define APP_ADV_INTERVAL                64 //MSEC_TO_UNITS(64, UNIT_0_625_MS)	/**< The advertising interval (in units of 0.625 ms. This value corresponds to 40 ms). */
+
+#define APP_ADV_TIMEOUT					0										/**< The advertising timeout in msec. 0 = no timeout. */
+
+#define MIN_CONN_INTERVAL               8//MSEC_TO_UNITS(8, UNIT_1_25_MS)     /**< Minimum acceptable connection interval (20 ms), Connection interval uses 1.25 ms units. */
+#define MAX_CONN_INTERVAL               40 //MSEC_TO_UNITS(40, UNIT_1_25_MS)     /**< Maximum acceptable connection interval (75 ms), Connection interval uses 1.25 ms units. */
+
+// 00000000-2a76-4901-a32a-db0eea85d0e5
+
+#define BLE_PDM_UUID_BASE				{ 0xe5, 0xd0, 0x85, 0xea, 0x0e, 0xdb, 0x2a, 0xa3, \
+										  0x01, 0x49, 0x76, 0x2a, 0x00, 0x00, 0x00, 0x00 }
+
+#define BLE_PDM_UUID_SERVICE		1
+#define BLE_PDM_CFG_UUID_CHAR		2
+#define BLE_PDM_DATA_UUID_CHAR		3
+
+typedef struct {
+	PDM_OPMODE Mode;
+	bool bDownsample;
+} MicConfig_t;
+
+#define PDM_BUFF_MAXLEN				400
+
+typedef struct __Pdm_Packet {
+	uint32_t Cnt;
+	int16_t Data[PDM_BUFF_MAXLEN / 2];
+} PdmPacket_t;
+
+static const BtUuidArr_t s_AdvUuid = {
+	.BaseIdx = 1,
+	.Type = BT_UUID_TYPE_16,
+	.Count = 1,
+	.Uuid16 = {BLE_PDM_UUID_SERVICE,}
+};
+
+static const char s_CfgCharDescString[] = {
+	"PDM Config characteristic",
+};
+
+static const char s_DataCharDescString[] = {
+	"PDM Data characteristic",
+};
+
+uint8_t g_ManData[8];
+
+static uint8_t s_PdmWrChar = 0;
+
+/// Characteristic definitions
+BtGattChar_t g_PdmChars[] = {
+	BT_CHAR(BLE_PDM_CFG_UUID_CHAR, 1,
+	        BT_GATT_CHAR_PROP_WRITE,
+	        s_CfgCharDescString,
+	        .WrCB = CfgSrvcCallback),
+	BT_CHAR(BLE_PDM_DATA_UUID_CHAR, sizeof(PdmPacket_t),
+	        BT_GATT_CHAR_PROP_READ | BT_GATT_CHAR_PROP_NOTIFY,
+	        s_DataCharDescString,
+	        .SetNotifCB = MicCharSetNotify),
+};
+
+uint8_t g_LWrBuffer[512];
+
+/// Service definition
+BtGattSrvc_t g_BlePdmSrvc = BT_SRVC_CUSTOM(BLE_PDM_UUID_BASE,
+                                           BLE_PDM_UUID_SERVICE,
+                                           g_PdmChars);
+
+const BtAppDevInfo_t s_BlePdmDevDesc = {
+	MODEL_NAME,       		// Model name
+	MANUFACTURER_NAME,		// Manufacturer name
+	"123",					// Serial number string
+	"0.0",					// Firmware version string
+	"0.0",					// Hardware version string
+};
+
+const BtAppCfg_t s_BleAppCfg = {
+	.Role = BTAPP_ROLE_PERIPHERAL,
+	.CentLinkCount = 0, 				// Number of central link
+	.PeriLinkCount = 1, 				// Number of peripheral link
+	.pDevName = DEVICE_NAME,			// Device name
+	.VendorId = ISYST_BLUETOOTH_ID,		// PnP Bluetooth/USB vendor id
+	.ProductId = 1,						// PnP Product ID
+	.ProductVer = 0,					// Pnp prod version
+	.Appearance = 0,
+	.pDevInfo = &s_BlePdmDevDesc,
+	.pAdvManData = g_ManData,			// Manufacture specific data to advertise
+	.AdvManDataLen = sizeof(g_ManData),	// Length of manufacture specific data
+	.pSrManData = NULL,
+	.SrManDataLen = 0,
+	.SecType = BTGAP_SECTYPE_NONE,		// Secure connection type
+	.SecExchg = BTAPP_SECEXCHG_NONE,	// Security key exchange
+	.pAdvUuid = NULL,      				// Service uuids to advertise
+	.AdvInterval = APP_ADV_INTERVAL,	// Advertising interval in msec
+	.AdvTimeout = APP_ADV_TIMEOUT,		// Advertising timeout in sec
+	.AdvSlowInterval = 0,				// Slow advertising interval, if > 0, fallback to
+										// slow interval on adv timeout and advertise until connected
+	.ConnIntervalMin = MIN_CONN_INTERVAL,
+	.ConnIntervalMax = MAX_CONN_INTERVAL,
+	.ConnLedPort = BLUEIO_CONNECT_LED_PORT,// Led port nuber
+	.ConnLedPin = BLUEIO_CONNECT_LED_PIN,// Led pin number
+	.TxPower = 0,						// Tx power
+	.MaxMtu = MAX_MTU,
+	.pLongWrPoolMem = g_LWrBuffer,		// Long-write reassembly pool (split across peer slots)
+	.LongWrPoolMemSize = sizeof(g_LWrBuffer),
+};
+
+int BleIntrfEvtCallback(DevIntrf_t *pDev, DEVINTRF_EVT EvtId, uint8_t *pBuffer, int BufferLen);
+
+#define BLEINTRF_FIFOSIZE			BTINTRF_CFIFO_TOTAL_MEMSIZE(20, sizeof(PdmPacket_t))
+
+alignas(4) static uint8_t s_BleIntrfRxFifo[BLEINTRF_FIFOSIZE];
+alignas(4) static uint8_t s_BleIntrfTxFifo[BLEINTRF_FIFOSIZE];
+
+
+static const BtIntrfCfg_t s_BleInrfCfg = {
+	&g_BlePdmSrvc,
+	0,
+	1,
+	sizeof(PdmPacket_t),			// Packet size : use default
+	true,
+	BLEINTRF_FIFOSIZE,			// Rx Fifo mem size
+	s_BleIntrfRxFifo,		// Rx Fifo mem pointer
+	BLEINTRF_FIFOSIZE,			// Tx Fifo mem size
+	s_BleIntrfTxFifo,		// Tx Fifo mem pointer
+	nullptr,//BleIntrfEvtCallback,
+};
+
+BtIntrf g_BleIntrf;
+
+static const IOPinCfg_t s_PdmPins[] = {
+	{ICS_41352_CLK_PORT, ICS_41352_CLK_PIN, ICS_41352_CLK_PINOP, IOPINDIR_OUTPUT, IOPINRES_NONE, IOPINTYPE_NORMAL},
+	{ICS_41352_DIN_PORT, ICS_41352_DIN_PIN, ICS_41352_DIN_PINOP, IOPINDIR_INPUT, IOPINRES_NONE, IOPINTYPE_NORMAL},
+};
+
+void PdmHandler(PdmDev_t *pDev, DEVINTRF_EVT Evt);
+
+#define PDM_FIFO_BLKSIZE		400
+#define PDM_FIFO_MEMSIZE		CFIFO_TOTAL_MEMSIZE(4, PDM_FIFO_BLKSIZE)
+
+alignas(4) static uint8_t s_PdmFifoMem[PDM_FIFO_MEMSIZE];
+
+static const PdmCfg_t s_PdmCfg = {
+	.pPins = s_PdmPins,
+	.NbPins = sizeof(s_PdmPins) / sizeof(IOPinCfg_t),
+	.Freq = 1032000,
+	.SmplMode = PDM_SMPLMODE_FALLING,
+	.OpMode = PDM_OPMODE_STEREO,
+	.GainLeft = 0,
+	.GainRight = 0,
+	.bIntEn = true,
+	.IntPrio = 6,
+	.EvtHandler = PdmHandler,
+	.pFifoMem = s_PdmFifoMem,
+	.FifoMemSize = PDM_FIFO_MEMSIZE,
+	.FifoBlkSize = PDM_FIFO_BLKSIZE,
+};
+
+PdmDev_t g_PdmDev;
+
+PdmPacket_t g_PdmPacket = {0,};
+
+MicConfig_t g_MicConfig = {PDM_OPMODE_STEREO,true};;
+
+int g_DelayCnt = 0;
+volatile bool g_bUartState = false;
+volatile bool g_bEnable = false;
+
+int BleIntrfEvtCallback(DevIntrf_t *pDev, DEVINTRF_EVT EvtId, uint8_t *pData, int Len)
+{
+	if (EvtId == DEVINTRF_EVT_RX_DATA)
+	{
+		// Nothing to do. The config below passes nullptr, this is left as the
+		// hook to fill in when the config characteristic needs to be handled.
+	}
+
+	return 0;
+}
+
+void CfgSrvcCallback(BtGattChar_t *pBleSvc, uint8_t *pData, int Offset, int Len)
+{
+
+}
+
+void MicCharSetNotify(BtGattChar_t *pBleSvc, bool bEnable, uint16_t ConnHdl)
+{
+	g_bEnable = bEnable;
+	if (bEnable)
+	{
+		PdmStart(&g_PdmDev);
+	}
+	else
+	{
+		PdmStop(&g_PdmDev);
+	}
+}
+
+void BtPeriphEvtHandler(uint32_t Evt, void *pCtx)
+{
+	BtGattEvtHandler(Evt, pCtx);
+}
+
+void BtAppInitUserServices()
+{
+    uint32_t       err_code;
+
+    err_code = BtGattSrvcAdd(&g_BlePdmSrvc);
+    assert(err_code == 0);
+}
+
+void PdmHandler(PdmDev_t *pDev, DEVINTRF_EVT Evt)
+{
+	if (Evt == DEVINTRF_EVT_RX_DATA)
+	{
+		static int bidx = 0;
+		static int sample_counter = 0;
+		static int sample_sum = 0;
+		static int sample_sum_L = 0;
+		static int sample_sum_R = 0;
+		//static int pkcnt = 0;
+		//static int j = 0;
+		static int16_t *pl = g_PdmPacket.Data;
+		static int16_t *pr = &g_PdmPacket.Data[1];
+
+		/*
+		int16_t Data1[PDM_BUFF_MAXLEN / 2];
+		for (int i=0;i<PDM_BUFF_MAXLEN / 2;i++){
+			Data1[i]=i;
+		}
+		memcpy(g_PdmPacket.Data, Data1, PDM_BUFF_MAXLEN);
+
+		g_BleIntrf.Tx(0, (uint8_t*)&g_PdmPacket, sizeof(PdmPacket_t));
+
+		g_PdmPacket.Cnt++; */
+
+
+		int16_t *sl = (int16_t*)PdmGetSamples(pDev);
+		if (sl)
+		{
+			//audio_pkt.PktCnt = pkcnt;
+			if (g_MicConfig.bDownsample)
+			{
+				//if (g_MicConfig.Mode != PDM_OPMODE_STEREO)
+				if (g_MicConfig.Mode == PDM_OPMODE_STEREO)
+				{
+					//Downsample STEREO-------------------------------------------------------
+					int16_t *sr = sl + 1;
+					for (int i = 0; i < (PDM_BUFF_MAXLEN / 2); i += 2)
+					{
+						sample_sum_L += EndianCvt16(*sl);
+						sample_sum_R += EndianCvt16(*sr);
+						sample_counter +=1;
+						if (sample_counter==3)
+						{
+							*pl = EndianCvt16(sample_sum_L/3);
+							*pr = EndianCvt16(sample_sum_R/3);
+							pl += 2;
+							pr += 2;
+							//j=j+2;
+							sample_sum_L = EndianCvt16(*sl);
+							sample_sum_R = EndianCvt16(*sr);
+							sample_counter = 1;
+						}
+						sl += 2;
+						sr += 2;
+					}
+				}
+				else
+				{
+					//Downsample MONO-------------------------------------------------------
+					for (int i = 0; i < (PDM_BUFF_MAXLEN / 2); i++)
+					{
+						sample_sum += EndianCvt16(*sl);
+						sample_counter +=1;
+						if (sample_counter==3)
+						{
+							*pl = EndianCvt16(sample_sum/3);
+							pl++;
+							sample_sum = EndianCvt16(*sl);
+							sample_counter = 1;
+						}
+						sl++;
+					}
+				}
+
+				if(bidx & 1)//== 0)
+				{
+					g_BleIntrf.Tx(0, (uint8_t*)&g_PdmPacket, sizeof(PdmPacket_t));
+					//j = 0;
+					g_PdmPacket.Cnt++;
+					pl = g_PdmPacket.Data;
+					pr = &g_PdmPacket.Data[1];
+					//pkcnt++;
+				}
+
+			}
+			else
+			{
+				memcpy(g_PdmPacket.Data, sl, PDM_BUFF_MAXLEN);
+				g_BleIntrf.Tx(0, (uint8_t*)&g_PdmPacket, sizeof(PdmPacket_t));
+				g_PdmPacket.Cnt++;
+				//pkcnt++;
+			}
+
+			bidx = (bidx + 1) & 1;
+		}
+	}
+}
+
+void HardwareInit()
+{
+	IOPinConfig(ICS_41352_VDD_PORT, ICS_41352_VDD_PIN, ICS_41352_VDD_PINOP, IOPINDIR_OUTPUT, IOPINRES_NONE, IOPINTYPE_NORMAL);
+	IOPinSet(ICS_41352_VDD_PORT, ICS_41352_VDD_PIN);
+
+	PdmInit(&g_PdmDev, &s_PdmCfg);
+}
+
+void BtAppInitUserData()
+{
+}
+
+//
+// Print a greeting message on standard output and exit.
+//
+// On embedded platforms this might require semi-hosting or similar.
+//
+// For example, for toolchains derived from GNU Tools for Embedded,
+// to enable semi-hosting, the following was added to the linker:
+//
+// --specs=rdimon.specs -Wl,--start-group -lgcc -lc -lm -lrdimon -Wl,--end-group
+//
+// Adjust it for other toolchains.
+//
+
+int main()
+{
+   HardwareInit();
+
+    BtAppInit(&s_BleAppCfg);//, true);
+
+    g_BleIntrf.Init(s_BleInrfCfg);
+
+    BtAppRun();
+
+	return 0;
+}
