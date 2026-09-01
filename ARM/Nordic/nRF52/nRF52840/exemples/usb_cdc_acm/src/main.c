d893f2982146354eb6030be2075debc0a8f0069c
hnhoan <hnhoan@i-syst.com>
2026-08-31 23:54:08 -0400

    Benchmark 64-byte CFifo USB staging

--- ARM/Nordic/nRF52/nRF52840/exemples/usb_cdc_acm/src/main.c
+++ ARM/Nordic/nRF52/nRF52840/exemples/usb_cdc_acm/src/main.c
@@ -39,6 +39,7 @@
  */
 #include <stdint.h>
 #include <stdbool.h>
+#include <stddef.h>
 
 #include "nrf.h"
 #include "nrf_drv_usbd.h"
@@ -46,7 +47,10 @@
 
 #include "app_error.h"
 #include "app_util.h"
+#include "app_usbd_core.h"
 #include "app_usbd.h"
+#include "app_usbd_string_desc.h"
+#include "app_usbd_cdc_acm.h"
 #include "app_usbd_serial_num.h"
 #include "app_timer.h"
 
@@ -57,131 +61,272 @@
 #include "nrf_log_ctrl.h"
 #include "nrf_log_default_backends.h"
 
-#include "prbs.h"
-#include "usbd_cdc_intrf.h"
+#include "cfifo.h"
 
-#define LED_USB_RESUME			(BSP_BOARD_LED_0)
+/**@file
+ * @defgroup usbd_cdc_acm_example main.c
+ * @{
+ * @ingroup usbd_cdc_acm_example
+ * @brief Nordic USBD CDC ACM PRBS transmit stream with CFifo staging
+ *
+ */
 
+#define LED_USB_RESUME      (BSP_BOARD_LED_0)
+#define LED_CDC_ACM_OPEN    (BSP_BOARD_LED_1)
+#define LED_CDC_ACM_RX      (BSP_BOARD_LED_2)
+#define LED_CDC_ACM_TX      (BSP_BOARD_LED_3)
+
+/**
+ * @brief Enable power USB detection
+ *
+ * Configure if example supports USB port connection
+ */
 #ifndef USBD_POWER_DETECTION
-#define USBD_POWER_DETECTION	true
+#define USBD_POWER_DETECTION true
 #endif
 
-#define CDC_RXFIFO_MEMSIZE		CFIFO_MEMSIZE(256)
-#define CDC_TXFIFO_MEMSIZE		CFIFO_MEMSIZE(2048)
+static void cdc_acm_user_ev_handler(app_usbd_class_inst_t const * p_inst,
+                                    app_usbd_cdc_acm_user_event_t event);
 
-static uint8_t s_CdcRxFifoMem[CDC_RXFIFO_MEMSIZE] __attribute__((aligned(4)));
-static uint8_t s_CdcTxFifoMem[CDC_TXFIFO_MEMSIZE] __attribute__((aligned(4)));
-static UsbdCdcDevIntrf_t s_CdcIntrf;
+#define CDC_ACM_COMM_INTERFACE  0
+#define CDC_ACM_COMM_EPIN       NRF_DRV_USBD_EPIN2
 
-static const UsbdCdcIntrfCfg_t s_CdcCfg = {
-	.bBlocking = true,
-	.RxFifoMemSize = sizeof(s_CdcRxFifoMem),
-	.pRxFifoMem = s_CdcRxFifoMem,
-	.TxFifoMemSize = sizeof(s_CdcTxFifoMem),
-	.pTxFifoMem = s_CdcTxFifoMem,
-	.EvtCB = NULL,
-};
+#define CDC_ACM_DATA_INTERFACE  1
+#define CDC_ACM_DATA_EPIN       NRF_DRV_USBD_EPIN1
+#define CDC_ACM_DATA_EPOUT      NRF_DRV_USBD_EPOUT1
+
+/**
+ * @brief CDC_ACM class instance
+ */
+APP_USBD_CDC_ACM_GLOBAL_DEF(m_test_cdc_acm,
+                            cdc_acm_user_ev_handler,
+                            CDC_ACM_COMM_INTERFACE,
+                            CDC_ACM_DATA_INTERFACE,
+                            CDC_ACM_COMM_EPIN,
+                            CDC_ACM_DATA_EPIN,
+                            CDC_ACM_DATA_EPOUT,
+                            APP_USBD_CDC_COMM_PROTOCOL_AT_V250
+);
+
+#define TX_FIFO_MEMSIZE CFIFO_MEMSIZE(2048)
+
+static uint8_t m_tx_fifo_mem[TX_FIFO_MEMSIZE] __attribute__((aligned(4)));
+static hCFifo_t m_tx_fifo;
+static volatile bool m_port_open;
+static volatile bool m_tx_ready;
+static uint8_t m_prbs = 0xff;
 
 uint8_t g_extern_usbd_serial_number[12 + 1] = { "123456"};
 uint8_t g_extern_usbd_product_string[12 + 1] = { "Test" };
 
+/**
+ * @brief Generate the next value of the same 8-bit PRBS used by IOsonata tests.
+ *
+ * Polynomial: X^7 + X^6 + 1, repetition period 127.
+ */
+static uint8_t prbs8(uint8_t cur)
+{
+    uint8_t newbit = (((cur >> 6) ^ (cur >> 5)) & 1U);
+    return (uint8_t)(((cur << 1) | newbit) & 0x7fU);
+}
+
+/**
+ * @brief CDC ACM user event handler
+ */
+static void cdc_acm_user_ev_handler(app_usbd_class_inst_t const * p_inst,
+                                    app_usbd_cdc_acm_user_event_t event)
+{
+    (void)p_inst;
+
+    switch (event)
+    {
+        case APP_USBD_CDC_ACM_USER_EVT_PORT_OPEN:
+            bsp_board_led_on(LED_CDC_ACM_OPEN);
+            m_prbs = 0xff;
+            CFifoFlush(m_tx_fifo);
+            m_port_open = true;
+            m_tx_ready = true;
+            break;
+
+        case APP_USBD_CDC_ACM_USER_EVT_PORT_CLOSE:
+            m_port_open = false;
+            m_tx_ready = false;
+            bsp_board_led_off(LED_CDC_ACM_OPEN);
+            break;
+
+        case APP_USBD_CDC_ACM_USER_EVT_TX_DONE:
+            m_tx_ready = true;
+            bsp_board_led_invert(LED_CDC_ACM_TX);
+            break;
+
+        case APP_USBD_CDC_ACM_USER_EVT_RX_DONE:
+            bsp_board_led_invert(LED_CDC_ACM_RX);
+            break;
+
+        default:
+            break;
+    }
+}
+
 static void usbd_user_ev_handler(app_usbd_event_type_t event)
 {
-	switch (event)
-	{
-		case APP_USBD_EVT_DRV_SOF:
-			UsbdCdcIntrfTxKick(&s_CdcIntrf);
-			break;
+    switch (event)
+    {
+        case APP_USBD_EVT_DRV_SUSPEND:
+            bsp_board_led_off(LED_USB_RESUME);
+            break;
 
-		case APP_USBD_EVT_DRV_SUSPEND:
-			bsp_board_led_off(LED_USB_RESUME);
-			break;
+        case APP_USBD_EVT_DRV_RESUME:
+            bsp_board_led_on(LED_USB_RESUME);
+            break;
 
-		case APP_USBD_EVT_DRV_RESUME:
-			bsp_board_led_on(LED_USB_RESUME);
-			break;
+        case APP_USBD_EVT_STARTED:
+            break;
 
-		case APP_USBD_EVT_STOPPED:
-			app_usbd_disable();
-			bsp_board_leds_off();
-			break;
+        case APP_USBD_EVT_STOPPED:
+            app_usbd_disable();
+            m_port_open = false;
+            m_tx_ready = false;
+            bsp_board_leds_off();
+            break;
 
-		case APP_USBD_EVT_POWER_DETECTED:
-			if (!nrf_drv_usbd_is_enabled())
-			{
-				app_usbd_enable();
-			}
-			break;
+        case APP_USBD_EVT_POWER_DETECTED:
+            NRF_LOG_INFO("USB power detected");
 
-		case APP_USBD_EVT_POWER_REMOVED:
-			app_usbd_stop();
-			break;
+            if (!nrf_drv_usbd_is_enabled())
+            {
+                app_usbd_enable();
+            }
+            break;
 
-		case APP_USBD_EVT_POWER_READY:
-			app_usbd_start();
-			break;
+        case APP_USBD_EVT_POWER_REMOVED:
+            NRF_LOG_INFO("USB power removed");
+            m_port_open = false;
+            m_tx_ready = false;
+            app_usbd_stop();
+            break;
 
-		default:
-			break;
-	}
+        case APP_USBD_EVT_POWER_READY:
+            NRF_LOG_INFO("USB ready");
+            app_usbd_start();
+            break;
+
+        default:
+            break;
+    }
+}
+
+static void init_bsp(void)
+{
+    bsp_board_init(BSP_INIT_LEDS);
 }
 
 int main(void)
 {
-	ret_code_t ret;
-	uint8_t data = 0xff;
-	static const app_usbd_config_t usbd_config = {
-		.ev_state_proc = usbd_user_ev_handler,
-		.enable_sof = true,
-	};
+    ret_code_t ret;
+    static const app_usbd_config_t usbd_config = {
+        .ev_state_proc = usbd_user_ev_handler
+    };
 
-	ret = NRF_LOG_INIT(NULL);
-	APP_ERROR_CHECK(ret);
+    ret = NRF_LOG_INIT(NULL);
+    APP_ERROR_CHECK(ret);
 
-	ret = nrf_drv_clock_init();
-	APP_ERROR_CHECK(ret);
+    ret = nrf_drv_clock_init();
+    APP_ERROR_CHECK(ret);
 
-	nrf_drv_clock_lfclk_request(NULL);
-	while (!nrf_drv_clock_lfclk_is_running())
-	{
-	}
+    nrf_drv_clock_lfclk_request(NULL);
 
-	ret = app_timer_init();
-	APP_ERROR_CHECK(ret);
+    while (!nrf_drv_clock_lfclk_is_running())
+    {
+        /* Just waiting */
+    }
 
-	bsp_board_init(BSP_INIT_LEDS);
-	app_usbd_serial_num_generate();
+    ret = app_timer_init();
+    APP_ERROR_CHECK(ret);
 
-	ret = app_usbd_init(&usbd_config);
-	APP_ERROR_CHECK(ret);
+    init_bsp();
 
-	if (!UsbdCdcIntrfInit(&s_CdcIntrf, &s_CdcCfg))
-	{
-		return -1;
-	}
+    app_usbd_serial_num_generate();
 
-	if (USBD_POWER_DETECTION)
-	{
-		ret = app_usbd_power_events_enable();
-		APP_ERROR_CHECK(ret);
-	}
-	else
-	{
-		app_usbd_enable();
-		app_usbd_start();
-	}
+    m_tx_fifo = CFifoInit(m_tx_fifo_mem, sizeof(m_tx_fifo_mem), 1, true);
+    if (m_tx_fifo == NULL)
+    {
+        return -1;
+    }
 
-	while (true)
-	{
+    ret = app_usbd_init(&usbd_config);
+    APP_ERROR_CHECK(ret);
+    NRF_LOG_INFO("Nordic USBD CDC ACM CFifo PRBS stream started.");
+
+    app_usbd_class_inst_t const * class_cdc_acm =
+        app_usbd_cdc_acm_class_inst_get(&m_test_cdc_acm);
+    ret = app_usbd_class_append(class_cdc_acm);
+    APP_ERROR_CHECK(ret);
+
+    if (USBD_POWER_DETECTION)
+    {
+        ret = app_usbd_power_events_enable();
+        APP_ERROR_CHECK(ret);
+    }
+    else
+    {
+        NRF_LOG_INFO("No USB power detection enabled\r\nStarting USB now");
+        app_usbd_enable();
+        app_usbd_start();
+    }
+
+    while (true)
+    {
 #if APP_USBD_CONFIG_EVENT_QUEUE_ENABLE
-		while (app_usbd_event_queue_process())
-		{
-		}
+        while (app_usbd_event_queue_process())
+        {
+            /* Drain all queued USB power, state and endpoint events. */
+        }
 #endif
 
-		if (s_CdcIntrf.DevIntrf.TxData(&s_CdcIntrf.DevIntrf, &data, 1) == 1)
-		{
-			data = Prbs8(data);
-		}
-	}
+        if (m_port_open && m_tx_ready)
+        {
+            int count = NRF_DRV_USBD_EPSIZE;
+            uint8_t * p_put = CFifoPutMultiple(m_tx_fifo, &count);
+
+            if (p_put != NULL && count == NRF_DRV_USBD_EPSIZE)
+            {
+                uint8_t next = m_prbs;
+
+                for (int i = 0; i < count; ++i)
+                {
+                    p_put[i] = next;
+                    next = prbs8(next);
+                }
+
+                count = NRF_DRV_USBD_EPSIZE;
+                uint8_t * p_tx = CFifoGetMultiple(m_tx_fifo, &count);
+
+                if (p_tx != NULL && count == NRF_DRV_USBD_EPSIZE)
+                {
+                    /*
+                     * The FIFO slot is the EasyDMA source buffer. No new FIFO
+                     * data is produced until TX_DONE, so this slot cannot be
+                     * reused while USB owns it.
+                     */
+                    m_tx_ready = false;
+                    ret = app_usbd_cdc_acm_write(&m_test_cdc_acm, p_tx, count);
+
+                    if (ret == NRF_SUCCESS)
+                    {
+                        m_prbs = next;
+                    }
+                    else
+                    {
+                        m_tx_ready = true;
+                    }
+                }
+            }
+        }
+
+        UNUSED_RETURN_VALUE(NRF_LOG_PROCESS());
+        __WFE();
+    }
 }
+
+/** @} */
