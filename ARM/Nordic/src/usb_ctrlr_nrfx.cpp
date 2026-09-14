cf1477d63a7721b4b9410c2028c0490235ef317e
hnhoan <hnhoan@i-syst.com>
2026-09-14 14:09:02 -0400

    Defer nRF52 EP0 setup processing

--- ARM/Nordic/src/usb_ctrlr_nrfx.cpp
+++ ARM/Nordic/src/usb_ctrlr_nrfx.cpp
@@ -221,6 +221,7 @@ typedef struct __nRF_Usbd_Xfer
 typedef struct __nRF_Usbd_Ctrlr
 {
 	nRFUsbdXfer_t Xfer[NRFX_USBD_EP_COUNT][2];
+	UsbCtrlrEvt_t SetupEvt;
 	bool SofEnabled;
 	bool SetupDirIn;
 } nRFUsbdCtrlr_t;
@@ -261,6 +262,7 @@ static hCFifo_t s_hQue;
 // packet here in either direction; control transfers are serialized by EP0.
 alignas(4) static uint8_t s_Ep0Bounce[NRFX_USBD_MAX_PACKET_SIZE];
 
+static atomic_bool s_PendingSetup;
 static atomic_bool s_PendingEp0Status;
 static atomic_bool s_PendingEp0RcvOut;
 static atomic_bool s_BusSuspended;
@@ -1359,7 +1361,8 @@ static bool nRFUsbdStartDmaNow(const nRFUsbdQue_t *pQue)
 
 static void nRFUsbdServicePending(void)
 {
-	if (atomic_load(&s_HostResumePending) ||
+	if (atomic_load(&s_PendingSetup) ||
+		atomic_load(&s_HostResumePending) ||
 		(atomic_load(&s_BusSuspended) && !atomic_load(&s_SuspendPending)))
 	{
 		return;
@@ -1551,6 +1554,7 @@ static void nRFUsbdResetState(void)
 	s_Ctrlr.SetupDirIn = false;
 
 	CFifoFlush(s_hQue);
+	atomic_store(&s_PendingSetup, false);
 	atomic_store(&s_PendingEp0Status, false);
 	atomic_store(&s_PendingEp0RcvOut, false);
 	atomic_store(&s_BusSuspended, false);
@@ -1603,6 +1607,7 @@ static void nRFUsbdTryEnterLowPower(void)
 		atomic_load(&s_HostResumePending) ||
 		nRFUsbdDmaActive() ||
 		CFifoUsed(s_hQue) > 0 ||
+		atomic_load(&s_PendingSetup) ||
 		atomic_load(&s_PendingEp0Status) ||
 		atomic_load(&s_PendingEp0RcvOut))
 	{
@@ -1623,6 +1628,7 @@ static void nRFUsbdTryEnterLowPower(void)
 		atomic_load(&s_HostResumePending) ||
 		nRFUsbdDmaActive() ||
 		CFifoUsed(s_hQue) > 0 ||
+		atomic_load(&s_PendingSetup) ||
 		atomic_load(&s_PendingEp0Status) ||
 		atomic_load(&s_PendingEp0RcvOut))
 	{
@@ -2160,6 +2166,26 @@ static void nRFUsbdBusReset(void)
 	nRFUsbdResetState();
 }
 
+static void nRFUsbdProcessSetup(uint32_t Evt, void *pContext)
+{
+	(void)Evt;
+	(void)pContext;
+
+	const uint32_t state = DisableInterrupt();
+	if (!atomic_load(&s_PendingSetup))
+	{
+		EnableInterrupt(state);
+		return;
+	}
+
+	const UsbCtrlrEvt_t evt = s_Ctrlr.SetupEvt;
+	atomic_store(&s_PendingSetup, false);
+	EnableInterrupt(state);
+
+	nRFUsbdEmit(&evt);
+	nRFUsbdServicePending();
+}
+
 static void nRFUsbdSetupEvent(void)
 {
 	UsbCtrlrEvt_t evt = {};
@@ -2176,21 +2202,20 @@ static void nRFUsbdSetupEvent(void)
 	s_Ctrlr.SetupDirIn =
 		(evt.Setup.bmRequestType & USB_REQTYPE_MASK_DIR) != 0;
 
-	const bool setAddress =
-		(evt.Setup.bmRequestType &
+	if ((evt.Setup.bmRequestType &
 		 (USB_REQTYPE_MASK_RECEIPT | USB_REQTYPE_MASK_TYPE)) == 0 &&
-		evt.Setup.bRequest == USB_REQ_SET_ADDRESS;
-
-	if (setAddress)
+		evt.Setup.bRequest == USB_REQ_SET_ADDRESS)
 	{
-		UsbCtrlrEvt_t addrEvt = {};
-		addrEvt.Type = USB_CTRLR_EVT_ADDRESS;
-		addrEvt.Address = (uint8_t)(evt.Setup.wValue & 0x7FU);
-		nRFUsbdEmit(&addrEvt);
-		return;
+		evt.Type = USB_CTRLR_EVT_ADDRESS;
+		evt.Address = (uint8_t)(evt.Setup.wValue & 0x7FU);
 	}
 
-	nRFUsbdEmit(&evt);
+	s_Ctrlr.SetupEvt = evt;
+	if (!atomic_exchange(&s_PendingSetup, true) &&
+		!AppEvtHandlerQue(0U, NULL, nRFUsbdProcessSetup))
+	{
+		atomic_store(&s_PendingSetup, false);
+	}
 }
 
 static void nRFUsbdHandleOutEnd(uint8_t EpNum, uint16_t TransferLen)
