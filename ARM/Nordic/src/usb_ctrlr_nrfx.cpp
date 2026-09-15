6ae2e35848a00e9ae426aeb8c0ec22370ed94e3f
hnhoan <hnhoan@i-syst.com>
2026-09-15 18:53:02 -0400

    Remove blocking EP0 ISR trace

--- ARM/Nordic/src/usb_ctrlr_nrfx.cpp
+++ ARM/Nordic/src/usb_ctrlr_nrfx.cpp
@@ -56,7 +56,6 @@ SOFTWARE.
 #include <stdint.h>
 #include <stdatomic.h>
 #include <string.h>
-#include <stdio.h>
 
 #include "nrf.h"
 #include "nrf_peripherals.h"
@@ -2600,8 +2599,6 @@ extern "C" void USBD_IRQHandler(void)
 			evt.Setup.bRequest == USB_REQ_SET_ADDRESS;
 		if (setAddress)
 		{
-			printf("EP0 ADDRESS IRQ value=%u\n",
-				(unsigned)(evt.Setup.wValue & 0x7FU));
 			UsbCtrlrEvt_t addrEvt = {};
 			addrEvt.Type = USB_CTRLR_EVT_ADDRESS;
 			addrEvt.Address = (uint8_t)(evt.Setup.wValue & 0x7FU);
