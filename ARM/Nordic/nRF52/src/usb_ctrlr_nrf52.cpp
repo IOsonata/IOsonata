e71a86d7b1e8fcbfca17054a0d79d94169f0c796
hnhoan <hnhoan@i-syst.com>
2026-09-19 10:52:46 -0400

    Revert neutral errata noinline change

--- ARM/Nordic/nRF52/src/usb_ctrlr_nrf52.cpp
+++ ARM/Nordic/nRF52/src/usb_ctrlr_nrf52.cpp
@@ -369,7 +369,7 @@ static void UsbdErrataApply(void)
 	Usbd171Write(0x000000C0UL);
 }
 
-static __attribute__((noinline)) void UsbdErrataRevert(void)
+static void UsbdErrataRevert(void)
 {
 	Usbd171Write(0x00000000UL);
 
