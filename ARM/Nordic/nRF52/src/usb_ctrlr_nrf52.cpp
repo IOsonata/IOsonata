c4d6711bb5a0d70cc67d79d3e508c53b08f366cb
hnhoan <hnhoan@i-syst.com>
2026-09-21 15:19:26 -0400

    usb: select DMA register bank directly

--- ARM/Nordic/nRF52/src/usb_ctrlr_nrf52.cpp
+++ ARM/Nordic/nRF52/src/usb_ctrlr_nrf52.cpp
@@ -626,34 +626,27 @@ static inline __attribute__((always_inline))
 void nRFUsbdStartDmaNow(const nRFUsbdQue_t *pQue)
 {
 	const uint8_t epNum = pQue->EpNum;
-	const bool isIn = pQue->Dir != NRFX_USBD_QUE_OUT;
-	const uint8_t *pBuffer;
-
-	if (pQue->Dir == NRFX_USBD_QUE_IN_SCRATCH)
-	{
-		pBuffer = (const uint8_t *)&pQue->Scratch;
-	}
-	else
-	{
-		pBuffer = pQue->pBuffer;
-	}
-
+	const bool out = pQue->Dir == NRFX_USBD_QUE_OUT;
+	const uint8_t *pBuffer = pQue->Dir == NRFX_USBD_QUE_IN_SCRATCH ?
+		(const uint8_t *)&pQue->Scratch : pQue->pBuffer;
 	uint16_t len = pQue->Len;
 
-	// Both directions use the same layout, at different register-bank offsets.
-	uintptr_t epReg = (uintptr_t)&NRF_USBD->EPIN[epNum];
-	uintptr_t taskReg = (uintptr_t)&NRF_USBD->TASKS_STARTEPIN[epNum];
-	if (!isIn)
+	volatile USBD_EPIN_Type *pEp;
+	volatile uint32_t *pTask;
+	if (out)
 	{
 		const uint16_t received = (uint16_t)NRF_USBD->SIZE.EPOUT[epNum];
 		if (received < len)
 			len = received;
-		epReg += offsetof(NRF_USBD_Type, EPOUT) - offsetof(NRF_USBD_Type, EPIN);
-		taskReg += offsetof(NRF_USBD_Type, TASKS_STARTEPOUT) -
-			offsetof(NRF_USBD_Type, TASKS_STARTEPIN);
+		pEp = (volatile USBD_EPIN_Type *)&NRF_USBD->EPOUT[epNum];
+		pTask = &NRF_USBD->TASKS_STARTEPOUT[epNum];
 	}
-	volatile USBD_EPIN_Type *pEp = (volatile USBD_EPIN_Type *)epReg;
-	volatile uint32_t *pTask = (volatile uint32_t *)taskReg;
+	else
+	{
+		pEp = &NRF_USBD->EPIN[epNum];
+		pTask = &NRF_USBD->TASKS_STARTEPIN[epNum];
+	}
+
 	pEp->PTR = (uint32_t)(uintptr_t)pBuffer;
 	pEp->MAXCNT = len;
 	*pTask = 1U;
