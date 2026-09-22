008956679504c820319de5b9d5cf38f7b8fdc908
hnhoan <hnhoan@i-syst.com>
2026-09-22 19:41:03 -0400

    usb: let endpoint limits reject invalid controller

--- src/usb/usbd_epalloc.cpp
+++ src/usb/usbd_epalloc.cpp
@@ -166,7 +166,6 @@ bool UsbdEpAlloc(int DevNo, const UsbdEpAllocReq_t *pReq,
 				 UsbDeviceClass *pClass, UsbdEpAllocRes_t *pRes)
 {
 	if (pReq == nullptr || pRes == nullptr ||
-		DevNo < 0 || DevNo >= USB_CTRLR_CNT ||
 		pReq->InterfaceCount > 16U ||
 		pReq->BidirectionalCount > USBD_EPALLOC_EP_MAXCNT ||
 		pReq->InCount > USBD_EPALLOC_EP_MAXCNT ||
