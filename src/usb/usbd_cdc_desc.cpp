55a64b7b7382ef95b6b5c18596090865e37afe80
hnhoan <hnhoan@i-syst.com>
2026-09-22 21:01:36 -0400

    usb: build CDC descriptors from a static template

--- src/usb/usbd_cdc_desc.cpp
+++ src/usb/usbd_cdc_desc.cpp
@@ -27,6 +27,63 @@ static_assert(sizeof(UsbInrtfAssDesc_t) == 8U, "USB IAD size");
 static_assert(sizeof(UsbIntrfDesc_t) == 9U, "USB interface descriptor size");
 static_assert(sizeof(UsbEndPointDesc_t) == 7U, "USB endpoint descriptor size");
 
+static constexpr UsbdCdcDesc_t UsbdCdcDescTemplate(void)
+{
+	UsbdCdcDesc_t desc = {};
+
+	desc.Association.bLength = sizeof(desc.Association);
+	desc.Association.bDescriptorType = USB_DESCTYPE_IA;
+	desc.Association.bInterfaceCount = 2U;
+	desc.Association.bFunctionClass = USB_INTRFCLASS_CDC;
+	desc.Association.bFunctionSubClass = USB_CDC_SUBCLASS_ACM;
+	desc.Association.bFunctionProtocol = USB_CDC_PROT_NONE;
+
+	desc.Control.bLength = sizeof(desc.Control);
+	desc.Control.bDescriptorType = USB_DESCTYPE_INTERFACE;
+	desc.Control.bNumEndpoints = 1U;
+	desc.Control.bInterfaceClass = USB_INTRFCLASS_CDC;
+	desc.Control.bInterfaceSubClass = USB_CDC_SUBCLASS_ACM;
+	desc.Control.bInterfaceProtocol = USB_CDC_PROT_NONE;
+
+	desc.Header.bFunctionLength = sizeof(desc.Header);
+	desc.Header.bDescriptorType = USB_FUNCTYPE_CS_INTERFACE;
+	desc.Header.bDescriptorSubtype = USB_CDC_FSUBTYPE_HEADER;
+	desc.Header.bcdCDC = 0x0120U;
+
+	desc.CallManagement.bFunctionLength = sizeof(desc.CallManagement);
+	desc.CallManagement.bDescriptorType = USB_FUNCTYPE_CS_INTERFACE;
+	desc.CallManagement.bDescriptorSubtype = USB_CDC_FSUBTYPE_CM;
+
+	desc.Acm.bFunctionLength = sizeof(desc.Acm);
+	desc.Acm.bDescriptorType = USB_FUNCTYPE_CS_INTERFACE;
+	desc.Acm.bDescriptorSubtype = USB_CDC_FSUBTYPE_ACM;
+	desc.Acm.bmCapabilities = USB_CDC_ACM_CAP_LINE_CODING;
+
+	desc.Union.bFunctionLength = sizeof(desc.Union);
+	desc.Union.bDescriptorType = USB_FUNCTYPE_CS_INTERFACE;
+	desc.Union.bDescriptorSubtype = USB_CDC_FSUBTYPE_UNION;
+
+	desc.Notification.bLength = sizeof(desc.Notification);
+	desc.Notification.bDescriptorType = USB_DESCTYPE_ENDPOINT;
+	desc.Notification.bmAttributes = USB_ENDPATT_TRANS_INT;
+	desc.Notification.wMaxPacketSize = USBD_CDC_NOTIF_MPS;
+
+	desc.Data.bLength = sizeof(desc.Data);
+	desc.Data.bDescriptorType = USB_DESCTYPE_INTERFACE;
+	desc.Data.bNumEndpoints = 2U;
+	desc.Data.bInterfaceClass = USB_INTRFCLASS_CDCDATA;
+	desc.Data.bInterfaceProtocol = USB_CDCDATA_PROT_NONE;
+
+	desc.Out.bLength = sizeof(desc.Out);
+	desc.Out.bDescriptorType = USB_DESCTYPE_ENDPOINT;
+	desc.Out.bmAttributes = USB_ENDPATT_TRANS_BULK;
+
+	desc.In = desc.Out;
+	return desc;
+}
+
+static constexpr UsbdCdcDesc_t s_CdcDescTemplate = UsbdCdcDescTemplate();
+
 // Weak so an application can replace runtime fragment building with a static
 // fragment. When overridden, this default is dropped by unused-section removal.
 // Pair a replacement here with a strong UsbGetDescriptor when building fully
@@ -48,72 +105,23 @@ bool UsbdCdcMakeDesc(UsbdCdcDesc_t *pDesc, const UsbdCdcDev_t *pCdc,
 		USBD_CDC_BULK_HS_MPS : USBD_CDC_BULK_FS_MPS;
 	const uint8_t interval = Speed == USB_SPEED_HIGH ?
 		USBD_CDC_NOTIF_INTERVAL_HS : USBD_CDC_NOTIF_INTERVAL_FS;
+	const uint8_t stringIndex = HasFunctionString ? 4U : 0U;
 
-	memset(pDesc, 0, sizeof(*pDesc));
+	memcpy(pDesc, &s_CdcDescTemplate, sizeof(*pDesc));
 
-	pDesc->Association.bLength = sizeof(pDesc->Association);
-	pDesc->Association.bDescriptorType = USB_DESCTYPE_IA;
 	pDesc->Association.bFirstInterface = control;
-	pDesc->Association.bInterfaceCount = 2U;
-	pDesc->Association.bFunctionClass = USB_INTRFCLASS_CDC;
-	pDesc->Association.bFunctionSubClass = USB_CDC_SUBCLASS_ACM;
-	pDesc->Association.bFunctionProtocol = USB_CDC_PROT_NONE;
-	pDesc->Association.iFunction = HasFunctionString ? 4U : 0U;
-
-	pDesc->Control.bLength = sizeof(pDesc->Control);
-	pDesc->Control.bDescriptorType = USB_DESCTYPE_INTERFACE;
+	pDesc->Association.iFunction = stringIndex;
 	pDesc->Control.bInterfaceNumber = control;
-	pDesc->Control.bAlternateSetting = 0U;
-	pDesc->Control.bNumEndpoints = 1U;
-	pDesc->Control.bInterfaceClass = USB_INTRFCLASS_CDC;
-	pDesc->Control.bInterfaceSubClass = USB_CDC_SUBCLASS_ACM;
-	pDesc->Control.bInterfaceProtocol = USB_CDC_PROT_NONE;
-	pDesc->Control.iInterface = HasFunctionString ? 4U : 0U;
-
-	pDesc->Header.bFunctionLength = sizeof(pDesc->Header);
-	pDesc->Header.bDescriptorType = USB_FUNCTYPE_CS_INTERFACE;
-	pDesc->Header.bDescriptorSubtype = USB_CDC_FSUBTYPE_HEADER;
-	pDesc->Header.bcdCDC = 0x0120U;
-
-	pDesc->CallManagement.bFunctionLength = sizeof(pDesc->CallManagement);
-	pDesc->CallManagement.bDescriptorType = USB_FUNCTYPE_CS_INTERFACE;
-	pDesc->CallManagement.bDescriptorSubtype = USB_CDC_FSUBTYPE_CM;
+	pDesc->Control.iInterface = stringIndex;
 	pDesc->CallManagement.bDataInterface = data;
-
-	pDesc->Acm.bFunctionLength = sizeof(pDesc->Acm);
-	pDesc->Acm.bDescriptorType = USB_FUNCTYPE_CS_INTERFACE;
-	pDesc->Acm.bDescriptorSubtype = USB_CDC_FSUBTYPE_ACM;
-	pDesc->Acm.bmCapabilities = USB_CDC_ACM_CAP_LINE_CODING;
-
-	pDesc->Union.bFunctionLength = sizeof(pDesc->Union);
-	pDesc->Union.bDescriptorType = USB_FUNCTYPE_CS_INTERFACE;
-	pDesc->Union.bDescriptorSubtype = USB_CDC_FSUBTYPE_UNION;
 	pDesc->Union.bControlInterface = control;
 	pDesc->Union.bSubordinateInterf[0] = data;
-
-	pDesc->Notification.bLength = sizeof(pDesc->Notification);
-	pDesc->Notification.bDescriptorType = USB_DESCTYPE_ENDPOINT;
-	pDesc->Notification.bEndpointAddress =
-		USB_ENDPADDR_DIRIN(pCdc->NotifyEpNo);
-	pDesc->Notification.bmAttributes = USB_ENDPATT_TRANS_INT;
-	pDesc->Notification.wMaxPacketSize = USBD_CDC_NOTIF_MPS;
+	pDesc->Notification.bEndpointAddress = USB_ENDPADDR_DIRIN(pCdc->NotifyEpNo);
 	pDesc->Notification.bInterval = interval;
-
-	pDesc->Data.bLength = sizeof(pDesc->Data);
-	pDesc->Data.bDescriptorType = USB_DESCTYPE_INTERFACE;
 	pDesc->Data.bInterfaceNumber = data;
-	pDesc->Data.bAlternateSetting = 0U;
-	pDesc->Data.bNumEndpoints = 2U;
-	pDesc->Data.bInterfaceClass = USB_INTRFCLASS_CDCDATA;
-	pDesc->Data.bInterfaceProtocol = USB_CDCDATA_PROT_NONE;
-
-	pDesc->Out.bLength = sizeof(pDesc->Out);
-	pDesc->Out.bDescriptorType = USB_DESCTYPE_ENDPOINT;
 	pDesc->Out.bEndpointAddress = USB_ENDPADDR_DIROUT(pCdc->DataEpNo);
-	pDesc->Out.bmAttributes = USB_ENDPATT_TRANS_BULK;
 	pDesc->Out.wMaxPacketSize = bulkMps;
-
-	pDesc->In = pDesc->Out;
 	pDesc->In.bEndpointAddress = USB_ENDPADDR_DIRIN(pCdc->DataEpNo);
+	pDesc->In.wMaxPacketSize = bulkMps;
 	return true;
 }
