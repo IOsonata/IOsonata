3457691629c6b13eace3f49cdb768dae26bfa264
hnhoan <hnhoan@i-syst.com>
2026-09-24 08:24:20 -0400

    usb: simplify ISO hardware enable

--- ARM/Nordic/nRF52/src/usb_ctrlr_nrf52_iso.cpp
+++ ARM/Nordic/nRF52/src/usb_ctrlr_nrf52_iso.cpp
@@ -57,9 +57,9 @@ void nRFIsoHwEnable(bool In, bool Enable)
 	const uint32_t endMsk = In ?
 		USBD_INTEN_ENDISOIN_Msk : USBD_INTEN_ENDISOOUT_Msk;
 
+	*pEnd = 0U;
 	if (Enable)
 	{
-		*pEnd = 0U;
 		NRF_USBD->INTENSET = endMsk;
 		*pEnable |= msk;
 	}
@@ -67,7 +67,6 @@ void nRFIsoHwEnable(bool In, bool Enable)
 	{
 		NRF_USBD->INTENCLR = endMsk;
 		*pEnable &= ~msk;
-		*pEnd = 0U;
 	}
 }
 
