4c156373b27474af2a56d25771129f663044c9df
hnhoan <hnhoan@i-syst.com>
2026-09-24 12:47:40 -0400

    usb: keep ISO endpoint enable helper shared

--- ARM/Nordic/nRF52/src/usb_ctrlr_nrf52_iso.cpp
+++ ARM/Nordic/nRF52/src/usb_ctrlr_nrf52_iso.cpp
@@ -47,7 +47,7 @@ SOFTWARE.
 static_assert(offsetof(USBD_ISOOUT_Type, MAXCNT) ==
 	offsetof(USBD_ISOIN_Type, MAXCNT), "ISO register layout");
 
-static inline __attribute__((always_inline))
+static __attribute__((noinline))
 void nRFIsoHwEnable(bool In, bool Enable)
 {
 	volatile uint32_t *pEnd = In ?
