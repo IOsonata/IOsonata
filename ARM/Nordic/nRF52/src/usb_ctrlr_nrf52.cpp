c8baf3c6353fde9b0fb737ab2063b7a1f12fb5e9
hnhoan <hnhoan@i-syst.com>
2026-09-25 12:53:45 -0400

    usb: inline endpoint close queue filtering

--- ARM/Nordic/nRF52/src/usb_ctrlr_nrf52.cpp
+++ ARM/Nordic/nRF52/src/usb_ctrlr_nrf52.cpp
@@ -110,13 +110,6 @@ enum
 	NRFX_USBD_QUE_IN_SCRATCH = 2U,
 };
 
-enum
-{
-	NRFUSBD_DIR_OUT = 1U,
-	NRFUSBD_DIR_IN = 2U,
-	NRFUSBD_DIR_BOTH = NRFUSBD_DIR_OUT | NRFUSBD_DIR_IN,
-};
-
 #pragma pack(push, 4)
 
 typedef struct __nRF_Usbd_Que {
@@ -668,21 +661,6 @@ static __attribute__((noinline)) void nRFUsbdStartQueuedDma(void)
 		return;
 	}
 	nRFUsbdDmaUnlock();
-}
-
-static void nRFUsbdQueRemove(uint8_t EpNum, uint8_t Dir)
-{
-	int count = CFifoUsed(s_Usbd.hQue);
-	while (count-- > 0)
-	{
-		nRFUsbdQue_t que = *(nRFUsbdQue_t *)CFifoGet(s_Usbd.hQue);
-		if (que.EpNum == EpNum &&
-			(Dir == NRFUSBD_DIR_BOTH ||
-			 (que.Dir == NRFX_USBD_QUE_OUT ?
-				Dir == NRFUSBD_DIR_OUT : Dir == NRFUSBD_DIR_IN)))
-			continue;
-		*(nRFUsbdQue_t *)CFifoPut(s_Usbd.hQue) = que;
-	}
 }
 
 
@@ -1401,7 +1379,14 @@ void UsbCtrlrEpClose(int DevNo, uint8_t EpNo, bool bIn)
 	// Quiesce the shared channel before rotating hQue. The active regular DMA
 	// owns the FIFO head, and scratch IN may DMA directly from that entry.
 	nRFUsbdDmaWait();
-	nRFUsbdQueRemove(EpNo, bIn ? NRFUSBD_DIR_IN : NRFUSBD_DIR_OUT);
+	int count = CFifoUsed(s_Usbd.hQue);
+	while (count-- > 0)
+	{
+		nRFUsbdQue_t que = *(nRFUsbdQue_t *)CFifoGet(s_Usbd.hQue);
+		if (que.EpNum != EpNo ||
+			((que.Dir != NRFX_USBD_QUE_OUT) != bIn))
+			*(nRFUsbdQue_t *)CFifoPut(s_Usbd.hQue) = que;
+	}
 	nRFUsbdEpHwEnable(EpNo, bIn, false);
 	NRF_USBD->EPDATASTATUS = 1UL << (EpNo + (bIn ? 0U : 16U));
 	__DSB();
